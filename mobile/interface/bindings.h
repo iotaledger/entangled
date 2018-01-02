b3a1320f78a3b788d43429cd0b034a89bc36e83a
Andreas C. Osowski <andreas@osowski.de>
2018-01-02 17:32:37 +0100

    mobile/interface: add extern C

--- mobile/interface/bindings.h
+++ mobile/interface/bindings.h
@@ -1,6 +1,14 @@
 #ifndef __MOBILE_INTERFACE_BINDINGS_H_
 #define __MOBILE_INTERFACE_BINDINGS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char* do_pow(const char* trytes, int mwm);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
