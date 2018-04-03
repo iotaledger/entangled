dc1fbc74083201e736ee3ada241848016939a19d
Emmanuel Merali <emmanuel@merali.me>
2018-04-03 14:11:04 +0300

    Fixed edge case with out of bound error

--- common/trinary/tests/test_tryte_long.c
+++ common/trinary/tests/test_tryte_long.c
@@ -11,11 +11,11 @@
 #include "common/trinary/tryte_long.h"
 
 static const char* TRYTES[] = {
-    "9", "A", "M", "NA", "LZ", "MM", "NNA", "VYM", "Z", "EBN", "PCHBSYZZHYK"
+    "9", "A", "M", "NA", "LZ", "MM", "NNA", "N9A", "VYM", "Z", "EBN", "PCHBSYZZHYK"
 };
 
 static const int64_t VALUES[] = {
-    0, 1, 13, 14, -15, 364, 365, 9418, -1, -9418, 2251799813685247
+    0, 1, 13, 14, -15, 364, 365, 716, 9418, -1, -9418, 2251799813685247
 };
 
 void test_trytes_to_long(void) {
