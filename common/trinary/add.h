c0e1d7f75888d82f4583a29250adadc7cf580a80
Andreas C. Osowski <andreas@osowski.de>
2018-04-04 18:24:10 +0200

    common/trinary: fix adder

--- common/trinary/add.h
+++ common/trinary/add.h
@@ -4,5 +4,6 @@
 #include "common/trinary/trits.h"
 
 int add_assign(trit_t *t, size_t s, int64_t v);
+void add_trits(const trit_t *const lh, trit_t *rh, size_t len);
 
 #endif  //__TRIT_ADD_H__
---
+++ common/trinary/tests/test_add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unity/unity.h>
+
+#include "common/trinary/add.h"
+
+void test_add(void) {
+  trit_t h[3] = {-1, 0, 1};
+  trit_t g[3] = {1, -1, 1};
+  trit_t z[3] = {-1, 0, 0};
+
+  add_trits(h, z, 3);
+  TEST_ASSERT_EQUAL_MEMORY(g, z, sizeof(z));
+  add_assign(z, 3, 1);
+  TEST_ASSERT_EQUAL_MEMORY(h, z, sizeof(z));
+}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_add);
+
+  return UNITY_END();
+}
