e5d4993a1c7683be47ac5a3c6feffcddd0aa3280
Emmanuel Merali <emmanuel@merali.me>
2018-04-29 12:40:15 +0300

    common/model: add transfer model

---
+++ common/model/tests/test_transfer.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://gitlab.com/iota-foundation/software/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <unity/unity.h>
+
+#include "common/model/transfer.h"
+
+int main(void) {
+  UNITY_BEGIN();
+
+  return UNITY_END();
+}
