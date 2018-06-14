41b321cc6bc3db9433496916042ca3e1476f64ee
Thibault Martinez <thibault@iota.org>
2018-06-14 19:53:31 +0200

    Fix hashing of merkle trees of size 1

--- common/mam/merkle.c
+++ common/mam/merkle.c
@@ -3,27 +3,6 @@
 
 static trit_t const merkle_null_hash[HASH_LENGTH] = {0};
 
-// thanks to
-// https://stackoverflow.com/questions/3272424/compute-fast-log-base-2-ceiling#3272516
-static int ceil_log2(unsigned long long x) {
-  static const unsigned long long t[6] = {
-      0xFFFFFFFF00000000ull, 0x00000000FFFF0000ull, 0x000000000000FF00ull,
-      0x00000000000000F0ull, 0x000000000000000Cull, 0x0000000000000002ull};
-
-  int y = (((x & (x - 1)) == 0) ? 0 : 1);
-  int j = 32;
-  int depth_i;
-
-  for (depth_i = 0; depth_i < 6; depth_i++) {
-    int k = (((x & t[depth_i]) == 0) ? 0 : j);
-    y += k;
-    x >>= k;
-    j >>= 1;
-  }
-
-  return y;
-}
-
 static size_t binary_tree_size(size_t acc, size_t depth) {
   if (depth == 0) {
     return acc + 1;
@@ -77,7 +56,7 @@ int merkle_create(trit_t *const tree, trit_t *const seed, int64_t offset,
   size_t key_size = security * ISS_KEY_LENGTH;
   trit_t key[key_size];
 
-  const size_t td = ceil_log2(base_size);
+  const size_t td = merkle_depth(merkle_size(base_size)) - 1;
 
   // create base addresses
   for (depth_i = 0; depth_i < base_size; depth_i++) {
