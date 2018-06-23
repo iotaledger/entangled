6b116b90e52c066444340724ffcda43fe9ab781f
Thibault Martinez <thibault@iota.org>
2018-06-23 20:58:21 +0200

    Add missing includes

--- common/curl-p/digest.c
+++ common/curl-p/digest.c
@@ -1,11 +1,12 @@
-#include "digest.h"
+#include <string.h>
+
 #include "common/trinary/trit_tryte.h"
+#include "digest.h"
 
-void curl_digest(const trit_t* const trits, size_t len, trit_t* out, Curl* curl){
-    char digest[HASH_LENGTH];
-    curl_absorb(curl, trits, len);
-    curl_squeeze(curl, digest, HASH_LENGTH);
-    memcpy((tryte_t*)out,(trit_t*)digest, HASH_LENGTH);
+void curl_digest(const trit_t* const trits, size_t len, trit_t* out,
+                 Curl* curl) {
+  char digest[HASH_LENGTH];
+  curl_absorb(curl, trits, len);
+  curl_squeeze(curl, digest, HASH_LENGTH);
+  memcpy((tryte_t*)out, (trit_t*)digest, HASH_LENGTH);
 }
-
-
--- common/helpers/digest.c
+++ common/helpers/digest.c
@@ -1,25 +1,26 @@
-#include "digest.h"
+#include <string.h>
+
 #include "common/curl-p/digest.h"
 #include "common/trinary/trit_tryte.h"
+#include "digest.h"
 
 #define TRYTE_LENGTH 2673
 #define TRANSACTION_LENGTH TRYTE_LENGTH * 3
 
 char* iota_digest(const char* trytes) {
-    Curl curl;
-    init_curl(&curl);
-    curl.type = CURL_P_81;
+  Curl curl;
+  init_curl(&curl);
+  curl.type = CURL_P_81;
 
-    size_t length = strnlen(trytes, TRANSACTION_LENGTH);
-    char input[sizeof(char) * length * 3];
-    trytes_to_trits((tryte_t*)trytes, input, length);
+  size_t length = strnlen(trytes, TRANSACTION_LENGTH);
+  char input[sizeof(char) * length * 3];
+  trytes_to_trits((tryte_t*)trytes, input, length);
 
-    char trits_hash[HASH_LENGTH];
-    curl_digest(input, length * 3, trits_hash, &curl);
+  char trits_hash[HASH_LENGTH];
+  curl_digest(input, length * 3, trits_hash, &curl);
 
-    char* hash = calloc(HASH_LENGTH/3 + 1, sizeof(char));
-    trits_to_trytes((trit_t*)trits_hash, (tryte_t*)hash, HASH_LENGTH);
+  char* hash = calloc(HASH_LENGTH / 3 + 1, sizeof(char));
+  trits_to_trytes((trit_t*)trits_hash, (tryte_t*)hash, HASH_LENGTH);
 
-    return hash;
-
+  return hash;
 }
