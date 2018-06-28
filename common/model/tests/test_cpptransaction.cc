6c725c9d0ee26f2c3a689993cbcf9153dc5c73b5
Emmanuel Merali <emmanuel@merali.me>
2018-06-28 09:08:27 +0300

    entangled/model: added missing test file

---
+++ common/model/tests/test_cpptransaction.cc
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <unity/unity.h>
+
+int main(void) {
+  UNITY_BEGIN();
+
+  return UNITY_END();
+}
