2a49b7f180e5138d86191ecb29e74ba45328f0dd
Thibault Martinez <thibault@iota.org>
2018-07-01 21:44:14 +0800

    Requester

---
+++ common/network/components/requester.h
@@ -0,0 +1,11 @@
+#ifndef __COMMON_NETWORK_COMPONENTS_REQUESTER_H__
+#define __COMMON_NETWORK_COMPONENTS_REQUESTER_H__
+
+#include "common/network/concurrent_queue_trytes.h"
+
+void request_transaction(tryte_t *const hash);
+tryte_t *get_transaction_to_request();
+
+extern concurrent_queue_of_tryte_p requester_queue;
+
+#endif  //__COMMON_NETWORK_COMPONENTS_REQUESTER_H__
