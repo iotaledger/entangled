64509ee44c96feed017d0c04d7b3a2ba19dabf74
Thibault Martinez <thibault@iota.org>
2018-07-01 19:06:20 +0800

    Broadcaster

---
+++ common/network/components/broadcaster.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_NETWORK_COMPONENTS_BROADCASTER_H__
+#define __COMMON_NETWORK_COMPONENTS_BROADCASTER_H__
+
+#include "common/network/concurrent_queue_transaction.h"
+#include "common/trinary/tryte.h"
+
+void broadcaster_on_next(tryte_t const *const hash);
+void *broadcaster_routine(void *arg);
+
+extern concurrent_queue_of_iota_transaction_t broadcaster;
+
+#endif  //__COMMON_NETWORK_COMPONENTS_BROADCASTER_H__
