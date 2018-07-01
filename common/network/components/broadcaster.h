48cedf61ca0717492b23c4a944a687a861c05749
Thibault Martinez <thibault@iota.org>
2018-07-01 22:04:58 +0800

    Remove concurrent queue of transaction

--- common/network/components/broadcaster.h
+++ common/network/components/broadcaster.h
@@ -1,12 +1,11 @@
 #ifndef __COMMON_NETWORK_COMPONENTS_BROADCASTER_H__
 #define __COMMON_NETWORK_COMPONENTS_BROADCASTER_H__
 
-#include "common/network/queues/concurrent_queue_transaction.h"
-#include "common/trinary/tryte.h"
+#include "common/network/queues/concurrent_queue_trytes.h"
 
 void broadcaster_on_next(tryte_t const *const hash);
 void *broadcaster_routine(void *arg);
 
-extern concurrent_queue_of_iota_transaction_t broadcaster_queue;
+extern concurrent_queue_of_tryte_p broadcaster_queue;
 
 #endif  //__COMMON_NETWORK_COMPONENTS_BROADCASTER_H__
--- common/network/queues/concurrent_queue_transaction.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COMMON_NETWORK_CONCURRENT_QUEUE_TRANSACTION_H__
-#define __COMMON_NETWORK_CONCURRENT_QUEUE_TRANSACTION_H__
-
-#include "common/model/transaction.h"
-#include "concurrent_queue.h.inc"
-
-DECLARE_CONCURRENT_QUEUE_OF(iota_transaction_t);
-
-#endif  // __COMMON_NETWORK_CONCURRENT_QUEUE_TRANSACTION_H__
