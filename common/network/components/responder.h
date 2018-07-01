7ba4a390e3d0f42c034a8de0887499ce79d2f5d8
Thibault Martinez <thibault@iota.org>
2018-07-01 22:27:10 +0800

    Rename replier to responder

---
+++ common/network/components/responder.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_NETWORK_COMPONENTS_RESPONDER_H__
+#define __COMMON_NETWORK_COMPONENTS_RESPONDER_H__
+
+#include "common/network/queues/concurrent_queue_hash_request.h"
+#include "common/trinary/tryte.h"
+
+void responder_on_next(tryte_t const *const hash, neighbor_t *const neighbor);
+void *responder_routine(void *arg);
+
+extern concurrent_queue_of_hash_request_t responder_queue;
+
+#endif  //__COMMON_NETWORK_COMPONENTS_RESPONDER_H__
