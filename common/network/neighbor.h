ba8610d77d48dce470eb43123d87a37d23ac6fbc
Thibault Martinez <thibault@iota.org>
2018-07-03 13:12:05 +0800

    Add send functions to neighbor

--- common/network/neighbor.h
+++ common/network/neighbor.h
@@ -1,13 +1,29 @@
-#ifndef __COMMON_MODEL_NEIGHBOR_H__
-#define __COMMON_MODEL_NEIGHBOR_H__
+#ifndef __COMMON_NETWORK_NEIGHBOR_H__
+#define __COMMON_NETWORK_NEIGHBOR_H__
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum connection_type_e { TCP, UDP } connection_type_t;
 
 typedef struct {
-  char *address;
+  char *host;
+  uint16_t port;
+  int socket;
+  connection_type_t connection_type;
   unsigned int number_of_all_transactions;
   unsigned int number_of_new_transactions;
   unsigned int number_of_invalid_transactions;
-  unsigned int random_transaction_requests;
   unsigned int number_of_sent_transactions;
+  unsigned int random_transaction_requests;
+  int flagged;
 } neighbor_t;
 
-#endif
+int neighbor_send(neighbor_t *const neighbor, void *const packet,
+                  size_t const packet_size);
+int tcp_neighbor_send(neighbor_t *const neighbor, void *const packet,
+                      size_t const packet_size);
+int udp_neigbhor_send(neighbor_t *const neighbor, void *const packet,
+                      size_t const packet_size);
+
+#endif  // __COMMON_NETWORK_NEIGHBOR_H__
