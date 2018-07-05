f0946be07918d4a2cafc2b528681da22af44b6fa
Thibault Martinez <thibault@iota.org>
2018-07-05 17:06:25 +0800

    Iota packet data from tryte_t to char *

--- common/network/iota_packet.h
+++ common/network/iota_packet.h
@@ -9,12 +9,11 @@
 #define __COMMON_NETWORK_IOTA_PACKET_H__
 
 #include "common/network/socket.h"
-#include "common/trinary/tryte.h"
 
 #define TRANSACTION_PACKET_SIZE 1650
 
 typedef struct {
-  tryte_t data[TRANSACTION_PACKET_SIZE];
+  char data[TRANSACTION_PACKET_SIZE];
   socket_addr_t source;
 } iota_packet_t;
 
