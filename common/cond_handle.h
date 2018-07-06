e4c5065d723deba3613477da8ba725857031fc07
Thibault Martinez <thibault@iota.org>
2018-07-06 10:10:45 +0800

    Conditional -> Condition variables

--- common/cond_handle.h
+++ common/cond_handle.h
@@ -9,8 +9,8 @@
 #define COMMON_COND_HANDLE_H_
 
 /**
- * We declare a type cond_handle_t that depends on the system conditional
- * variable primitives
+ * We declare a type cond_handle_t that depends on the system condition variable
+ * primitives
  */
 
 #include <unistd.h>
@@ -22,21 +22,20 @@ typedef pthread_cond_t cond_handle_t;
 
 #else
 
-#error "No conditional variable primitives found"
+#error "No condition variable primitives found"
 
 #endif  // _POSIX_THREADS
 
 /**
  * The following functions are defined contextually with macros depending on the
- * system available conditional variable primitives, some of them might have
- * no effect if not needed by the underlying API
+ * system available condition variable primitives, some of them might have no
+ * effect if not needed by the underlying API
  */
 
 /**
- * Initializes a condition variable object with the specified attributes for
- * use
+ * Initializes a condition variable object with the specified attributes for use
  *
- * @param cond The conditional variable
+ * @param cond The condition variable
  *
  * @return exit status
  */
@@ -46,7 +45,7 @@ int cond_handle_init(cond_handle_t* const cond);
  * Wakes up at least one thread that is currently waiting on the condition
  * variable specified by cond
  *
- * @param cond The conditional variable
+ * @param cond The condition variable
  *
  * @return exit status
  */
@@ -56,7 +55,7 @@ int cond_handle_signal(cond_handle_t* const cond);
  * Wakes up all threads that are currently waiting on the condition variable
  * specified by cond
  *
- * @param cond The conditional variable
+ * @param cond The condition variable
  *
  * @return exit status
  */
@@ -66,7 +65,7 @@ int cond_handle_broadcast(cond_handle_t* const cond);
  * Blocks the calling thread, waiting for the condition specified by cond to be
  * signaled or broadcast to
  *
- * @param cond The conditional variable
+ * @param cond The condition variable
  *
  * @return exit status
  */
@@ -75,7 +74,7 @@ int cond_handle_wait(cond_handle_t* const cond);
 /**
  * Destroys the condition variable specified by cond
  *
- * @param cond The conditional variable
+ * @param cond The condition variable
  *
  * @return exit status
  */
