f07441bf82756df3d43ba6916af7d45934cfb9d7
Thibault Martinez <thibault.martinez.30@gmail.com>
2018-07-16 18:57:02 +0800

    Concurrent queue / Lock handle / Cond handle (#58)
    * Implement concurrent queue
    
    * Test concurrent queue
    
    * Implement hash request and trit array concurrent queues
    
    * Implement lock handle
    
    * Switch lock to pthread_mutex_t
    
    * Implement cond handle
    
    * Add cond/lock to concurrent queue
    
    * Constness
    
    * Improve NEW_CONCURRENT_QUEUE_OF
    
    * Documentation
    
    * Static inline lock/cond handle functions
    
    * Improve queue initialization
    
    * Address PR comments
    
    * Empty returns bool
    
    * Sizeof on actual type
    
    * INIT_CONCURRENT_QUEUE_OF now calls a function
    
    * DESTROY_CONCURRENT_QUEUE_OF
    
    * Cast calloc for C++ compilation
    
    * Add include guards
    
    * Avoid C++ mangling of concurrent queue functions
    
    * Move #include <stdlib.h> to implementation file

--- common/cond_handle.h
+++ common/cond_handle.h
@@ -9,28 +9,20 @@
 #define COMMON_COND_HANDLE_H_
 
 /**
- * We declare a type cond_handle_t that depends on the system condition variable
- * primitives
+ * We define a type cond_handle_t mapping to a system available condition
+ * variable primitive and its associated functions, some of them might have no
+ * effect if not needed by the underlying API
  */
 
 #include <unistd.h>
 
+#include "lock_handle.h"
+
 #ifdef _POSIX_THREADS
 
 #include <pthread.h>
+
 typedef pthread_cond_t cond_handle_t;
-
-#else
-
-#error "No condition variable primitives found"
-
-#endif  // _POSIX_THREADS
-
-/**
- * The following functions are defined contextually with macros depending on the
- * system available condition variable primitives, some of them might have no
- * effect if not needed by the underlying API
- */
 
 /**
  * Initializes a condition variable object with the specified attributes for use
@@ -39,7 +31,9 @@ typedef pthread_cond_t cond_handle_t;
  *
  * @return exit status
  */
-int cond_handle_init(cond_handle_t* const cond);
+static inline int cond_handle_init(cond_handle_t* const cond) {
+  return pthread_cond_init(cond, NULL);
+}
 
 /**
  * Wakes up at least one thread that is currently waiting on the condition
@@ -49,7 +43,9 @@ int cond_handle_init(cond_handle_t* const cond);
  *
  * @return exit status
  */
-int cond_handle_signal(cond_handle_t* const cond);
+static inline int cond_handle_signal(cond_handle_t* const cond) {
+  return pthread_cond_signal(cond);
+}
 
 /**
  * Wakes up all threads that are currently waiting on the condition variable
@@ -59,17 +55,23 @@ int cond_handle_signal(cond_handle_t* const cond);
  *
  * @return exit status
  */
-int cond_handle_broadcast(cond_handle_t* const cond);
+static inline int cond_handle_broadcast(cond_handle_t* const cond) {
+  return pthread_cond_broadcast(cond);
+}
 
 /**
  * Blocks the calling thread, waiting for the condition specified by cond to be
  * signaled or broadcast to
  *
  * @param cond The condition variable
+ * @param lock The associated lock
  *
  * @return exit status
  */
-int cond_handle_wait(cond_handle_t* const cond);
+static inline int cond_handle_wait(cond_handle_t* const cond,
+                                   lock_handle_t* const lock) {
+  return pthread_cond_wait(cond, lock);
+}
 
 /**
  * Destroys the condition variable specified by cond
@@ -78,6 +80,14 @@ int cond_handle_wait(cond_handle_t* const cond);
  *
  * @return exit status
  */
-int cond_handle_destroy(cond_handle_t* const cond);
+static inline int cond_handle_destroy(cond_handle_t* const cond) {
+  return pthread_cond_destroy(cond);
+}
+
+#else
+
+#error "No condition variable primitives found"
+
+#endif  // _POSIX_THREADS
 
 #endif  // COMMON_COND_HANDLE_H_
---
+++ common/network/queues/test/test_concurrent_queue.c
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include <unity/unity.h>
+
+#include "common/network/queues/concurrent_queue.c.inc"
+#include "common/network/queues/concurrent_queue.h.inc"
+
+DECLARE_CONCURRENT_QUEUE_OF(int)
+DEFINE_CONCURRENT_QUEUE_OF(int)
+
+typedef concurrent_queue_of_int conqueue_int;
+
+void test_concurrent_queue() {
+  int data;
+  conqueue_int *queue;
+  conqueue_int *nqueue = NULL;
+  TEST_ASSERT_EQUAL_INT(INIT_CONCURRENT_QUEUE_OF(int, queue),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_NOT_NULL(queue);
+
+  TEST_ASSERT_NULL(queue->vtable->front(NULL));
+  TEST_ASSERT_NULL(queue->vtable->front(queue));
+  TEST_ASSERT_NULL(queue->vtable->back(NULL));
+  TEST_ASSERT_NULL(queue->vtable->back(queue));
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(NULL), true);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(queue), true);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(NULL), 0);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(queue), 0);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(NULL, 0),
+                        CONCURRENT_QUEUE_NULL_SELF);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->pop(NULL, &data),
+                        CONCURRENT_QUEUE_NULL_SELF);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->pop(queue, NULL),
+                        CONCURRENT_QUEUE_NULL_DATA);
+
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 0),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 1),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 2),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 3),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 4),
+                        CONCURRENT_QUEUE_SUCCESS);
+
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->front(queue), 0);
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->back(queue), 4);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(queue), false);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(queue), 5);
+
+  TEST_ASSERT_EQUAL_INT(queue->vtable->pop(queue, &data),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(data, 0);
+
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->front(queue), 1);
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->back(queue), 4);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(queue), false);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(queue), 4);
+
+  TEST_ASSERT_EQUAL_INT(queue->vtable->pop(queue, &data),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(data, 1);
+
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->front(queue), 2);
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->back(queue), 4);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(queue), false);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(queue), 3);
+
+  TEST_ASSERT_EQUAL_INT(queue->vtable->pop(queue, &data),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(data, 2);
+
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->front(queue), 3);
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->back(queue), 4);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(queue), false);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(queue), 2);
+
+  TEST_ASSERT_EQUAL_INT(queue->vtable->pop(queue, &data),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(data, 3);
+
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->front(queue), 4);
+  TEST_ASSERT_EQUAL_INT(*queue->vtable->back(queue), 4);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(queue), false);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(queue), 1);
+
+  TEST_ASSERT_EQUAL_INT(queue->vtable->pop(queue, &data),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(data, 4);
+
+  TEST_ASSERT_NULL(queue->vtable->front(queue));
+  TEST_ASSERT_NULL(queue->vtable->back(queue));
+  TEST_ASSERT_EQUAL_INT(queue->vtable->empty(queue), true);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->size(queue), 0);
+
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 0),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 1),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 2),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 3),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_EQUAL_INT(queue->vtable->push(queue, 4),
+                        CONCURRENT_QUEUE_SUCCESS);
+
+  TEST_ASSERT_EQUAL_INT(DESTROY_CONCURRENT_QUEUE_OF(int, nqueue),
+                        CONCURRENT_QUEUE_NULL_SELF);
+  TEST_ASSERT_EQUAL_INT(DESTROY_CONCURRENT_QUEUE_OF(int, queue),
+                        CONCURRENT_QUEUE_SUCCESS);
+  TEST_ASSERT_NULL(queue);
+}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_concurrent_queue);
+
+  return UNITY_END();
+}
