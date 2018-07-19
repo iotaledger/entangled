877bd2107df272ce5eeb0524c93f0e1fe5149053
Emmanuel Merali <emmanuel@merali.me>
2018-07-19 09:03:23 +0300

    entangled/trinary: cpp wrapper for flex_trit

---
+++ common/trinary/flex_trit.cc
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include "common/trinary/flex_trit.h"
+
+namespace iota {
+namespace trinary {}
+}  // namespace iota
