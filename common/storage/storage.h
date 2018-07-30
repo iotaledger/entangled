86ff21a3acd6c6dfd586ab252a2d6269df496fe7
Tsvi Sabo <tsvi.sabo@iota.org>
2018-07-30 11:19:26 +0300

    common/storage: layer & sqlite3 implementation (#69)
    * Storage layer WIP
    
    * storage layer WIP - indexes
    
    * storage layer WIP - change api to input iota_transaction
    
    * storage Layer WIP - insert statement
    
    * storage Layer WIP - select + exist impl
    
    * storage Layer WIP - constants
    
    * storage Layer WIP - multithreaded conf
    
    * storage - add logger support
    
    * storage - add logger support for statements too
    
    * rename some files
    
    * Fix compilation errors caused by renaming *.cc to *.c
    
    * Fix according to review
    
    * Storage layer:\n-Add tests\n-Remove unnecessary main file\n-Add file helper
    
    * Fix nits - renaming

---
+++ common/storage/storage.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#ifndef __COMMON_STORAGE_STORAGE_H__
+#define __COMMON_STORAGE_STORAGE_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "common/errors.h"
+#include "common/model/transaction.h"
+#include "common/storage/connection.h"
+#include "common/trinary/trit_array.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern retcode_t iota_stor_init();
+extern retcode_t iota_stor_destroy();
+/*index_name = column name by to compare to key*/
+extern retcode_t iota_stor_store(const connection_t* const conn,
+                                 const iota_transaction_t data_in);
+extern retcode_t iota_stor_load(const connection_t* const conn,
+                                const char* col_name, const trit_array_p key,
+                                iota_transaction_t data_out[],
+                                size_t max_num_txs, size_t* num_loaded);
+extern retcode_t iota_stor_exist(const connection_t* const conn,
+                                 const char* index_name, const trit_array_p key,
+                                 bool* exist);
+
+extern retcode_t iota_stor_update(const connection_t* const conn,
+                                  const char* index_name,
+                                  const trit_array_p key,
+                                  const iota_transaction_t data_in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __COMMON_STORAGE_STORAGE_H__
