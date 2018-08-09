722dfe48951d9476c239f2cc8d256c2cfe470ae2
Thibault Martinez <thibault.martinez.30@gmail.com>
2018-08-09 11:36:46 +0200

    Logger facility (#127)
    * logger: remove old logger
    
    * logger: remove all references to old logger
    
    * logger: rework the logs
    
    * logger: move logger to utils
    
    * logger: add logs to requester component
    
    * logger: switch from static const vars to define

--- common/network/components/requester.c
+++ common/network/components/requester.c
@@ -6,13 +6,19 @@
  */
 
 #include "common/network/components/requester.h"
+#include "utils/logger_helper.h"
+
+#define REQUESTER_COMPONENT_LOGGER_ID "requester_component"
 
 bool requester_init(requester_state_t *const state, node_t *const node) {
   if (state == NULL || node == NULL) {
     return false;
   }
+  logger_helper_init(REQUESTER_COMPONENT_LOGGER_ID, LOGGER_DEBUG, true);
   if (INIT_CONCURRENT_QUEUE_OF(trit_array_p, state->queue) !=
       CONCURRENT_QUEUE_SUCCESS) {
+    log_critical(REQUESTER_COMPONENT_LOGGER_ID,
+                 "Initializing requester queue failed\n");
     return false;
   }
   state->node = node;
@@ -26,6 +32,8 @@ bool request_transaction(requester_state_t *const state,
   }
   if (state->queue->vtable->push(state->queue, hash) !=
       CONCURRENT_QUEUE_SUCCESS) {
+    log_warning(REQUESTER_COMPONENT_LOGGER_ID,
+                "Pushing to requester queue failed\n");
     return false;
   }
   return true;
@@ -39,18 +47,23 @@ trit_array_p get_transaction_to_request(requester_state_t *const state) {
   }
   if (state->queue->vtable->pop(state->queue, &hash) !=
       CONCURRENT_QUEUE_SUCCESS) {
+    log_warning(REQUESTER_COMPONENT_LOGGER_ID,
+                "Popping from requester queue failed\n");
     return NULL;
   }
   return hash;
 }
 
 bool requester_destroy(requester_state_t *const state) {
+  bool ret = true;
+
   if (state == NULL) {
     return false;
   }
   if (DESTROY_CONCURRENT_QUEUE_OF(trit_array_p, state->queue) !=
       CONCURRENT_QUEUE_SUCCESS) {
-    return false;
+    ret = false;
   }
-  return true;
+  logger_helper_destroy(REQUESTER_COMPONENT_LOGGER_ID);
+  return ret;
 }
--- utils/logger_helper.h
+++ utils/logger_helper.h
@@ -17,6 +17,17 @@ extern "C" {
 
 typedef struct logger_t logger_t;
 
+#define log_debug(id, ...) logger_helper_print(id, LOGGER_DEBUG, __VA_ARGS__)
+#define log_info(id, ...) logger_helper_print(id, LOGGER_INFO, __VA_ARGS__)
+#define log_notice(id, ...) logger_helper_print(id, LOGGER_NOTICE, __VA_ARGS__)
+#define log_warning(id, ...) \
+  logger_helper_print(id, LOGGER_WARNING, __VA_ARGS__)
+#define log_error(id, ...) logger_helper_print(id, LOGGER_ERR, __VA_ARGS__)
+#define log_critical(id, ...) logger_helper_print(id, LOGGER_CRIT, __VA_ARGS__)
+#define log_alert(id, ...) logger_helper_print(id, LOGGER_ALERT, __VA_ARGS__)
+#define log_emergency(id, ...) \
+  logger_helper_print(id, LOGGER_EMERG, __VA_ARGS__)
+
 extern void logger_helper_init(const char* const logger_id,
                                logger_level_t level, bool enable_color);
 extern void logger_helper_destroy(const char* const logger_id);
