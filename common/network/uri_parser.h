88b63d2317b7e8207cdd421ae66fdff0703e3a31
Thibault Martinez <thibault@iota.org>
2018-08-20 22:32:07 +0200

    uri_parser: add C++ linkage guards

--- common/network/uri_parser.h
+++ common/network/uri_parser.h
@@ -12,7 +12,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool uri_parse(char const *const uri, char *const scheme, size_t scheme_len,
                char *const host, size_t host_len, uint16_t *const port);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // __COMMON_NETWORK_URI_PARSER_H__
