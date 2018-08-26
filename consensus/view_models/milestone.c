5b9b4b2fa7721b0f3874ad788f38656b77d77df7
Thibault Martinez <thibault@iota.org>
2018-08-26 18:49:00 +0200

    view_models: typo

--- consensus/view_models/milestone.c
+++ consensus/view_models/milestone.c
@@ -7,7 +7,7 @@
 
 #include "consensus/view_models/milestone.h"
 
-retcode_t iota_concencues_find_closest_next_milestone(
+retcode_t iota_consensus_find_closest_next_milestone(
     const tangle_t tangle, size_t index, bool testnet,
     size_t milestone_start_index, milestone_view_t closest_milestone_found) {
   return RC_CONSENSUS_NOT_IMPLEMENTED;
