619c7aa193aa0ae73717c8b7c8e795ffb6dec8a5
Thibault Martinez <thibault@iota.org>
2018-08-26 18:28:18 +0200

    utils: add useful macros file

---
+++ utils/macros.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#ifndef __UTILS_MACROS_H__
+#define __UTILS_MACROS_H__
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+#endif  // __UTILS_MACROS_H__
