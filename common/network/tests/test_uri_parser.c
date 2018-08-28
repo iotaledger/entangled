19318955093a5ef45b4b60fa62ca7051d9793dd6
YangHau <vulxj0j8j8@gmail.com>
2018-08-28 16:50:57 +0800

    Couple fixes

--- common/network/tests/test_uri_parser.c
+++ common/network/tests/test_uri_parser.c
@@ -1,43 +1,42 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
 #include <stdio.h>
 #include <unity/unity.h>
 #include "common/network/uri_parser.h"
 
 void test_uri_parser_ipv4(void) {
-  char *uri_ipv4 = "tcp://8.8.8.8:4242";
-  char *scheme;
+  char *uri_ipv4 = "tcp://8.8.8.8:4242"; 
   size_t scheme_len = 5;
-  char *host;
   size_t host_len = 17;
-  uint16_t *port;
-
-  scheme = (char *)malloc(sizeof(char) * scheme_len);
-  host = (char *)malloc(sizeof(char) * host_len);
-  port = (uint16_t *)malloc(sizeof(uint16_t));
+  char scheme[scheme_len];
+  char host[host_len];  
+  uint16_t port;
 
   TEST_ASSERT_TRUE(
-      uri_parse(uri_ipv4, scheme, scheme_len, host, host_len, port));
+      uri_parse(uri_ipv4, scheme, scheme_len, host, host_len, &port));
   TEST_ASSERT_EQUAL_STRING("tcp", scheme);
   TEST_ASSERT_EQUAL_STRING("8.8.8.8", host);
-  TEST_ASSERT_EQUAL_INT(4242, *port);
+  TEST_ASSERT_EQUAL_INT(4242, port);
 }
 
 void test_uri_parser_ipv6(void) {
   char *uri_ipv6 = "tcp://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:4242";
-  char *scheme;
   size_t scheme_len = 5;
-  char *host;
   size_t host_len = 40;
-  uint16_t *port;
-
-  scheme = (char *)malloc(sizeof(char) * scheme_len);
-  host = (char *)malloc(sizeof(char) * host_len);
-  port = (uint16_t *)malloc(sizeof(uint16_t));
+  char scheme[scheme_len];
+  char host[host_len];  
+  uint16_t port;
 
   TEST_ASSERT_TRUE(
-      uri_parse(uri_ipv6, scheme, scheme_len, host, host_len, port));
+      uri_parse(uri_ipv6, scheme, scheme_len, host, host_len, &port));
   TEST_ASSERT_EQUAL_STRING("tcp", scheme);
   TEST_ASSERT_EQUAL_STRING("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210", host);
-  TEST_ASSERT_EQUAL_INT(4242, *port);
+  TEST_ASSERT_EQUAL_INT(4242, port);
 }
 
 int main(void) {
--- common/network/uri_parser.c
+++ common/network/uri_parser.c
@@ -16,6 +16,10 @@ bool uri_parse(char const *const uri, char *const scheme,
   size_t length = 0;
   size_t offset = 0;
   char *delim = NULL;
+  enum URI_CASE {
+    ipv4,
+    ipv6
+  } uri_case;
 
   if (uri == NULL) {
     return false;
@@ -33,44 +37,35 @@ bool uri_parse(char const *const uri, char *const scheme,
   }
   offset += length;
   if (strncmp(uri + offset, "://[", 4) == 0) {
-    // IPv6
+    uri_case = ipv6;
     offset += 4;
     if ((delim = strstr(uri + offset, "]:")) == NULL) {
       return false;
     }
-    length = (size_t)(delim - (uri + offset));
-    if (length > MAX_HOST_LENGTH || length + 1 > host_len) {
-      return false;
-    }
-    if (host != NULL) {
-      memcpy(host, uri + offset, length);
-      host[length] = '\0';
-    }
-    offset += length + 2;
-    if (port != NULL) {
-      *port = atoi(uri + offset);
-    }
-    return true;
   } else if (strncmp(uri + offset, "://", 3) == 0) {
-    // IPv4
+    uri_case = ipv4;
     offset += 3;
     if ((delim = strchr(uri + offset, ':')) == NULL) {
       return false;
     }
-    length = (size_t)(delim - (uri + offset));
-    if (length > MAX_HOST_LENGTH || length + 1 > host_len) {
-      return false;
-    }
-    if (host != NULL) {
-      memcpy(host, uri + offset, length);
-      host[length] = '\0';
-    }
-    offset += length + 1;
-    if (port != NULL) {
-      *port = atoi(uri + offset);
-    }
-    return true;
   } else {
     return false;
   }
+  length = (size_t)(delim - (uri + offset));
+  if (length > MAX_HOST_LENGTH || length + 1 > host_len) {
+    return false;
+  }
+  if (host != NULL) {
+    memcpy(host, uri + offset, length);
+    host[length] = '\0';
+  }
+  if (uri_case == ipv6) {
+    offset += length + 2;
+  } else if (uri_case == ipv4) {
+    offset += length + 1;
+  }
+  if (port != NULL) {
+    *port = atoi(uri + offset);
+  }
+  return true;
 }
