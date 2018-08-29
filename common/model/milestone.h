ee7a758ed486a963f41d1a3fb39c375fa3e2b406
Thibault Martinez <thibault@iota.org>
2018-08-29 11:29:10 +0200

    milestone: change model to contain flex_trit[]

--- common/model/milestone.h
+++ common/model/milestone.h
@@ -8,12 +8,14 @@
 #ifndef __COMMON_MODEL_MILESTONE_H__
 #define __COMMON_MODEL_MILESTONE_H__
 
+#include "common/trinary/flex_trit.h"
+
 // Forward declarations
 typedef struct _trit_array *trit_array_p;
 
 typedef struct iota_milestone_s {
   uint64_t index;
-  trit_array_p hash;
+  flex_trit_t hash[FLEX_TRIT_SIZE_243];
 } iota_milestone_t;
 
 #endif  //  __COMMON_MODEL_MILESTONE_H__
