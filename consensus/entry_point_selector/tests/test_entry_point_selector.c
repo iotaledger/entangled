aa4f7f2b878c2bc98fd947dc1ab2de267ce6c5bc
Thibault Martinez <thibault@iota.org>
2018-08-31 17:39:39 +0200

    eps: use test definitions

--- consensus/entry_point_selector/tests/test_entry_point_selector.c
+++ consensus/entry_point_selector/tests/test_entry_point_selector.c
@@ -12,17 +12,48 @@
 #include "consensus/entry_point_selector/entry_point_selector.h"
 #include "consensus/milestone/milestone.h"
 #include "consensus/tangle/tangle.h"
+#include "consensus/test_definitions/defs.h"
 #include "utils/files.h"
 
 static entry_point_selector_t eps;
 static tangle_t tangle;
 static milestone_t milestone;
+connection_config_t config;
+
+// gdb --args ./test_cw_ratings_dfs 1
+static bool debug_mode = false;
 
 static char* test_db_path = "consensus/entry_point_selector/tests/test.db";
 static char* ciri_db_path = "consensus/entry_point_selector/tests/ciri.db";
 
-void test_eps_init(void) {
-  connection_config_t config;
+void test_entry_point_with_tangle_data() {
+  TEST_ASSERT(test_setup(&tangle, &config, test_db_path, ciri_db_path) ==
+              RC_OK);
+  TEST_ASSERT(iota_consensus_entry_point_selector_init(&eps, &milestone,
+                                                       &tangle, true) == RC_OK);
+  TEST_ASSERT(test_cleanup(&tangle, test_db_path) == RC_OK);
+  TEST_ASSERT(iota_consensus_entry_point_selector_destroy(&eps) == RC_OK);
+}
+
+void test_entry_point_without_tangle_data() {
+  TEST_ASSERT(test_setup(&tangle, &config, test_db_path, ciri_db_path) ==
+              RC_OK);
+  TEST_ASSERT(iota_consensus_entry_point_selector_init(&eps, &milestone,
+                                                       &tangle, true) == RC_OK);
+  TEST_ASSERT(test_cleanup(&tangle, test_db_path) == RC_OK);
+  TEST_ASSERT(iota_consensus_entry_point_selector_destroy(&eps) == RC_OK);
+}
+
+int main(int argc, char* argv[]) {
+  UNITY_BEGIN();
+
+  if (argc >= 2) {
+    debug_mode = true;
+  }
+  if (debug_mode) {
+    test_db_path = "test.db";
+    ciri_db_path = "ciri.db";
+  }
 
   config.db_path = test_db_path;
   config.index_transaction_address = true;
@@ -31,37 +62,6 @@ void test_eps_init(void) {
   config.index_transaction_tag = true;
   config.index_transaction_hash = true;
   config.index_milestone_hash = true;
-  TEST_ASSERT(iota_tangle_init(&tangle, &config) == RC_OK);
-  TEST_ASSERT(iota_consensus_entry_point_selector_init(&eps, &milestone,
-                                                       &tangle, true) == RC_OK);
-}
-
-void test_eps_destroy(void) {
-  TEST_ASSERT(iota_tangle_destroy(&tangle) == RC_OK);
-  TEST_ASSERT(iota_consensus_entry_point_selector_destroy(&eps) == RC_OK);
-}
-
-void test_setup() {
-  TEST_ASSERT(copy_file(test_db_path, ciri_db_path) == RC_OK);
-  RUN_TEST(test_eps_init);
-}
-void test_cleanup() {
-  TEST_ASSERT(remove_file(test_db_path) == RC_OK);
-  RUN_TEST(test_eps_destroy);
-}
-
-void test_entry_point_with_tangle_data() {
-  test_setup();
-  test_cleanup();
-}
-
-void test_entry_point_without_tangle_data() {
-  test_setup();
-  test_cleanup();
-}
-
-int main() {
-  UNITY_BEGIN();
 
   if (TEST_PROTECT()) {
     RUN_TEST(test_entry_point_with_tangle_data);
