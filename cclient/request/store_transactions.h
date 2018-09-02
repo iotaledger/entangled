2b28e3787f0cbb2d833ffa0270d8ee9aac4736c8
Zhen Wei <r06922135@csie.ntu.edu.tw>
2018-09-03 02:51:19 +0800

    cclient: implement iota_api_store_transactions (#220)

--- cclient/request/store_transactions.h
+++ cclient/request/store_transactions.h
@@ -16,6 +16,11 @@ extern "C" {
 // List of raw data of transactions to be store.
 typedef UT_array store_transactions_req_t;
 
+store_transactions_req_t* store_transactions_req_new();
+void store_transactions_req_add(store_transactions_req_t* transactions,
+                                const char* trytes);
+void store_transactions_req_free(store_transactions_req_t* transactions);
+
 #ifdef __cplusplus
 }
 #endif
