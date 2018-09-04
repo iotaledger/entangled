0eedb246e4848297f5310854329e1e5f835dce55
jkrvivian <jkrvivian@gmail.com>
2018-09-04 16:10:07 +0800

    Check malloc return

--- cclient/response/get_transactions_to_approve.c
+++ cclient/response/get_transactions_to_approve.c
@@ -11,6 +11,9 @@ get_transactions_to_approve_res_t* get_transactions_to_approve_res_new() {
   get_transactions_to_approve_res_t* res =
       (get_transactions_to_approve_res_t*)malloc(
           sizeof(get_transactions_to_approve_res_t));
+  if (res == NULL) {
+    return NULL;
+  }
   res->trunk = char_buffer_new();
   res->branch = char_buffer_new();
   return res;
