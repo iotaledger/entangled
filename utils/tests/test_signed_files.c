c18991426bc7c80dbf60464701f5b6b10f312b9c
Thibault Martinez <thibault@iota.org>
2018-09-14 16:16:08 +0200

    utils/signed_files: test base

---
+++ utils/tests/test_signed_files.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include <unity/unity.h>
+
+#include "utils/signed_files.h"
+
+void test_digest_file(void) {}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_digest_file);
+
+  return UNITY_END();
+}
