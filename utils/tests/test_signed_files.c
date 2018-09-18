6bbb05e88d0319527bc8839754cada04b995744f
Thibault Martinez <thibault@iota.org>
2018-09-18 11:50:20 +0200

    utils/signed_files: tests

--- utils/tests/test_signed_files.c
+++ utils/tests/test_signed_files.c
@@ -7,14 +7,32 @@
 
 #include <unity/unity.h>
 
+#include "consensus/snapshot/snapshot.h"
 #include "utils/signed_files.h"
 
-void test_digest_file(void) {}
+void test_good_signature(void) {
+  bool valid = false;
+  TEST_ASSERT(
+      is_file_signature_valid(MAINNET_SNAPSHOT_FILE, MAINNET_SNAPSHOT_SIG_FILE,
+                              (tryte_t *)SNAPSHOT_PUBKEY, SNAPSHOT_PUBKEY_DEPTH,
+                              SNAPSHOT_INDEX, &valid) == RC_OK);
+  TEST_ASSERT_TRUE(valid);
+}
+
+void test_bad_signature(void) {
+  bool valid = false;
+  TEST_ASSERT(
+      is_file_signature_valid(MAINNET_SNAPSHOT_FILE, TESTNET_SNAPSHOT_SIG_FILE,
+                              (tryte_t *)SNAPSHOT_PUBKEY, SNAPSHOT_PUBKEY_DEPTH,
+                              SNAPSHOT_INDEX, &valid) == RC_OK);
+  TEST_ASSERT_FALSE(valid);
+}
 
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_digest_file);
+  RUN_TEST(test_good_signature);
+  RUN_TEST(test_bad_signature);
 
   return UNITY_END();
 }
