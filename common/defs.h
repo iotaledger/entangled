8acb49aafa6b8dd6705390e7da682e114bad1675
Thibault Martinez <thibault@iota.org>
2018-10-05 12:51:06 +0200

    common/defs: comments to distinguish definition sections

--- common/defs.h
+++ common/defs.h
@@ -8,20 +8,27 @@
 #ifndef __COMMON_DEFS_H__
 #define __COMMON_DEFS_H__
 
-#define TRYTE_ALPHABET "9ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 #define RADIX 3
 
-#define NUMBER_OF_TRITS_IN_A_TRYTE 3
-#define NUMBER_OF_TRITS_IN_A_BYTE 5
+// Trytes related definitions
 
+#define TRYTE_ALPHABET "9ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #define TRYTE_VALUE_MAX 13
 #define TRYTE_VALUE_MIN -13
 #define TRYTE_WIDTH 3
 
+// Conversion definitions
+
+#define NUMBER_OF_TRITS_IN_A_TRYTE 3
+#define NUMBER_OF_TRITS_IN_A_BYTE 5
+
+// Hash length definitions
+
 #define HASH_LENGTH_BYTE 48
 #define HASH_LENGTH_TRYTE 81
 #define HASH_LENGTH_TRIT 243
+
+// IOTA Signing Scheme definitions
 
 #define SECURITY_LEVEL_MAX 3
 #define NORMALIZED_FRAGMENT_LENGTH 27
