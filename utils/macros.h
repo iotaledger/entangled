0886647a6c9d05a283a7f354b5047502e53cee10
tsvisabo <tsvi.sabo@iota.org>
2018-10-05 12:29:03 +0300

    utils/macros - use typeof rather than compiler specific

--- utils/macros.h
+++ utils/macros.h
@@ -8,18 +8,18 @@
 #ifndef __UTILS_MACROS_H__
 #define __UTILS_MACROS_H__
 
-#define MAX(a, b)           \
-  ({                        \
-    __typeof__(a) _a = (a); \
-    __typeof__(b) _b = (b); \
-    _a > _b ? _a : _b;      \
+#define MAX(a, b)       \
+  ({                    \
+    typeof(a) _a = (a); \
+    typeof(b) _b = (b); \
+    _a > _b ? _a : _b;  \
   })
 
-#define MIN(a, b)           \
-  ({                        \
-    __typeof__(a) _a = (a); \
-    __typeof__(b) _b = (b); \
-    _a < _b ? _a : _b;      \
+#define MIN(a, b)       \
+  ({                    \
+    typeof(a) _a = (a); \
+    typeof(b) _b = (b); \
+    _a < _b ? _a : _b;  \
   })
 
 #endif  // __UTILS_MACROS_H__
