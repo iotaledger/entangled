dbc8b45130d3243ca5f749e510f198e88dd4371d
Tsvi Sabo <tsvi.sabo@iota.org>
2018-10-05 13:38:50 +0300

    consensus/exit_probability_randomizer_test -  (#353)
    * consensus/exit_probability_randomizer_test - make test more conclusive and reduce numbers to make it faster
    
    * consensus/storage -  NULL TRYTE fields are of zero size and therefor retrieved from db as null
    
    * consensus/exit_probability_randomizer/tests: fix integer division bug
    
    * consensus/exit_probability_randomizer_test -  types, less strict results expected
    
    * utils/macros - use typeof rather than compiler specific
    
    * utils/macros - revert

--- consensus/exit_probability_randomizer/tests/test_exit_probability_randomizer.c
+++ consensus/exit_probability_randomizer/tests/test_exit_probability_randomizer.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <unity/unity.h>
 
+#include "math.h"
+
 #include "common/model/transaction.h"
 #include "common/storage/connection.h"
 #include "common/storage/sql/defs.h"
@@ -19,6 +21,7 @@
 #include "consensus/test_utils/bundle.h"
 #include "consensus/test_utils/tangle.h"
 #include "utarray.h"
+#include "utils/macros.h"
 
 static cw_rating_calculator_t calc;
 static tangle_t tangle;
@@ -43,7 +46,7 @@ typedef enum test_tangle_topology {
 void test_cw_gen_topology(test_tangle_topology topology) {
   cw_entry_t *curr_cw_entry = NULL;
   cw_entry_t *tmp_cw_entry = NULL;
-  size_t num_approvers = 150;
+  size_t num_approvers = 50;
   size_t num_txs = num_approvers + 1;
 
   TEST_ASSERT(tangle_setup(&tangle, &config, test_db_path, ciri_db_path) ==
@@ -122,30 +125,41 @@ void test_cw_gen_topology(test_tangle_topology topology) {
   TEST_ASSERT(iota_consensus_exit_prob_transaction_validator_init(
                   &tangle, NULL, NULL, &epv) == RC_OK);
   /// Select the tip
+  uint16_t selected_tip_counts[num_approvers];
+  memset(selected_tip_counts, 0, sizeof(selected_tip_counts));
 
-  size_t selected_tip_count = 0;
-  int selections = 1000;
+  int selections = 200;
   for (size_t i = 0; i < selections; ++i) {
     TEST_ASSERT(iota_consensus_exit_probability_randomize(
                     &ep_randomizer, &epv, &out, ep, &tip) == RC_OK);
-    if (memcmp(tip.trits, txs[num_approvers - 1].hash, FLEX_TRIT_SIZE_243) ==
-        0) {
-      selected_tip_count++;
+
+    for (size_t a = 0; a < num_approvers; ++a) {
+      if (memcmp(tip.trits, txs[a].hash, FLEX_TRIT_SIZE_243) == 0) {
+        selected_tip_counts[a] += 1;
+      }
     }
   }
 
+  uint16_t total_selections = 0;
   if (topology == ONLY_DIRECT_APPROVERS) {
     // We can look on the previous trial as a sample from
     // binomial distribution where `p` = 1/num_approvers, `n` = selections,
     // so we get (mean = `np`, stdev = `np*(1-p)`):
-    double expected_mean = selections / num_approvers;
-    double expected_stdev = sqrt(expected_mean * (1 - 1 / num_approvers));
-    TEST_ASSERT(selected_tip_count < expected_mean + 3 * expected_stdev);
-    TEST_ASSERT(selected_tip_count > expected_mean - 3 * expected_stdev);
-  } else if (topology == BLOCKCHAIN) {
-    // Sum of series 1 + 2 + ... + (num_txs)
-    TEST_ASSERT_EQUAL_INT(selections, selected_tip_count);
+    double expected_mean = ((double)selections / (double)num_approvers);
+    double expected_stdev =
+        sqrt(expected_mean * (1.0 - 1.0 / ((double)num_approvers)));
+    for (size_t a = 0; a < num_approvers; ++a) {
+      uint16_t comp_up = expected_mean + 5 * expected_stdev;
+      uint16_t comp_low = MAX((expected_mean - 5 * expected_stdev), 0);
+      TEST_ASSERT(selected_tip_counts[a] < comp_up);
+      TEST_ASSERT(selected_tip_counts[a] >= comp_low);
+    }
   }
+
+  for (size_t a = 0; a < num_approvers; ++a) {
+    total_selections += selected_tip_counts[a];
+  }
+  TEST_ASSERT(total_selections == selections);
 
   TEST_ASSERT(iota_consensus_ep_randomizer_destroy(&ep_randomizer) == RC_OK);
 
@@ -395,7 +409,7 @@ void test_cw_topology_two_inequal_tips(void) {
   /// Select the tip
 
   size_t selected_tip_count = 0;
-  int selections = 1000;
+  int selections = 200;
   for (size_t i = 0; i < selections; ++i) {
     TEST_ASSERT(iota_consensus_exit_probability_randomize(
                     &ep_randomizer, &epv, &out, ep, &tip) == RC_OK);
