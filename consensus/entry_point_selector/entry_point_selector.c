1e23407c35df6cf2313a5414465aa8bba6715c3a
Thibault Martinez <thibault.martinez.30@gmail.com>
2018-10-05 17:44:51 +0200

    Fix entry point selector (#318)
    * entry_point_selector: scope function name
    
    * entry_point_selector: add documentation
    
    * entry_point_selector: remove useless includes
    
    * entry_point_selector: fix milestone pack
    
    * entry_point_selector: fix milestone index
    
    * entry_point_selector: fix latest_solid_subtangle_milestone trits
    
    * entry_point_selector: test
    
    * entry_point_selector: fix PRIu64
    
    * entry_point_selector: struct init coding style
    
    * consensus/entry_point_selector: add missing cast
    
    * entry_point_selector: remove extern specifier

--- consensus/entry_point_selector/entry_point_selector.c
+++ consensus/entry_point_selector/entry_point_selector.c
@@ -5,10 +5,10 @@
  * Refer to the LICENSE file for licensing information
  */
 
-#include "consensus/entry_point_selector/entry_point_selector.h"
+#include <inttypes.h>
+
 #include "common/model/milestone.h"
-#include "common/storage/pack.h"
-#include "common/trinary/trit_array.h"
+#include "consensus/entry_point_selector/entry_point_selector.h"
 #include "consensus/milestone_tracker/milestone_tracker.h"
 #include "consensus/tangle/tangle.h"
 #include "utils/logger_helper.h"
@@ -26,30 +26,31 @@ retcode_t iota_consensus_entry_point_selector_init(
   return RC_OK;
 }
 
-retcode_t iota_consensus_get_entry_point(entry_point_selector_t *const eps,
-                                         size_t const depth,
-                                         trit_array_p const ep) {
+retcode_t iota_consensus_entry_point_selector_get_entry_point(
+    entry_point_selector_t *const eps, size_t const depth,
+    trit_array_p const ep) {
   retcode_t ret = RC_OK;
-  size_t milestone_index = MAX(eps->mt->latest_milestone_index - depth - 1, 0);
+  uint64_t milestone_index = MAX(
+      (int64_t)eps->mt->latest_solid_subtangle_milestone_index - depth - 1, 0);
   iota_milestone_t milestone;
-  iota_stor_pack_t pack = {.models = (void **)&milestone,
+  iota_milestone_t *milestone_ptr = &milestone;
+  iota_stor_pack_t pack = {.models = (void **)&milestone_ptr,
                            .capacity = 1,
                            .num_loaded = 0,
                            .insufficient_capacity = false};
 
   if ((ret = iota_tangle_milestone_load_next(eps->tangle, milestone_index,
                                              &pack))) {
-    log_error(
-        ENTRY_POINT_SELECTOR_LOGGER_ID,
-        "Finding closest next milestone failed with error %\" PRIu64 \"\n",
-        ret);
+    log_error(ENTRY_POINT_SELECTOR_LOGGER_ID,
+              "Finding closest next milestone failed with error %" PRIu64 "\n",
+              ret);
     return ret;
   }
 
   if (pack.num_loaded != 0) {
     memcpy(ep->trits, milestone.hash, FLEX_TRIT_SIZE_243);
   } else {
-    memcpy(ep->trits, eps->mt->latest_solid_subtangle_milestone,
+    memcpy(ep->trits, eps->mt->latest_solid_subtangle_milestone->trits,
            FLEX_TRIT_SIZE_243);
   }
 
