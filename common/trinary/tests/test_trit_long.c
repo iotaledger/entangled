f7208f77cbfddf1f93056df8ce639c65f7146d02
Emmanuel Merali <emmanuel@merali.me>
2018-10-09 23:08:05 +0300

    common/trinary: fix long_to_trits fails for very large integer values (issue #375)

--- common/trinary/tests/test_trit_long.c
+++ common/trinary/tests/test_trit_long.c
@@ -16,20 +16,74 @@
   1, 0, -1, -1, 0, -1, 1, 0, -1, 1, -1, 0, 1, 0, 0, -1, 1, 1, 0
 #define TRITS_IN_ENC_LEN 19
 
+#define MAX_LONG_EXP 0x7FFFFFFFFFFFFFFF
+#define MAX_TRITS_IN                                                           \
+  1, -1, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0, -1, 0, 1, 0, 1, 0, -1, 1, 1, -1, -1, \
+      1, 0, 1, -1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, -1, 1, -1, 1
+#define MAX_TRITS_IN_LEN 41
+
+#define MIN_LONG_EXP 0x8000000000000000
+#define MIN_TRITS_IN                                                          \
+  1, 0, 0, -1, 1, 0, 1, -1, -1, 0, 1, 0, 1, 0, -1, 0, -1, 0, 1, -1, -1, 1, 1, \
+      -1, 0, -1, 1, 0, 0, -1, -1, -1, 0, 0, -1, -1, -1, 1, -1, 1, -1
+#define MIN_TRITS_IN_LEN 41
+
 void test_min_trits(void) {
   trit_t trits[] = {TRITS_IN};
   size_t trits_size = sizeof(trits) / sizeof(trit_t);
 
   TEST_ASSERT_EQUAL_INT(trits_size, min_trits(LONG_EXP));
+  TEST_ASSERT_EQUAL_INT(MAX_TRITS_IN_LEN, min_trits(MAX_LONG_EXP));
+  TEST_ASSERT_EQUAL_INT(MIN_TRITS_IN_LEN, min_trits(MIN_LONG_EXP));
 }
 
 void test_trit_to_long(void) {
   trit_t trits[] = {TRITS_IN};
   size_t trits_size = sizeof(trits) / sizeof(trit_t);
-  long long exp = LONG_EXP;
-  long long val = trits_to_long(trits, trits_size);
+  int64_t exp = LONG_EXP;
+  int64_t val = trits_to_long(trits, trits_size);
 
   TEST_ASSERT_EQUAL_INT64(exp, val);
+}
+
+void test_trit_to_max_long(void) {
+  trit_t trits[] = {MAX_TRITS_IN};
+  size_t trits_size = sizeof(trits) / sizeof(trit_t);
+  int64_t exp = MAX_LONG_EXP;
+  int64_t val = trits_to_long(trits, trits_size);
+
+  TEST_ASSERT_EQUAL_INT64(exp, val);
+}
+
+void test_trit_to_min_long(void) {
+  trit_t trits[] = {MIN_TRITS_IN};
+  size_t trits_size = sizeof(trits) / sizeof(trit_t);
+  int64_t exp = MIN_LONG_EXP;
+  int64_t val = trits_to_long(trits, trits_size);
+
+  TEST_ASSERT_EQUAL_INT64(exp, val);
+}
+
+void test_max_long_to_trits(void) {
+  size_t trits_size = min_trits(MAX_LONG_EXP);
+  TEST_ASSERT_EQUAL_INT(MAX_TRITS_IN_LEN, trits_size);
+
+  trit_t trits[trits_size];
+  trit_t exp[] = {MAX_TRITS_IN};
+  long_to_trits(MAX_LONG_EXP, trits);
+
+  TEST_ASSERT_EQUAL_MEMORY(exp, trits, sizeof(trits));
+}
+
+void test_min_long_to_trits(void) {
+  size_t trits_size = min_trits(MIN_LONG_EXP);
+  TEST_ASSERT_EQUAL_INT(MIN_TRITS_IN_LEN, trits_size);
+
+  trit_t trits[trits_size];
+  trit_t exp[] = {MIN_TRITS_IN};
+  long_to_trits(MIN_LONG_EXP, trits);
+
+  TEST_ASSERT_EQUAL_MEMORY(exp, trits, sizeof(trits));
 }
 
 void test_from_long(void) {
@@ -70,8 +124,14 @@ int main(void) {
   UNITY_BEGIN();
 
   RUN_TEST(test_min_trits);
+
   RUN_TEST(test_trit_to_long);
+  RUN_TEST(test_trit_to_max_long);
+  RUN_TEST(test_trit_to_min_long);
+
   RUN_TEST(test_from_long);
+  RUN_TEST(test_max_long_to_trits);
+  RUN_TEST(test_min_long_to_trits);
 
   RUN_TEST(test_encoded_long_length);
   RUN_TEST(test_encoded_long);
--- common/trinary/trit_long.h
+++ common/trinary/trit_long.h
@@ -14,16 +14,44 @@ extern "C" {
 
 #include "common/trinary/trits.h"
 
-int64_t trits_to_long(trit_t const *const trit, size_t const i);
+/// Returns the number of trits needed to encode the value
+/// @return size_t - the number of trit needed to encode the value
 size_t min_trits(int64_t const value);
+
+/// Convert an array of trits to an integer value
+/// @param[in] trits - an array of trits
+/// @param[in] num_trits - the number of trits in the array
+/// @return int64_t - the value encoded in the array of trits
+int64_t trits_to_long(trit_t const *const trits, size_t const num_trits);
+/// Convert an integer value to an array of trits
+/// @param[in] value - an integer value
+/// @param[in] trits - an array of trits
+/// @return size_t - the number of trits needed to encode the value
 size_t long_to_trits(int64_t const value, trit_t *const trits);
 
+/// Returns the nearest multiple of 3 of the value, rounded up
+/// @param[in] value - an integer value
+/// @return size_t - the nearest multiple of 3 of the value
 size_t nearest_greater_multiple_of_three(size_t const value);
 
+/// Returns the number of trits needed for encoding of a value
+/// @return size_t - the number of trit needed for encoding
 size_t encoded_length(int64_t const value);
-int encode_long(int64_t const value, trit_t *const trits, size_t const size);
-int64_t decode_long(trit_t const *const trits, size_t const length,
-                    size_t *const end);
+
+/// Encode a value (for MAM)
+/// @param[in] value - an integer value
+/// @param[in] trits - an array of trits
+/// @param[in] num_trits - the number of trits in the array
+/// @return int - return 0 on succes, -1 on failure
+int encode_long(int64_t const value, trit_t *const trits,
+                size_t const num_trits);
+/// Decode a value (for MAM)
+/// @param[in] trits - an array of trits
+/// @param[in] num_trits - the number of trits in the array
+/// @param[in/out] size - the size of the encoded data
+/// @return int64_t - the value decoded from the array of trits
+int64_t decode_long(trit_t const *const trits, size_t const num_trits,
+                    size_t *const size);
 
 #endif
 #ifdef __cplusplus
