33d2b4aec20ec51b38a51856a8ca9021de791a52
Thibault Martinez <thibault@iota.org>
2018-10-11 13:56:27 +0200

    consensus: clean snapshot

--- consensus/consensus.c
+++ consensus/consensus.c
@@ -81,19 +81,12 @@ retcode_t iota_consensus_init(iota_consensus_t *const consensus,
   }
 
   log_info(CONSENSUS_LOGGER_ID, "Initializing snapshot\n");
-  char *snapshot_file = NULL;
-  char *snapshot_sig_file = NULL;
-  if (core->config.testnet) {
-    snapshot_file = SNAPSHOT_TESTNET;
-    snapshot_sig_file = SNAPSHOT_SIG_TESTNET;
-    snapshot_conf_file = SNAPSHOT_CONF_TESTNET;
-  } else {
-    snapshot_file = SNAPSHOT_MAINNET;
-    snapshot_sig_file = SNAPSHOT_SIG_MAINNET;
-    snapshot_conf_file = SNAPSHOT_CONF_MAINNET;
-  }
-  if ((ret = iota_snapshot_init(&consensus->snapshot, CIRI_SNAPSHOT_FILE,
-                                CIRI_SNAPSHOT_SIG_FILE, testnet)) != RC_OK) {
+  if ((ret = iota_snapshot_init(
+           &consensus->snapshot,
+           (testnet ? SNAPSHOT_TESTNET : SNAPSHOT_MAINNET),
+           (testnet ? SNAPSHOT_SIG_TESTNET : SNAPSHOT_SIG_MAINNET),
+           (testnet ? SNAPSHOT_CONF_TESTNET : SNAPSHOT_CONF_MAINNET),
+           testnet)) != RC_OK) {
     log_critical(CONSENSUS_LOGGER_ID, "Initializing snapshot failed\n");
     return ret;
   }
@@ -117,7 +110,8 @@ retcode_t iota_consensus_init(iota_consensus_t *const consensus,
 
   log_info(CONSENSUS_LOGGER_ID, "Initializing transaction validator\n");
   if ((ret = iota_consensus_transaction_validator_init(
-           &consensus->transaction_validator, SNAPSHOT_TIMESTAMP * 1000,
+           &consensus->transaction_validator,
+           consensus->snapshot.conf.timestamp * 1000,
            (testnet ? TESTNET_MWM : MAINNET_MWM))) != RC_OK) {
     log_critical(CONSENSUS_LOGGER_ID,
                  "Initializing transaction validator failed\n");
