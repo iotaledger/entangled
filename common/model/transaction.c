1eca64d3c0162388465c80b0230d0855bfda79e4
Thibault Martinez <thibault@iota.org>
2018-10-19 19:41:09 +0200

    flex_trit: remove duplicate definitions

--- common/model/transaction.c
+++ common/model/transaction.c
@@ -426,16 +426,16 @@ uint8_t transaction_weight_magnitude(const iota_transaction_t transaction) {
   uint8_t pos = FLEX_TRIT_SIZE_243;
 
   while (pos-- > 0 && transaction->hash[pos] == FLEX_TRIT_NULL_VALUE) {
-    num_trailing_null_values += NUM_TRITS_FOR_FLEX_TRIT;
+    num_trailing_null_values += NUM_TRITS_PER_FLEX_TRIT;
   }
 
   if (pos > 0) {
-    trit_t one_trit_buffer[NUM_TRITS_FOR_FLEX_TRIT];
-    flex_trits_to_trits(one_trit_buffer, NUM_TRITS_FOR_FLEX_TRIT,
-                        &transaction->hash[pos], NUM_TRITS_FOR_FLEX_TRIT,
-                        NUM_TRITS_FOR_FLEX_TRIT);
+    trit_t one_trit_buffer[NUM_TRITS_PER_FLEX_TRIT];
+    flex_trits_to_trits(one_trit_buffer, NUM_TRITS_PER_FLEX_TRIT,
+                        &transaction->hash[pos], NUM_TRITS_PER_FLEX_TRIT,
+                        NUM_TRITS_PER_FLEX_TRIT);
 
-    pos = NUM_TRITS_FOR_FLEX_TRIT;
+    pos = NUM_TRITS_PER_FLEX_TRIT;
     while (pos-- > 0 && one_trit_buffer[pos] == 0) {
       ++num_trailing_null_values;
     }
--- consensus/bundle_validator/tests/bundle_validator.c
+++ consensus/bundle_validator/tests/bundle_validator.c
@@ -242,14 +242,14 @@ void test_iota_consensus_bundle_validator_validate_size_4_value_wrong_sig_invali
       TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
       TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
   transactions_deserialize(trytes, txs, 4);
-  trit_t buffer[NUM_TRITS_FOR_FLEX_TRIT];
-  flex_trits_to_trits(buffer, NUM_TRITS_FOR_FLEX_TRIT,
-                      txs[1]->signature_or_message, NUM_TRITS_FOR_FLEX_TRIT,
-                      NUM_TRITS_FOR_FLEX_TRIT);
-  buffer[NUM_TRITS_FOR_FLEX_TRIT - 1] = 1;
+  trit_t buffer[NUM_TRITS_PER_FLEX_TRIT];
+  flex_trits_to_trits(buffer, NUM_TRITS_PER_FLEX_TRIT,
+                      txs[1]->signature_or_message, NUM_TRITS_PER_FLEX_TRIT,
+                      NUM_TRITS_PER_FLEX_TRIT);
+  buffer[NUM_TRITS_PER_FLEX_TRIT - 1] = 1;
   flex_trits_from_trits(&txs[1]->signature_or_message[FLEX_TRIT_SIZE_243 - 1],
-                        NUM_TRITS_FOR_FLEX_TRIT, buffer,
-                        NUM_TRITS_FOR_FLEX_TRIT, NUM_TRITS_FOR_FLEX_TRIT);
+                        NUM_TRITS_PER_FLEX_TRIT, buffer,
+                        NUM_TRITS_PER_FLEX_TRIT, NUM_TRITS_PER_FLEX_TRIT);
   trit_array_p tail_hash = trit_array_new(NUM_TRITS_HASH);
   trit_array_set_trits(tail_hash, txs[0]->hash, NUM_TRITS_HASH);
   build_tangle(&tangle, txs, 4);
