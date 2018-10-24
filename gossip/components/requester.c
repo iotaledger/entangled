94e7e87c9f458b11ecdf185d71dc30e45a2b8a2d
Thibault Martinez <thibault@iota.org>
2018-10-24 18:10:26 +0200

    requester: refactoring

--- gossip/components/requester.c
+++ gossip/components/requester.c
@@ -8,7 +8,6 @@
 #include "gossip/components/requester.h"
 #include "common/storage/sql/defs.h"
 #include "consensus/tangle/tangle.h"
-#include "utils/containers/lists/concurrent_list_trit_array.h"
 #include "utils/logger_helper.h"
 
 // TODO(thibault) configuration variable
@@ -17,136 +16,184 @@
 
 retcode_t requester_init(requester_state_t *const state,
                          tangle_t *const tangle) {
-  if (state == NULL) {
+  if (state == NULL || tangle == NULL) {
     return RC_REQUESTER_COMPONENT_NULL_STATE;
-  } else if (tangle == NULL) {
-    return RC_REQUESTER_COMPONENT_NULL_TANGLE;
   }
 
   logger_helper_init(REQUESTER_COMPONENT_LOGGER_ID, LOGGER_DEBUG, true);
   memset(state, 0, sizeof(requester_state_t));
+  state->milestones = NULL;
+  state->transactions = NULL;
   state->tangle = tangle;
+  rw_lock_handle_init(&state->lock);
 
-  log_debug(REQUESTER_COMPONENT_LOGGER_ID, "Initializing requester list\n");
-  if (CL_INIT(trit_array_p, state->list, trit_array_cmp) != CL_SUCCESS) {
-    log_critical(REQUESTER_COMPONENT_LOGGER_ID,
-                 "Initializing requester list failed\n");
-    return RC_REQUESTER_COMPONENT_FAILED_INIT_LIST;
-  }
   return RC_OK;
+}
+
+retcode_t requester_destroy(requester_state_t *const state) {
+  if (state == NULL) {
+    return RC_REQUESTER_COMPONENT_NULL_STATE;
+  }
+
+  hash243_set_free(&state->milestones);
+  hash243_set_free(&state->transactions);
+  state->tangle = NULL;
+  rw_lock_handle_destroy(&state->lock);
+  logger_helper_destroy(REQUESTER_COMPONENT_LOGGER_ID);
+
+  return RC_OK;
+}
+
+retcode_t requester_get_requested_transactions(
+    requester_state_t *const state, hash243_set_t *const transactions) {
+  retcode_t ret = RC_OK;
+
+  if (state == NULL || transactions == NULL) {
+    return RC_REQUESTER_COMPONENT_NULL_STATE;
+  }
+
+  rw_lock_handle_rdlock(&state->lock);
+
+  if ((ret = hash243_set_append(&state->transactions, transactions)) != RC_OK) {
+    goto done;
+  }
+  if ((ret = hash243_set_append(&state->milestones, transactions)) != RC_OK) {
+    goto done;
+  }
+
+done:
+  rw_lock_handle_unlock(&state->lock);
+  return ret;
 }
 
 size_t requester_size(requester_state_t *const state) {
-  if (state == NULL) {
-    return 0;
-  }
+  size_t size = 0;
 
-  return CL_SIZE(state->list);
-}
-
-retcode_t requester_clear_request(requester_state_t *const state,
-                                  trit_array_p const hash) {
   if (state == NULL) {
     return RC_REQUESTER_COMPONENT_NULL_STATE;
-  } else if (hash == NULL) {
-    return RC_REQUESTER_COMPONENT_NULL_HASH;
   }
 
-  if (CL_REMOVE(state->list, hash) != CL_SUCCESS) {
-    log_warning(REQUESTER_COMPONENT_LOGGER_ID,
-                "Clearing request from list failed\n");
-    return RC_REQUESTER_COMPONENT_FAILED_REMOVE_LIST;
-  }
-  return RC_OK;
+  rw_lock_handle_rdlock(&state->lock);
+  size = hash243_set_size(&state->transactions) +
+         hash243_set_size(&state->milestones);
+  rw_lock_handle_unlock(&state->lock);
+
+  return size;
 }
 
 bool requester_is_full(requester_state_t *const state) {
+  size_t size = 0;
+
   if (state == NULL) {
     return RC_REQUESTER_COMPONENT_NULL_STATE;
   }
 
-  return CL_SIZE(state->list) >= MAX_TX_REQ_NBR;
+  rw_lock_handle_rdlock(&state->lock);
+  size = hash243_set_size(&state->transactions);
+  rw_lock_handle_unlock(&state->lock);
+
+  return size >= MAX_TX_REQ_NBR;
+}
+
+retcode_t requester_clear_request(requester_state_t *const state,
+                                  flex_trit_t const *const hash) {
+  if (state == NULL || hash == NULL) {
+    return RC_REQUESTER_COMPONENT_NULL_STATE;
+  }
+
+  rw_lock_handle_wrlock(&state->lock);
+  // TODO REMOVE
+  rw_lock_handle_unlock(&state->lock);
+
+  return RC_OK;
 }
 
 retcode_t request_transaction(requester_state_t *const state,
-                              trit_array_p const hash) {
+                              flex_trit_t *const hash,
+                              bool const is_milestone) {
   retcode_t ret = RC_OK;
   bool exists = false;
+  trit_array_t key = {.trits = hash,
+                      .num_trits = HASH_LENGTH_TRIT,
+                      .num_bytes = FLEX_TRIT_SIZE_243,
+                      .dynamic = 0};
 
-  if (state == NULL) {
+  if (state == NULL || hash == NULL) {
     return RC_REQUESTER_COMPONENT_NULL_STATE;
-  } else if (hash == NULL) {
-    return RC_REQUESTER_COMPONENT_NULL_HASH;
   }
 
-  // TODO(thibault) check null hash
+  //  TODO CHECK NULL HASH
   if ((ret = iota_tangle_transaction_exist(
-           state->tangle, TRANSACTION_FIELD_HASH, hash, &exists))) {
+           state->tangle, TRANSACTION_FIELD_HASH, &key, &exists)) != RC_OK) {
     return ret;
   }
   if (exists) {
     return RC_OK;
   }
-  if (CL_CONTAINS(state->list, hash) == false) {
-    if (requester_is_full(state) == false) {
-      if (CL_PUSH_BACK(state->list, hash) != CL_SUCCESS) {
-        log_warning(REQUESTER_COMPONENT_LOGGER_ID,
-                    "Adding new transaction request to the list failed\n");
-        return RC_REQUESTER_COMPONENT_FAILED_ADD_LIST;
-      }
-    } else {
-      log_warning(REQUESTER_COMPONENT_LOGGER_ID,
-                  "Transactions requests list is full\n");
+
+  rw_lock_handle_wrlock(&state->lock);
+
+  if (is_milestone) {
+    // TODO remove hash from transactions set
+    if ((ret = hash243_set_add(&state->milestones, hash)) != RC_OK) {
+      goto done;
+    }
+  } else if (!hash243_set_contains(&state->milestones, hash) &&
+             hash243_set_size(&state->transactions) < MAX_TX_REQ_NBR) {
+    if ((ret = hash243_set_add(&state->transactions, hash)) != RC_OK) {
+      goto done;
     }
   }
-  return RC_OK;
+
+done:
+  rw_lock_handle_unlock(&state->lock);
+  return ret;
 }
 
 retcode_t get_transaction_to_request(requester_state_t *const state,
-                                     trit_array_p *hash) {
+                                     flex_trit_t *const hash,
+                                     bool const milestone) {
   retcode_t ret = RC_OK;
-  concurrent_list_node_trit_array_p *iter = NULL;
+  hash243_set_t request_set = NULL;
+  hash243_set_t backup_set = NULL;
+  hash243_set_entry_t *iter = NULL;
+  hash243_set_entry_t *tmp = NULL;
+  trit_array_t key = {.trits = hash,
+                      .num_trits = HASH_LENGTH_TRIT,
+                      .num_bytes = FLEX_TRIT_SIZE_243,
+                      .dynamic = 0};
   bool exists = false;
 
-  if (state == NULL) {
+  if (state == NULL || hash == NULL) {
     return RC_REQUESTER_COMPONENT_NULL_STATE;
-  } else if (hash == NULL) {
-    return RC_REQUESTER_COMPONENT_NULL_HASH;
   }
 
-  iter = state->list->front;
-  while (iter) {
-    *hash = iter->data;
-    iter = iter->next;
+  request_set = milestone ? state->milestones : state->transactions;
+  backup_set = milestone ? state->transactions : state->milestones;
+
+  rw_lock_handle_wrlock(&state->lock);
+
+  request_set = hash243_set_size(&request_set) != 0 ? request_set : backup_set;
+
+  HASH_ITER(hh, request_set, iter, tmp) {
+    key.trits = iter->hash;
     if ((ret = iota_tangle_transaction_exist(
-             state->tangle, TRANSACTION_FIELD_HASH, *hash, &exists))) {
-      return ret;
+             state->tangle, TRANSACTION_FIELD_HASH, &key, &exists))) {
+      goto done;
     }
     if (exists) {
-      requester_clear_request(state, *hash);
+      // TODO REMOVE
     } else {
       break;
     }
   }
-  if (iter == NULL) {
-    *hash = NULL;
-  }
-  return RC_OK;
-}
-
-retcode_t requester_destroy(requester_state_t *const state) {
-  retcode_t ret = RC_OK;
-
-  if (state == NULL) {
-    return RC_REQUESTER_COMPONENT_NULL_STATE;
+  if (hash243_set_size(&request_set) == 0) {
+    memset(hash, FLEX_TRIT_NULL_VALUE, FLEX_TRIT_SIZE_243);
+  } else {
+    memcpy(hash, iter->hash, FLEX_TRIT_SIZE_243);
   }
 
-  log_debug(REQUESTER_COMPONENT_LOGGER_ID, "Destroying requester list\n");
-  if (CL_DESTROY(trit_array_p, state->list) != CL_SUCCESS) {
-    log_error(REQUESTER_COMPONENT_LOGGER_ID,
-              "Destroying requester list failed\n");
-    ret = RC_REQUESTER_COMPONENT_FAILED_DESTROY_LIST;
-  }
-  logger_helper_destroy(REQUESTER_COMPONENT_LOGGER_ID);
+done:
+  rw_lock_handle_unlock(&state->lock);
   return ret;
 }
--- gossip/components/requester.h
+++ gossip/components/requester.h
@@ -12,15 +12,17 @@
 #include <stdlib.h>
 
 #include "common/errors.h"
+#include "utils/containers/hash/hash243_set.h"
+#include "utils/handles/rw_lock.h"
 
 // Forward declarations
-typedef struct concurrent_list_trit_array_p_s requester_list_t;
-typedef struct _trit_array *trit_array_p;
 typedef struct tangle_s tangle_t;
 
 typedef struct requester_state_s {
-  requester_list_t *list;
+  hash243_set_t milestones;
+  hash243_set_t transactions;
   tangle_t *tangle;
+  rw_lock_handle_t lock;
 } requester_state_t;
 
 #ifdef __cplusplus
@@ -39,6 +41,26 @@ retcode_t requester_init(requester_state_t *const state,
                          tangle_t *const tangle);
 
 /**
+ * Destroys a requester
+ *
+ * @param state The requester state
+ *
+ * @return a status code
+ */
+retcode_t requester_destroy(requester_state_t *const state);
+
+/**
+ * Get all the requested transactions and milestones
+ *
+ * @param state The requester state
+ * @param transactions The set of transactions to be filled
+ *
+ * @return a status code
+ */
+retcode_t requester_get_requested_transactions(
+    requester_state_t *const state, hash243_set_t *const transactions);
+
+/**
  * Gets the number of transactions to request
  *
  * @param state The requester state
@@ -46,17 +68,6 @@ retcode_t requester_init(requester_state_t *const state,
  * @return The number of transactions to request
  */
 size_t requester_size(requester_state_t *const state);
-
-/**
- * Cancels a request for a transaction
- *
- * @param state The requester state
- * @param hash The transaction request to cancel
- *
- * @return a status code
- */
-retcode_t requester_clear_request(requester_state_t *const state,
-                                  trit_array_p const hash);
 
 /**
  * Tells whether the requester is full or not
@@ -68,35 +79,40 @@ retcode_t requester_clear_request(requester_state_t *const state,
 bool requester_is_full(requester_state_t *const state);
 
 /**
+ * Cancels a request for a transaction
+ *
+ * @param state The requester state
+ * @param hash The transaction request to cancel
+ *
+ * @return a status code
+ */
+retcode_t requester_clear_request(requester_state_t *const state,
+                                  flex_trit_t const *const hash);
+
+/**
  * Adds a transaction to be requested
  *
  * @param state The requester state
  * @param hash The transaction to request
+ * @param is_milestone Whether the hash is a milestone or not
  *
  * @return a status code
  */
 retcode_t request_transaction(requester_state_t *const state,
-                              trit_array_p const hash);
+                              flex_trit_t *const hash, bool const is_milestone);
 
 /**
  * Gets a transaction to request
  *
  * @param state The requester state
  * @param hash The transaction to be requested
+ * @param milestone Whether to request a milestone or not
  *
  * @return a status code
  */
 retcode_t get_transaction_to_request(requester_state_t *const state,
-                                     trit_array_p *hash);
-
-/**
- * Destroys a requester
- *
- * @param state The requester state
- *
- * @return a status code
- */
-retcode_t requester_destroy(requester_state_t *const state);
+                                     flex_trit_t *const hash,
+                                     bool const milestone);
 
 #ifdef __cplusplus
 }
