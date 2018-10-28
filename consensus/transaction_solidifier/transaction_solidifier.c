1c92eaed2b6975f0a758d69cb20729de6c8fa24d
Tsvi Sabo <tsvi.sabo@iota.org>
2018-10-28 21:56:02 +0200

    consensus/transaction_solidifier - (#446)
    * consensus/transaction_solidifier - remove redundant db update, add new solid to set
    
    * consensus/transaction_solidifier - refactor

--- consensus/transaction_solidifier/transaction_solidifier.c
+++ consensus/transaction_solidifier/transaction_solidifier.c
@@ -69,12 +69,12 @@ static retcode_t propagate_solid_transactions(
   retcode_t ret;
   hash243_set_t transactions_to_propagate = NULL;
   lock_handle_lock(&ts->lock);
-  if ((ret = hash243_set_for_each(&ts->newly_set_solid_transactions,
-                                  hash243_set_add,
-                                  (void *)&transactions_to_propagate))) {
+  ret = hash243_set_append(&ts->newly_set_solid_transactions,
+                           &transactions_to_propagate);
+  lock_handle_unlock(&ts->lock);
+  if (ret) {
     return ret;
   }
-  lock_handle_unlock(&ts->lock);
 
   iota_stor_pack_t hash_pack;
   if ((ret = hash_pack_init(&hash_pack, 10)) != RC_OK) {
@@ -85,7 +85,6 @@ static retcode_t propagate_solid_transactions(
   hash243_set_entry_t *tmp_entry = NULL;
   flex_trit_t *curr_approver_hash;
   uint16_t approver_index;
-  bool is_approver_solid;
   HASH_ITER(hh, transactions_to_propagate, curr_entry, tmp_entry) {
     if (!ts->running) {
       break;
@@ -99,18 +98,13 @@ static retcode_t propagate_solid_transactions(
     for (approver_index = 0; approver_index < hash_pack.num_loaded;
          ++approver_index) {
       curr_approver_hash = (flex_trit_t *)hash_pack.models[approver_index];
-      if ((ret = check_transaction_and_update_solid_state(
-               ts, curr_approver_hash, &is_approver_solid)) != RC_OK) {
+      if ((ret =
+               iota_consensus_transaction_solidifier_check_and_update_solid_state(
+                   ts, curr_approver_hash)) != RC_OK) {
         log_error(TRANSACTION_SOLIDIFIER_LOGGER_ID,
                   "In %s, failed check_transaction_and_update_solid_state\n",
                   __FUNCTION__);
         return ret;
-      }
-      if (is_approver_solid) {
-        if ((ret = iota_tangle_transaction_update_solid_state(
-                 ts->tangle, curr_approver_hash, true)) != RC_OK) {
-          return ret;
-        }
       }
     }
   }
@@ -245,10 +239,14 @@ retcode_t iota_consensus_transaction_solidifier_check_solidity(
     return RC_OK;
   }
 
+  lock_handle_lock(&ts->lock);
   check_solidity_do_func_params_t params = {
       .ts = ts, .is_milestone = is_milestone, .is_solid = true};
-  ret = tangle_traversal_dfs_to_genesis(ts->tangle, check_solidity_do_func,
-                                        hash, &params);
+
+  if ((ret = tangle_traversal_dfs_to_genesis(ts->tangle, check_solidity_do_func,
+                                             hash, &params)) != RC_OK) {
+    goto done;
+  }
   if (params.is_solid) {
     log_debug(TRANSACTION_SOLIDIFIER_LOGGER_ID, "In %s, updating solid state\n",
               __FUNCTION__);
@@ -258,11 +256,15 @@ retcode_t iota_consensus_transaction_solidifier_check_solidity(
     HASH_ITER(hh, ts->solid_transactions_candidates, curr_entry, tmp_entry) {
       if ((ret = iota_tangle_transaction_update_solid_state(
                ts->tangle, curr_entry->hash, true)) != RC_OK) {
-        return ret;
+        goto done;
       }
     }
+    hash243_set_append(&ts->solid_transactions_candidates,
+                       &ts->newly_set_solid_transactions);
   }
+done:
   hash243_set_free(&ts->solid_transactions_candidates);
+  lock_handle_unlock(&ts->lock);
   return ret;
 }
 
