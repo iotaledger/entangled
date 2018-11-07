ec15c796b1cb3c08d3cefaf4bea6b3abf02b5646
Thibault Martinez <thibault@iota.org>
2018-11-07 15:59:56 +0100

    requester: do not randomly remove milestone requests

--- gossip/components/transaction_requester.c
+++ gossip/components/transaction_requester.c
@@ -200,7 +200,8 @@ retcode_t get_transaction_to_request(requester_state_t *const state,
     memset(hash, FLEX_TRIT_NULL_VALUE, FLEX_TRIT_SIZE_243);
   }
 
-  if (rand_handle_probability() < state->conf->p_remove_request) {
+  if (rand_handle_probability() < state->conf->p_remove_request &&
+      request_set != &state->milestones) {
     hash243_set_remove(request_set, iter->hash);
   }
 
