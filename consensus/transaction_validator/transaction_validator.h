76027df6e2f9db5cd9f52a0d875ebca9a514a5b5
Thibault Martinez <thibault@iota.org>
2019-01-02 14:17:46 -0500

    consensus/transaction_validator: unsigned timestamp, clean and documentation

--- consensus/transaction_validator/transaction_validator.h
+++ consensus/transaction_validator/transaction_validator.h
@@ -9,7 +9,6 @@
 #define __CONSENSUS_TRANSACTION_VALIDATOR_TRANSACTION_VALIDATOR_H__
 
 #include <stdbool.h>
-#include <stdint.h>
 
 #include "common/errors.h"
 #include "common/model/transaction.h"
@@ -19,18 +18,50 @@
 extern "C" {
 #endif
 
-typedef struct transaction_validator_t {
+/**
+ * Structure used to check transaction validity
+ */
+typedef struct transaction_validator_s {
   iota_consensus_conf_t *conf;
 } transaction_validator_t;
 
-extern retcode_t iota_consensus_transaction_validator_init(
+/**
+ * Initializes a transaction validator
+ *
+ * @param tv The transaction validator
+ * @param conf A consensus configuration
+ *
+ * @return a status code
+ */
+retcode_t iota_consensus_transaction_validator_init(
     transaction_validator_t *const tv, iota_consensus_conf_t *const conf);
 
-extern retcode_t iota_consensus_transaction_validator_destroy(
+/**
+ * Destroys a transaction validator
+ *
+ * @param tv The transaction validator
+ *
+ * @return a status code
+ */
+retcode_t iota_consensus_transaction_validator_destroy(
     transaction_validator_t *const tv);
 
-extern bool iota_consensus_transaction_validate(
-    transaction_validator_t *const tv, iota_transaction_t *const transaction);
+/**
+ * Runs the following validation checks on a transaction:
+ * - Sufficient PoW was performed
+ * - Timestamps are above the last global snapshot and not too far in the future
+ * - Values are lower than the maximum supply
+ * - For value transactions, the address has a 0 as the last trit due to the
+ * conversion between bytes and trits
+ *
+ * @param tv The transaction validator
+ * @param transaction A transaction under test
+ *
+ * @return true if valid, false otherwise
+ */
+bool iota_consensus_transaction_validate(
+    transaction_validator_t const *const tv,
+    iota_transaction_t const *const transaction);
 
 #ifdef __cplusplus
 }
