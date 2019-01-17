803e75e2120a45e67ed38e0d6dd90114e1848902
tsvisabo <tsvi.sabo@iota.org>
2019-01-17 10:36:01 +0200

    mam/v2: mam_test - send message

--- mam/v2/mam/tests/test_mam.c
+++ mam/v2/mam/tests/test_mam.c
@@ -69,6 +69,132 @@ void test_init_send_msg_cfg(mam_send_msg_context_t *const cfg,
   mam_list_insert_end(cfg->psks, psk);
 }
 
+static trits_t mam_test_generic_send_msg(
+    sponge_t *spongos_sponge, sponge_t *fork_sponge, sponge_t *ntru_sponge,
+    void *sponge_alloc_ctx, sponge_t *(create_sponge)(void *ctx),
+    void (*destroy_sponge)(void *ctx, sponge_t *), prng_t *pa, prng_t *pb,
+    mam_msg_pubkey_t pubkey, mam_msg_keyload_t keyload,
+    mam_msg_checksum_t checksum, mam_channel_t *const cha,
+    mam_endpoint_t *const epa, mam_channel_t *const ch1a,
+    mam_endpoint_t *const ep1a, size_t *const pos) {
+  retcode_t e = RC_MAM2_INTERNAL_ERROR;
+
+  trits_t msg = trits_null();
+  mss_mt_height_t d = 1;
+  mam_psk_node pska[1], pskb[1];
+  ntru_t nb[1];
+  mam_ntru_pk_node nbpk[1];
+  mam_send_msg_context_t cfg_msga[1];
+
+  {
+    MAM2_TRITS_DEF0(k, MAM2_PRNG_KEY_SIZE);
+    k = MAM2_TRITS_INIT(k, MAM2_PRNG_KEY_SIZE);
+
+    trits_from_str(k,
+                   "SENDERPRNGKEYASENDERPRNGKEY"
+                   "SENDERPRNGKEYASENDERPRNGKEY"
+                   "SENDERPRNGKEYASENDERPRNGKEY");
+    prng_init(pa, pa->sponge, k);
+
+    trits_from_str(k,
+                   "RECIPIPRNGKEYBRECIPIPRNGKEY"
+                   "RECIPIPRNGKEYBRECIPIPRNGKEY"
+                   "RECIPIPRNGKEYBRECIPIPRNGKEY");
+    prng_init(pb, pb->sponge, k);
+  }
+
+  /* gen psk */
+  {
+    pska->prev = pska->next = 0;
+    trits_from_str(mam_psk_id(&pska->info), "PSKIDAPSKIDAPSKIDAPSKIDAPSK");
+    prng_gen_str(pa, MAM2_PRNG_DST_SECKEY, "PSKANONCE",
+                 mam_psk_trits(&pska->info));
+
+    pskb->prev = pskb->next = 0;
+    trits_from_str(mam_psk_id(&pskb->info), "PSKIDBPSKIDBPSKIDBPSKIDBPSK");
+    prng_gen_str(pb, MAM2_PRNG_DST_SECKEY, "PSKBNONCE",
+                 mam_psk_trits(&pskb->info));
+  }
+  /* gen recipient'spongos ntru keys */
+  {
+    MAM2_TRITS_DEF0(N, 30);
+    N = MAM2_TRITS_INIT(N, 30);
+    trits_from_str(N, "NTRUBNONCE");
+
+    e = ntru_create(nb);
+    ntru_gen(nb, pb, N, mam_ntru_pk_trits(&nbpk->info));
+    TEST_ASSERT(RC_OK == e);
+  }
+  {
+    mam_send_msg_context_t *cfg = cfg_msga;
+
+    cfg->ma = 0;
+    cfg->spongos->sponge = spongos_sponge;
+    cfg->fork->sponge = fork_sponge;
+    cfg->prng = pa;
+    cfg->rng = pa;
+    cfg->ns->sponge = ntru_sponge;
+
+    cfg->ch = cha;
+    cfg->ch1 = 0;
+    cfg->ep = 0;
+    cfg->ep1 = 0;
+    if (mam_msg_pubkey_epid == pubkey)
+      cfg->ep = epa;
+    else if (mam_msg_pubkey_chid1 == pubkey)
+      cfg->ch1 = ch1a;
+    else if (mam_msg_pubkey_epid1 == pubkey)
+      cfg->ep1 = ep1a;
+    else
+      ;
+
+    cfg->key_plain = 0;
+    cfg->psks.begin = 0;
+    cfg->psks.end = 0;
+    cfg->ntru_pks.begin = 0;
+    cfg->ntru_pks.end = 0;
+    if (mam_msg_keyload_plain == keyload)
+      cfg->key_plain = 1;
+    else if (mam_msg_keyload_psk == keyload) {
+      pska->prev = pska->next = 0;
+      pskb->prev = pskb->next = 0;
+      mam_list_insert_end(cfg->psks, pska);
+      mam_list_insert_end(cfg->psks, pskb);
+    } else if (mam_msg_keyload_ntru == keyload) {
+      nbpk->prev = nbpk->next = 0;
+      mam_list_insert_end(cfg->ntru_pks, nbpk);
+    } else
+      ;
+
+    trits_from_str(mam_send_msg_cfg_nonce(cfg), "SENDERNONCEAAAAASENDERNONCE");
+  }
+
+  size_t sz;
+  sz = mam_send_msg_size(cfg_msga);
+  msg = trits_alloc(sz);
+  TEST_ASSERT(!trits_is_null(msg));
+
+  mam_send_msg(cfg_msga, &msg);
+  TEST_ASSERT(trits_is_empty(msg));
+  msg = trits_pickup(msg, sz);
+
+  *pos = cfg_msga->spongos->pos;
+
+  ntru_destroy(nb);
+
+  return msg;
+}
+
+static void mam_test_generic_receive_msg(
+    sponge_t *spongos_sponge, sponge_t *fork_sponge, sponge_t *ntru_sponge,
+    void *sponge_alloc_ctx, sponge_t *(create_sponge)(void *ctx),
+    void (*destroy_sponge)(void *ctx, sponge_t *), prng_t *pa, prng_t *pb,
+    mam_msg_pubkey_t pubkey, mam_msg_keyload_t keyload,
+    mam_msg_checksum_t checksum, mam_channel_t *const cha, trits_t *const msg,
+    size_t *const pos) {
+  retcode_t e = RC_MAM2_INTERNAL_ERROR;
+}
+
 static void mam_test_generic(sponge_t *s, void *sponge_alloc_ctx,
                              sponge_t *(create_sponge)(void *ctx),
                              void (*destroy_sponge)(void *ctx, sponge_t *),
@@ -93,7 +219,6 @@ static void mam_test_generic(sponge_t *s, void *sponge_alloc_ctx,
   ntru_t nb[1];
   mam_ntru_pk_node nbpk[1];
 
-  mam_send_msg_context_t cfg_msga[1];
   mam_recv_msg_context_t cfg_msgb[1];
   mam_send_packet_context_t cfg_packeta[1];
   mam_recv_packet_context_t cfg_packetb[1];
@@ -109,9 +234,9 @@ static void mam_test_generic(sponge_t *s, void *sponge_alloc_ctx,
   }
   /* create spongos */
   {
-    sa = ma->create_sponge();
     forka = ma->create_sponge();
     nsa = ma->create_sponge();
+    sa = ma->create_sponge();
 
     msb = ma->create_sponge();
     wsb = ma->create_sponge();
@@ -213,51 +338,6 @@ static void mam_test_generic(sponge_t *s, void *sponge_alloc_ctx,
       for (checksum = 0; checksum < 3; ++checksum)
       /* none=0, mac=1, mssig=2 */
       {
-        /* init send msg context */
-        {
-          mam_send_msg_context_t *cfg = cfg_msga;
-
-          cfg->ma = 0;
-          cfg->spongos->sponge = sa;
-          cfg->fork->sponge = forka;
-          cfg->prng = pa;
-          cfg->rng = pa;
-          cfg->ns->sponge = nsa;
-
-          cfg->ch = cha;
-          cfg->ch1 = 0;
-          cfg->ep = 0;
-          cfg->ep1 = 0;
-          if (mam_msg_pubkey_epid == pubkey)
-            cfg->ep = epa;
-          else if (mam_msg_pubkey_chid1 == pubkey)
-            cfg->ch1 = ch1a;
-          else if (mam_msg_pubkey_epid1 == pubkey)
-            cfg->ep1 = ep1a;
-          else
-            ;
-
-          cfg->key_plain = 0;
-          cfg->psks.begin = 0;
-          cfg->psks.end = 0;
-          cfg->ntru_pks.begin = 0;
-          cfg->ntru_pks.end = 0;
-          if (mam_msg_keyload_plain == keyload)
-            cfg->key_plain = 1;
-          else if (mam_msg_keyload_psk == keyload) {
-            pska->prev = pska->next = 0;
-            pskb->prev = pskb->next = 0;
-            mam_list_insert_end(cfg->psks, pska);
-            mam_list_insert_end(cfg->psks, pskb);
-          } else if (mam_msg_keyload_ntru == keyload) {
-            nbpk->prev = nbpk->next = 0;
-            mam_list_insert_end(cfg->ntru_pks, nbpk);
-          } else
-            ;
-
-          trits_from_str(mam_send_msg_cfg_nonce(cfg),
-                         "SENDERNONCEAAAAASENDERNONCE");
-        }
         /* init recv msg context */
         {
           mam_recv_msg_context_t *cfg = cfg_msgb;
@@ -276,16 +356,12 @@ static void mam_test_generic(sponge_t *s, void *sponge_alloc_ctx,
           trits_copy(mam_channel_id(cha), mam_recv_msg_cfg_chid(cfg));
         }
 
+        size_t pos;
         /* send/recv msg */
         {
-          size_t sz;
-          sz = mam_send_msg_size(cfg_msga);
-          msg = trits_alloc(sz);
-          TEST_ASSERT(!trits_is_null(msg));
-
-          mam_send_msg(cfg_msga, &msg);
-          TEST_ASSERT(trits_is_empty(msg));
-          msg = trits_pickup(msg, sz);
+          msg = mam_test_generic_send_msg(
+              sa, forka, nsa, sponge_alloc_ctx, create_sponge, destroy_sponge,
+              pa, pb, pubkey, keyload, checksum, cha, epa, ch1a, ep1a, &pos);
 
           e = mam_recv_msg(cfg_msgb, &msg);
           TEST_ASSERT(RC_OK == e);
@@ -297,7 +373,7 @@ static void mam_test_generic(sponge_t *s, void *sponge_alloc_ctx,
           mam_send_packet_context_t *cfg = cfg_packeta;
 
           cfg->spongos->sponge = sa;
-          cfg->spongos->pos = cfg_msga->spongos->pos;
+          cfg->spongos->pos = pos;
           cfg->ord = 0;
           cfg->checksum = checksum;
           cfg->mss = 0;
