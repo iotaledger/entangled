7dda32de8cae3011dedf046d43e6cdc0bc207512
Sam Chen <sam.chen@iota.org>
2019-01-21 11:32:59 +0800

    cclient: added local PoW in attach_to_tangle.(WIP)

--- cclient/iota_client_core_api.c
+++ cclient/iota_client_core_api.c
@@ -8,6 +8,8 @@
 #include "cclient/iota_client_core_api.h"
 #include "cclient/http/http.h"
 #include "cclient/service.h"
+#include "common/helpers/pow.h"
+#include "utils/time.h"
 
 #define CCLIENT_CORE_LOGGER_ID "cclient_core_api"
 
@@ -432,32 +434,92 @@ retcode_t iota_client_attach_to_tangle(
     const iota_client_service_t* const service,
     const attach_to_tangle_req_t* const req, attach_to_tangle_res_t* res) {
   retcode_t result = RC_OK;
+  flex_trit_t prev_tx_hash[FLEX_TRIT_SIZE_243];
+  iota_transaction_t tx = {};
+  char_buffer_t* res_buff = NULL;
+  char_buffer_t* req_buff = NULL;
+  flex_trit_t* array_elt = NULL;
+  flex_trit_t* nonce = NULL;
+  flex_trit_t* raw_tx = NULL;
 
-  char_buffer_t* res_buff = char_buffer_new();
-  char_buffer_t* req_buff = char_buffer_new();
-  log_info(logger_id, "[%s:%d]\n", __func__, __LINE__);
-  if (req_buff == NULL || res_buff == NULL) {
-    log_critical(logger_id, "[%s:%d] %s\n", __func__, __LINE__,
-                 STR_CCLIENT_OOM);
-    result = RC_CCLIENT_OOM;
-    goto done;
+  if (service == NULL) {
+    log_info(logger_id, "[%s:%d] local PoW\n", __func__, __LINE__);
+    memset(prev_tx_hash, FLEX_TRIT_NULL_VALUE, FLEX_TRIT_SIZE_243);
+    HASH_ARRAY_FOREACH(req->trytes, array_elt) {
+      transaction_deserialize_from_trits(&tx, array_elt, false);
+      transaction_set_trunk(
+          &tx, flex_trits_are_null(prev_tx_hash, FLEX_TRIT_SIZE_243)
+                   ? req->trunk
+                   : prev_tx_hash);
+      transaction_set_branch(
+          &tx, flex_trits_are_null(prev_tx_hash, FLEX_TRIT_SIZE_243)
+                   ? req->branch
+                   : req->trunk);
+
+      if (flex_trits_are_null(transaction_tag(&tx), FLEX_TRIT_SIZE_27)) {
+        transaction_set_tag(&tx, transaction_obsolete_tag(&tx));
+      }
+      transaction_set_attachment_timestamp(&tx, current_timestamp_ms());
+      transaction_set_attachment_timestamp_lower(&tx, 0);
+      transaction_set_attachment_timestamp_upper(&tx, 3812798742493LL);
+
+      raw_tx = transaction_serialize(&tx);
+      if (raw_tx) {
+        nonce = iota_flex_pow(raw_tx, FLEX_TRIT_SIZE_8019, req->mwm);
+        if (nonce) {
+          transaction_set_nonce(&tx, nonce);
+          free(nonce);
+        } else {
+          log_info(logger_id, "[%s:%d] calculating PoW failed.\n", __func__,
+                   __LINE__);
+          return RC_CCLIENT_POW_FAILED;
+        }
+        free(raw_tx);
+      } else {
+        log_info(logger_id,
+                 "[%s:%d] transaction serialize failed during PoW.\n", __func__,
+                 __LINE__);
+        return RC_CCLIENT_TX_DESERIALIZE_FAILED;
+      }
+
+      memcpy(prev_tx_hash, transaction_hash(&tx), FLEX_TRIT_SIZE_243);
+      raw_tx = transaction_serialize(&tx);
+      if (raw_tx) {
+        hash_array_push(res->trytes, transaction_serialize(&tx));
+      } else {
+        log_info(logger_id, "[%s:%d] transaction serialize failed.\n", __func__,
+                 __LINE__);
+        return RC_CCLIENT_TX_DESERIALIZE_FAILED;
+      }
+    }
+
+  } else {
+    log_info(logger_id, "[%s:%d] remote PoW\n", __func__, __LINE__);
+    res_buff = char_buffer_new();
+    req_buff = char_buffer_new();
+    if (req_buff == NULL || res_buff == NULL) {
+      log_critical(logger_id, "[%s:%d] %s\n", __func__, __LINE__,
+                   STR_CCLIENT_OOM);
+      result = RC_CCLIENT_OOM;
+      goto done;
+    }
+
+    result = service->serializer.vtable.attach_to_tangle_serialize_request(
+        &service->serializer, req, req_buff);
+    if (result != RC_OK) {
+      goto done;
+    }
+
+    result = iota_service_query(service, req_buff, res_buff);
+    if (result != RC_OK) {
+      log_error(logger_id, "[%s:%d] %s\n", __func__, __LINE__,
+                error_2_string(result));
+      goto done;
+    }
+
+    result = service->serializer.vtable.attach_to_tangle_deserialize_response(
+        &service->serializer, res_buff->data, res);
   }
-
-  result = service->serializer.vtable.attach_to_tangle_serialize_request(
-      &service->serializer, req, req_buff);
-  if (result != RC_OK) {
-    goto done;
-  }
-
-  result = iota_service_query(service, req_buff, res_buff);
-  if (result != RC_OK) {
-    log_error(logger_id, "[%s:%d] %s\n", __func__, __LINE__,
-              error_2_string(result));
-    goto done;
-  }
-
-  result = service->serializer.vtable.attach_to_tangle_deserialize_response(
-      &service->serializer, res_buff->data, res);
 
 done:
   if (req_buff) {
