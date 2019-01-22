fab56d92d73d270dacf01ad7290ae08887fd2e22
Thibault Martinez <thibault@iota.org>
2019-01-22 14:55:11 +0100

    mam2: NTRU interface clean

--- mam/v2/ntru/ntru.h
+++ mam/v2/ntru/ntru.h
@@ -8,10 +8,6 @@
  * Refer to the LICENSE file for licensing information
  */
 
-/*!
-\file ntru.h
-\brief MAM2 NTRU layer.
-*/
 #ifndef __MAM_V2_NTRU_NTRU_H__
 #define __MAM_V2_NTRU_NTRU_H__
 
@@ -24,57 +20,115 @@
 extern "C" {
 #endif
 
-/*! \brief NTRU public key - 3g(x)/(1+3f(x)) - size. */
+// NTRU public key - 3g(x)/(1+3f(x)) - size
 #define MAM2_NTRU_PK_SIZE 9216
-/*! \brief NTRU private key - f(x) - size. */
+// NTRU private key - f(x) - size
 #define MAM2_NTRU_SK_SIZE 1024
-/*! \brief NTRU session symmetric key size. */
+// NTRU session symmetric key size
 #define MAM2_NTRU_KEY_SIZE MAM2_SPONGE_KEY_SIZE
-/*! \brief NTRU encrypted key size. */
+// NTRU encrypted key size
 #define MAM2_NTRU_EKEY_SIZE 9216
-/*! \brief NTRU id size. */
+// NTRU id size
 #define MAM2_NTRU_ID_SIZE 81
 
+// NTRU layer interface
 typedef struct ntru_s {
-  trit_t
-      *id; /*!< Key id - the first 27 trytes of the corresponding public key.*/
-  trit_t *sk; /*!< Private key trits - small coefficients of polynomial f. */
-  void *
-      f; /*!< Internal representation (`poly_t`) of a private key: NTT(1+3f). */
+  // Key id - the first 27 trytes of the corresponding public key
+  trit_t *public_key_id;
+  // Private key trits - small coefficients of polynomial f
+  trit_t *secret_key;
+  // Internal representation (`poly_t`) of a private key: NTT(1+3f)
+  void *f;
 } ntru_t;
 
-trits_t ntru_id_trits(ntru_t *n);
+/**
+ * Allocates memory for a NTRU interface
+ *
+ * @param ntru A NTRU interface
+ *
+ * @return a status code
+ */
+retcode_t ntru_create(ntru_t *const ntru);
 
-trits_t ntru_sk_trits(ntru_t *n);
+/**
+ * Deallocates memory for a NTRU interface
+ *
+ * @param ntru A NTRU interface
+ */
+void ntru_destroy(ntru_t *const ntru);
 
-/*! \brief Load `NTT(1+3f)` from `sk` trits. */
-void ntru_load_sk(ntru_t *n);
+/**
+ * Gets public key id trits
+ *
+ * @param ntru A NTRU interface
+ *
+ * @return the id trits
+ */
+static inline trits_t ntru_id_trits(ntru_t const *const ntru) {
+  return trits_from_rep(MAM2_NTRU_ID_SIZE, ntru->public_key_id);
+}
 
-void ntru_gen(ntru_t *n, /*!< [in] NTRU interface */
-              prng_t *p, /*!< [in] PRNG interface */
-              trits_t N, /*!< [in] nonce */
-              trits_t pk /*!< [out] NTRU public key (serialized NTT of
-                            polynomial h=3g/(1+3f)) */
-);
+/**
+ * Getssecret key trits
+ *
+ * @param ntru A NTRU interface
+ *
+ * @return the secret  key trits
+ */
+static inline trits_t ntru_sk_trits(ntru_t const *const ntru) {
+  return trits_from_rep(MAM2_NTRU_SK_SIZE, ntru->secret_key);
+}
 
-void ntru_encr(trits_t pk,   /*!< [in] NTRU public key */
-               prng_t *p,    /*!< [in] PRNG interface */
-               spongos_t *s, /*!< [in] Spongos interface */
-               trits_t K,    /*!< [in] session symmetric key to be encrypted */
-               trits_t N,    /*!< [in] nonce */
-               trits_t Y     /*!< [out] encrypted K */
-);
-void ntru_encr_r(trits_t pk, spongos_t *s, trits_t r, trits_t K, trits_t Y);
+/**
+ * Generates a NTRU puublic key
+ *
+ * @param ntru A NTRU interface
+ * @param prng A PRNG interface
+ * @param nonce A nonce
+ * @param public_key A NTRU public key: serialized NTT of polynomial h=3g/(1+3f)
+ */
+void ntru_gen(ntru_t const *const ntru, prng_t const *const prng,
+              trits_t const nonce, trits_t public_key);
 
-bool ntru_decr(ntru_t *n,    /*!< [in] NTRU interface */
-               spongos_t *s, /*!< [in] Spongos interface */
-               trits_t Y,    /*!< [in] encrypted K */
-               trits_t K     /*!< [out] decrypted session symmetric key */
-);
+/**
+ * NTRU encryption of a session key
+ *
+ * @param public_key A NTRU public key
+ * @param prng A PRNG interface
+ * @param spongos A spongos interface
+ * @param session_key A session symmetric key to be encrypted
+ * @param nonce A nonce
+ * @param encrypted_session_key The encrypted session key
+ */
+void ntru_encr(trits_t const public_key, prng_t const *const prng,
+               spongos_t *const spongos, trits_t const session_key,
+               trits_t const nonce, trits_t encrypted_session_key);
 
-retcode_t ntru_create(ntru_t *n);
+/**
+ * NTRU encryption of a session key
+ *
+ * @param public_key A NTRU public key
+ * @param spongos A spongos interface
+ * @param r Pseudo-random trits
+ * @param session_key A session symmetric key to be encrypted
+ * @param encrypted_session_key The encrypted session key
+ */
+void ntru_encr_r(trits_t const public_key, spongos_t *const spongos,
+                 trits_t const r, trits_t const session_key,
+                 trits_t encrypted_session_key);
 
-void ntru_destroy(ntru_t *n);
+/**
+ * NTRU decryption of an encrypted session key
+ *
+ * @param ntru A NTRU interface
+ * @param spongos Aspongos interface
+ * @param encrypted_session_key An encrypted session key
+ * @param session_key The decrypted session symmetric key
+ *
+ * @return
+ */
+bool ntru_decr(ntru_t const *const ntru, spongos_t *const spongos,
+               trits_t const encrypted_session_key, trits_t session_key);
 
 #ifdef __cplusplus
 }
