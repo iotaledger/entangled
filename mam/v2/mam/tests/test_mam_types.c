41aac449edfe501d90b402784cffa4358bd3caf4
tsvisabo <tsvi.sabo@iota.org>
2019-01-30 09:37:25 +0200

    mam/v2/mam/mam_types: serialization test - ntru_pk

--- mam/v2/mam/tests/test_mam_types.c
+++ mam/v2/mam/tests/test_mam_types.c
@@ -13,6 +13,7 @@
 #include <unity/unity.h>
 
 #include "common/trinary/trit_tryte.h"
+#include "mam/v2/mam/mam_ntru_pk_t_set.h"
 #include "mam/v2/mam/mam_pre_shared_key_t_set.h"
 #include "mam/v2/mam/mam_types.h"
 
@@ -27,6 +28,23 @@ static bool mam_pre_shared_key_t_set_cmp(
 
   HASH_ITER(hh, psks_1, entry, tmp) {
     if (!mam_pre_shared_key_t_set_contains(&psks_2, &entry->value)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static bool mam_ntru_pk_t_set_cmp(mam_ntru_pk_t_set_t const ntru_set_1,
+                                  mam_ntru_pk_t_set_t const ntru_set_2) {
+  mam_ntru_pk_t_set_entry_t *entry = NULL;
+  mam_ntru_pk_t_set_entry_t *tmp = NULL;
+
+  TEST_ASSERT_EQUAL_INT(mam_ntru_pk_t_set_size(ntru_set_1),
+                        mam_ntru_pk_t_set_size(ntru_set_2));
+
+  HASH_ITER(hh, ntru_set_1, entry, tmp) {
+    if (!mam_ntru_pk_t_set_contains(&ntru_set_2, &entry->value)) {
       return false;
     }
   }
@@ -66,10 +84,40 @@ static void test_psks_serialization(void) {
   mam_pre_shared_key_t_set_free(&psks_2);
 }
 
+static void test_ntru_serialization(void) {
+  mam_ntru_pk_t_set_t ntru_set_1 = NULL;
+  mam_ntru_pk_t_set_t ntru_set_2 = NULL;
+  mam_ntru_pk_t ntru;
+  tryte_t ntru_pk[MAM2_NTRU_PK_SIZE / 3];
+
+  for (size_t i = 0; i < 26; i++) {
+    memset(ntru_pk, 'A' + i, MAM2_NTRU_PK_SIZE / 3);
+    trytes_to_trits(ntru_pk, ntru.pk, MAM2_NTRU_PK_SIZE / 3);
+    TEST_ASSERT(mam_ntru_pk_t_set_add(&ntru_set_1, &ntru) == RC_OK);
+  }
+
+  size_t size = ntru_serialized_size(ntru_set_1);
+
+  TEST_ASSERT_EQUAL_INT(size, 26 * MAM2_NTRU_PK_SIZE);
+
+  trits_t trits = trits_alloc(size);
+
+  TEST_ASSERT(ntru_serialize(ntru_set_1, trits) == RC_OK);
+
+  TEST_ASSERT(ntru_deserialize(trits, &ntru_set_2) == RC_OK);
+
+  TEST_ASSERT_TRUE(mam_ntru_pk_t_set_cmp(ntru_set_1, ntru_set_2));
+
+  trits_free(trits);
+  mam_ntru_pk_t_set_free(&ntru_set_1);
+  mam_ntru_pk_t_set_free(&ntru_set_2);
+}
+
 int main(void) {
   UNITY_BEGIN();
 
   RUN_TEST(test_psks_serialization);
+  RUN_TEST(test_ntru_serialization);
 
   return UNITY_END();
 }
