cf7c3b3e5abc6fd5f52c69963d5f57b04ad659c4
Thibault Martinez <thibault@iota.org>
2019-01-31 17:11:53 +0100

    mam2: channels serizalization

--- mam/v2/mam/channel.c
+++ mam/v2/mam/channel.c
@@ -9,6 +9,7 @@
  */
 
 #include "mam/v2/mam/channel.h"
+#include "mam/v2/mam/mam_channel_t_set.h"
 #include "mam/v2/pb3/pb3.h"
 
 trits_t mam_channel_id(mam_channel_t const *const channel) {
@@ -83,24 +84,73 @@ size_t mam_channel_serialized_size(mam_channel_t const *const channel) {
 
 retcode_t mam_channel_serialize(mam_channel_t const *const channel,
                                 trits_t *const buffer) {
-  return RC_OK;
+  size_t mss_size = mss_stored_size(&channel->mss);
+
+  return pb3_sizeof_ntrytes(MAM2_CHANNEL_ID_SIZE / 3) +
+         pb3_sizeof_size_t(mss_size) + pb3_sizeof_ntrytes(mss_size / 3);
 }
 
 retcode_t mam_channel_deserialize(trits_t *const buffer,
                                   mam_channel_t *const channel) {
+  size_t mss_size = mss_stored_size(&channel->mss);
+
+  fprintf(stderr, "SERIALIZE\n");
+  pb3_encode_ntrytes(trits_from_rep(MAM2_CHANNEL_ID_SIZE, channel->id), buffer);
+  pb3_encode_size_t(mss_size, buffer);
+  // TODO: update mss_save interface
+  mss_save(&channel->mss, trits_take(*buffer, mss_size));
+  trits_advance(buffer, mss_size);
+
   return RC_OK;
 }
 
 size_t mam_channels_serialized_size(mam_channel_t_set_t const channels) {
-  return 0;
+  mam_channel_t_set_entry_t *entry = NULL;
+  mam_channel_t_set_entry_t *tmp = NULL;
+  size_t s = pb3_sizeof_size_t(mam_channel_t_set_size(channels));
+
+  HASH_ITER(hh, channels, entry, tmp) {
+    s += mam_channel_serialized_size(&entry->value);
+  }
+
+  return s;
 }
 
 retcode_t mam_channels_serialize(mam_channel_t_set_t const channels,
                                  trits_t *const buffer) {
-  return RC_OK;
+  retcode_t ret = RC_OK;
+  mam_channel_t_set_entry_t *entry = NULL;
+  mam_channel_t_set_entry_t *tmp = NULL;
+
+  pb3_encode_size_t(mam_channel_t_set_size(channels), buffer);
+
+  HASH_ITER(hh, channels, entry, tmp) {
+    if ((ret = mam_channel_serialize(&entry->value, buffer)) != RC_OK) {
+      return ret;
+    }
+  }
+
+  return ret;
 }
 
 retcode_t mam_channels_deserialize(trits_t *const buffer,
                                    mam_channel_t_set_t *const channels) {
-  return RC_OK;
+  retcode_t ret = RC_OK;
+  mam_channel_t channel;
+  size_t size = 0;
+
+  if ((ret = pb3_decode_size_t(&size, buffer)) != RC_OK) {
+    return ret;
+  }
+
+  while (size--) {
+    if ((ret = mam_channel_deserialize(buffer, &channel)) != RC_OK) {
+      return ret;
+    }
+    if ((ret = mam_channel_t_set_add(channels, &channel)) != RC_OK) {
+      return ret;
+    }
+  }
+
+  return ret;
 }
