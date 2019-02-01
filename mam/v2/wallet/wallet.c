745b3ab7627594f16e16bcd987e05758b7f8f910
Thibault Martinez <thibault@iota.org>
2019-02-01 16:41:18 +0100

    mam2/wallet: basic struct

---
+++ mam/v2/wallet/wallet.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * MAM is based on an original implementation & specification by apmi.bsu.by
+ * [ITSec Lab]
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include "mam/v2/wallet/wallet.h"
---
+++ mam/v2/wallet/wallet.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * MAM is based on an original implementation & specification by apmi.bsu.by
+ * [ITSec Lab]
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#ifndef __MAM_V2_WALLET_WALLET_H__
+#define __MAM_V2_WALLET_WALLET_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct mam_wallet_s {
+} mam_wallet_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __MAM_V2_WALLET_WALLET_H__
