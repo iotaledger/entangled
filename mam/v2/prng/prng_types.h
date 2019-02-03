66b7af62f5f95a363518956f7d30db7fc8fe41ad
tsvisabo <tsvi.sabo@iota.org>
2019-02-03 09:25:45 +0200

    mam/v2/prng: serialization - prototypes

--- mam/v2/prng/prng_types.h
+++ mam/v2/prng/prng_types.h
@@ -33,9 +33,21 @@ typedef enum prng_destination_tryte_e {
 
 // PRNG layer interface
 typedef struct prng_s {
-  sponge_t *sponge;
-  trit_t *secret_key;
+  sponge_t* sponge;
+  trit_t* secret_key;
 } mam_prng_t;
+
+typedef struct mam_prng_t_set_entry_s mam_prng_t_set_entry_t;
+typedef mam_prng_t_set_entry_t* mam_prng_t_set_t;
+
+size_t mam_prngs_serialized_size(mam_prng_t_set_t const prng_set);
+retcode_t mam_prngs_serialize(mam_prng_t_set_t const prng_set, trits_t trits);
+retcode_t mam_prngs_deserialize(trits_t const trits,
+                                mam_prng_t_set_t* const prng_set);
+
+size_t mam_prng_serialized_size(mam_prng_t const* const prng);
+retcode_t mam_prng_serialize(mam_prng_t const* const prng, trits_t trits);
+retcode_t mam_prng_deserialize(trits_t const trits, mam_prng_t* const prng);
 
 #ifdef __cplusplus
 }
