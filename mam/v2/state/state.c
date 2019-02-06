49d310ebe6ac144603d34551ce9e2fd1f6738fbd
Thibault Martinez <thibault@iota.org>
2019-02-06 12:42:55 +0100

    mam2/state: add prng instance

--- mam/v2/state/state.c
+++ mam/v2/state/state.c
@@ -10,15 +10,27 @@
 
 #include "mam/v2/state/state.h"
 #include "mam/v2/mam/mam_pre_shared_key_t_set.h"
+#include "mam/v2/prng/prng.h"
 
-retcode_t mam_state_init(mam_state_t* const state) {
+retcode_t mam_state_init(mam_state_t* const state,
+                         trits_t const prng_secret_key) {
+  retcode_t ret = RC_OK;
+
+  if ((ret = mam_prng_init(&state->prng, prng_secret_key)) != RC_OK) {
+    return ret;
+  }
   state->psks = NULL;
 
-  return RC_OK;
+  return ret;
 }
 
 retcode_t mam_state_destroy(mam_state_t* const state) {
+  retcode_t ret = RC_OK;
+
+  if ((ret = mam_prng_destroy(&state->prng)) != RC_OK) {
+    return ret;
+  }
   mam_pre_shared_key_t_set_free(&state->psks);
 
-  return RC_OK;
+  return ret;
 }
