b40eab77c82ee7cd011026b5d0fef71751225537
Thibault Martinez <thibault@iota.org>
2019-02-07 14:53:50 +0100

    mam2: mam_state_add_ntru_sk

--- mam/v2/state/state.c
+++ mam/v2/state/state.c
@@ -10,6 +10,7 @@
 
 #include "mam/v2/state/state.h"
 #include "mam/v2/mam/mam_pre_shared_key_t_set.h"
+#include "mam/v2/ntru/mam_ntru_sk_t_set.h"
 #include "mam/v2/prng/prng.h"
 
 retcode_t mam_state_init(mam_state_t* const state,
@@ -19,18 +20,21 @@ retcode_t mam_state_init(mam_state_t* const state,
   if ((ret = mam_prng_init(&state->prng, prng_secret_key)) != RC_OK) {
     return ret;
   }
+  state->ntru_sks = NULL;
   state->psks = NULL;
 
   return ret;
 }
 
 retcode_t mam_state_destroy(mam_state_t* const state) {
-  retcode_t ret = RC_OK;
-
-  if ((ret = mam_prng_destroy(&state->prng)) != RC_OK) {
-    return ret;
-  }
+  mam_prng_destroy(&state->prng);
+  mam_ntru_sk_t_set_free(&state->ntru_sks);
   mam_pre_shared_key_t_set_free(&state->psks);
 
-  return ret;
+  return RC_OK;
+}
+
+retcode_t mam_state_add_ntru_sk(mam_state_t* const state,
+                                mam_ntru_sk_t const* const ntru_sk) {
+  return mam_ntru_sk_t_set_add(&state->ntru_sks, ntru_sk);
 }
