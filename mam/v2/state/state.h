d4514cedc1cad791a4026b334072bfaf3555bd74
Thibault Martinez <thibault.martinez.30@gmail.com>
2019-02-12 17:27:32 +0100

    mam2: psk, ntru_pk and ntru_sk api (#797)
    * mam2: mam_state_add_ntru_sk
    
    * mam2: mam_state_add_ntru_pk
    
    * mam2: mam_state_add_psk
    
    * mam2: renaming for consistency

--- mam/v2/state/state.h
+++ mam/v2/state/state.h
@@ -13,6 +13,7 @@
 
 #include "common/errors.h"
 #include "mam/v2/mam/mam_types.h"
+#include "mam/v2/ntru/ntru_types.h"
 #include "mam/v2/prng/prng_types.h"
 
 #ifdef __cplusplus
@@ -21,12 +22,23 @@ extern "C" {
 
 typedef struct mam_state_s {
   mam_prng_t prng;
-  mam_pre_shared_key_t_set_t psks;
+  mam_ntru_sk_t_set_t ntru_sks;
+  mam_ntru_pk_t_set_t ntru_pks;
+  mam_psk_t_set_t psks;
 } mam_state_t;
 
 retcode_t mam_state_init(mam_state_t* const state,
                          trits_t const prng_secret_key);
 retcode_t mam_state_destroy(mam_state_t* const state);
+
+retcode_t mam_state_add_ntru_sk(mam_state_t* const state,
+                                mam_ntru_sk_t const* const ntru_sk);
+
+retcode_t mam_state_add_ntru_pk(mam_state_t* const state,
+                                mam_ntru_pk_t const* const ntru_pk);
+
+retcode_t mam_state_add_psk(mam_state_t* const state,
+                            mam_psk_t const* const psk);
 
 #ifdef __cplusplus
 }
