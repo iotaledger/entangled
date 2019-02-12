febf85d3d6f973fde0def0eb75782454671ffe6c
Thibault Martinez <thibault@iota.org>
2019-02-12 15:24:48 +0100

    mobile: implement pow_bundle binding

--- mobile/android/Interface.cpp
+++ mobile/android/Interface.cpp
@@ -32,8 +32,61 @@ JNIEXPORT jstring JNICALL Java_org_iota_mobile_Interface_iota_1pow_1trytes(
  * ([Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)[Ljava/lang/String;
  */
 JNIEXPORT jobjectArray JNICALL Java_org_iota_mobile_Interface_iota_1pow_1bundle(
-    JNIEnv*, jclass, jobjectArray, jstring, jstring, jint) {
-  return NULL;
+    JNIEnv* env, jclass thiz, jobjectArray txsTrytes, jstring trunk,
+    jstring branch, jint mwm) {
+  bundle_transactions_t* bundle = NULL;
+  iota_transaction_t tx;
+  iota_transaction_t* curTx = NULL;
+  flex_trit_t serializedFlexTrits[FLEX_TRIT_SIZE_8019];
+  char serializedTrytes[NUM_TRYTES_SERIALIZED_TRANSACTION + 1] = {0};
+  flex_trit_t flexTrunk[FLEX_TRIT_SIZE_243];
+  flex_trit_t flexBranch[FLEX_TRIT_SIZE_243];
+  jobjectArray outputTxsTrytes = NULL;
+  jstring outputTxtrytes;
+  size_t i = 0;
+
+  flex_trits_from_trytes(flexTrunk, NUM_TRITS_TRUNK,
+                         (tryte_t*)env->GetStringUTFChars(trunk, 0),
+                         NUM_TRYTES_TRUNK, NUM_TRYTES_TRUNK);
+  flex_trits_from_trytes(flexBranch, NUM_TRITS_BRANCH,
+                         (tryte_t*)env->GetStringUTFChars(branch, 0),
+                         NUM_TRYTES_BRANCH, NUM_TRYTES_BRANCH);
+
+  bundle_transactions_new(&bundle);
+
+  int txNum = env->GetArrayLength(txsTrytes);
+  for (int i = 0; i < txNum; i++) {
+    jstring txString = (jstring)(env->GetObjectArrayElement(txsTrytes, i));
+    const char* txTrytes = env->GetStringUTFChars(txString, 0);
+    flex_trits_from_trytes(serializedFlexTrits,
+                           NUM_TRITS_SERIALIZED_TRANSACTION, (tryte_t*)txTrytes,
+                           NUM_TRYTES_SERIALIZED_TRANSACTION,
+                           NUM_TRYTES_SERIALIZED_TRANSACTION);
+    transaction_deserialize_from_trits(&tx, serializedFlexTrits, false);
+    bundle_transactions_add(bundle, &tx);
+  }
+
+  if (iota_pow_bundle(bundle, flexTrunk, flexBranch, mwm) != RC_OK) {
+    goto done;
+  }
+
+  outputTxsTrytes =
+      env->NewObjectArray(txNum, env->FindClass("java/lang/String"), 0);
+
+  BUNDLE_FOREACH(bundle, curTx) {
+    transaction_serialize_on_flex_trits(curTx, serializedFlexTrits);
+    flex_trits_to_trytes((tryte_t*)serializedTrytes,
+                         NUM_TRYTES_SERIALIZED_TRANSACTION, serializedFlexTrits,
+                         NUM_TRITS_SERIALIZED_TRANSACTION,
+                         NUM_TRITS_SERIALIZED_TRANSACTION);
+    outputTxtrytes = env->NewStringUTF(serializedTrytes);
+    env->SetObjectArrayElement(outputTxsTrytes, i, outputTxtrytes);
+    i++;
+  }
+
+done:
+  bundle_transactions_free(&bundle);
+  return outputTxsTrytes;
 }
 
 /*
