86a972a3c2ddfd405887e3d1fe5bf754c76722df
Sam Chen <sam.chen@iota.org>
2019-02-14 20:29:43 +0800

    utils: support XTENSA toolchain.

--- utils/handles/rand.h
+++ utils/handles/rand.h
@@ -13,7 +13,7 @@ extern "C" {
 #endif
 
 #if defined(_WIN32) || defined(__unix__) || defined(__unix) || \
-    (defined(__APPLE__) && defined(__MACH__))
+    (defined(__APPLE__) && defined(__MACH__) || defined(__XTENSA__))
 
 #include <stdlib.h>
 
