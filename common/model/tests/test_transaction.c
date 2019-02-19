80cac7325d137cbfc114f1b99ff46cfe0fa48987
YangHau <vulxj0j8j8@gmail.com>
2019-02-19 16:51:20 +0800

    fix wrong arguments error

--- common/model/tests/test_transaction.c
+++ common/model/tests/test_transaction.c
@@ -50,115 +50,109 @@ void test_reset(void) {
 }
 
 void test_read_write_transaction_obj(void) {
-  flex_trit_t test_address[FLEX_TRIT_SIZE_243];
-  int64_t test_value = 123456;
-  flex_trit_t test_obsolete_tag[FLEX_TRIT_SIZE_81];
-  uint64_t test_timestamp = 1477037811737;
-  int64_t test_current_index = 2;
-  int64_t test_last_index = 5;
-  flex_trit_t test_bundle[FLEX_TRIT_SIZE_243];
-
-  flex_trit_t test_trunk[FLEX_TRIT_SIZE_243];
-  flex_trit_t test_branch[FLEX_TRIT_SIZE_243];
-  uint64_t test_attachment_timestamp = 1477037821737;
-  uint64_t test_attachment_timestamp_lower = 1477037831737;
-  uint64_t test_attachment_timestamp_upper = 1477037841737;
-  flex_trit_t test_nonce[FLEX_TRIT_SIZE_81];
-  flex_trit_t test_tag[FLEX_TRIT_SIZE_81];
-
-  flex_trit_t test_hash[FLEX_TRIT_SIZE_243];
-  flex_trit_t test_signature_or_message[FLEX_TRIT_SIZE_6561];
-
-  uint64_t test_snapshot_index = 2345;
-  bool test_solid = true;
-  uint64_t test_arrival_timestamp = 1477037812737;
+  flex_trit_t TEST_FLEX_TRIT_ADDRESS[FLEX_TRIT_SIZE_243];
+  flex_trit_t TEST_FLEX_TRIT_OBSOLETE_TAG[FLEX_TRIT_SIZE_81];
+  flex_trit_t TEST_FLEX_TRIT_BUNDLE[FLEX_TRIT_SIZE_243];
+  flex_trit_t TEST_FLEX_TRIT_TRUNK[FLEX_TRIT_SIZE_243];
+  flex_trit_t TEST_FLEX_TRIT_BRANCH[FLEX_TRIT_SIZE_243];
+  flex_trit_t TEST_FLEX_TRIT_NONCE[FLEX_TRIT_SIZE_81];
+  flex_trit_t TEST_FLEX_TRIT_TAG[FLEX_TRIT_SIZE_81];
+  flex_trit_t TEST_FLEX_TRIT_HASH[FLEX_TRIT_SIZE_243];
+  flex_trit_t TEST_FLEX_TRIT_SIGNATURE_OR_MESSAGE[FLEX_TRIT_SIZE_6561];
 
   iota_transaction_t transaction;
 
   // write transaction values
-  flex_trits_from_trytes(test_address, FLEX_TRIT_SIZE_243, TEST_ADDRESS_0,
-                         NUM_TRYTES_ADDRESS, sizeof(TEST_ADDRESS_0));
-  transaction_set_address(&transaction, test_address);
-  transaction_set_value(&transaction, test_value);
-  flex_trits_from_trytes(test_obsolete_tag, FLEX_TRIT_SIZE_81,
+  flex_trits_from_trytes(TEST_FLEX_TRIT_ADDRESS, NUM_TRITS_ADDRESS,
+                         TEST_ADDRESS_0, NUM_TRYTES_ADDRESS,
+                         NUM_TRYTES_ADDRESS);
+  transaction_set_address(&transaction, TEST_FLEX_TRIT_ADDRESS);
+  transaction_set_value(&transaction, TEST_VALUE);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_OBSOLETE_TAG, NUM_TRITS_OBSOLETE_TAG,
                          TEST_OBSOLETE_TAG, NUM_TRYTES_OBSOLETE_TAG,
-                         sizeof(TEST_OBSOLETE_TAG));
-  transaction_set_obsolete_tag(&transaction, test_obsolete_tag);
-  transaction_set_timestamp(&transaction, test_timestamp);
-  transaction_set_current_index(&transaction, test_current_index);
-  transaction_set_last_index(&transaction, test_last_index);
-  flex_trits_from_trytes(test_bundle, FLEX_TRIT_SIZE_243, TEST_BUNDLE,
-                         NUM_TRYTES_BUNDLE, sizeof(TEST_BUNDLE));
-  transaction_set_bundle(&transaction, test_bundle);
+                         NUM_TRYTES_OBSOLETE_TAG);
+  transaction_set_obsolete_tag(&transaction, TEST_FLEX_TRIT_OBSOLETE_TAG);
+  transaction_set_timestamp(&transaction, TEST_TIMESTAMP);
+  transaction_set_current_index(&transaction, TEST_CURRENT_INDEX);
+  transaction_set_last_index(&transaction, TEST_LAST_INDEX);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_BUNDLE, NUM_TRITS_BUNDLE, TEST_BUNDLE,
+                         NUM_TRYTES_BUNDLE, NUM_TRYTES_BUNDLE);
+  transaction_set_bundle(&transaction, TEST_FLEX_TRIT_BUNDLE);
 
-  flex_trits_from_trytes(test_trunk, FLEX_TRIT_SIZE_243, TEST_TRUNK,
-                         NUM_TRYTES_TRUNK, sizeof(TEST_TRUNK));
-  transaction_set_trunk(&transaction, test_trunk);
-  flex_trits_from_trytes(test_branch, FLEX_TRIT_SIZE_243, TEST_BRANCH,
-                         NUM_TRYTES_BRANCH, sizeof(TEST_BRANCH));
-  transaction_set_branch(&transaction, test_branch);
-  transaction_set_attachment_timestamp(&transaction, test_attachment_timestamp);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_TRUNK, NUM_TRITS_TRUNK, TEST_TRUNK,
+                         NUM_TRYTES_TRUNK, NUM_TRYTES_TRUNK);
+  transaction_set_trunk(&transaction, TEST_FLEX_TRIT_TRUNK);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_BRANCH, NUM_TRITS_BRANCH, TEST_BRANCH,
+                         NUM_TRYTES_BRANCH, NUM_TRYTES_BRANCH);
+  transaction_set_branch(&transaction, TEST_FLEX_TRIT_BRANCH);
+  transaction_set_attachment_timestamp(&transaction, TEST_ATTACHMENT_TIMESTAMP);
   transaction_set_attachment_timestamp_lower(&transaction,
-                                             test_attachment_timestamp_lower);
+                                             TEST_ATTACHMENT_TIMESTAMP_LOWER);
   transaction_set_attachment_timestamp_upper(&transaction,
-                                             test_attachment_timestamp_upper);
-  flex_trits_from_trytes(test_nonce, FLEX_TRIT_SIZE_81, TEST_NONCE,
-                         NUM_TRYTES_NONCE, sizeof(TEST_NONCE));
-  transaction_set_nonce(&transaction, test_nonce);
-  flex_trits_from_trytes(test_tag, FLEX_TRIT_SIZE_81, TEST_TAG_NULL,
-                         NUM_TRYTES_TAG, sizeof(TEST_TAG_NULL));
-  transaction_set_tag(&transaction, test_tag);
+                                             TEST_ATTACHMENT_TIMESTAMP_UPPER);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_NONCE, NUM_TRITS_NONCE, TEST_NONCE,
+                         NUM_TRYTES_NONCE, NUM_TRYTES_NONCE);
+  transaction_set_nonce(&transaction, TEST_FLEX_TRIT_NONCE);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_TAG, NUM_TRITS_TAG, TEST_TAG_NULL,
+                         NUM_TRYTES_TAG, NUM_TRYTES_TAG);
+  transaction_set_tag(&transaction, TEST_FLEX_TRIT_TAG);
 
-  flex_trits_from_trytes(test_hash, FLEX_TRIT_SIZE_243, TEST_HASH,
-                         NUM_TRYTES_HASH, sizeof(TEST_HASH));
-  transaction_set_hash(&transaction, test_hash);
-  flex_trits_from_trytes(test_signature_or_message, FLEX_TRIT_SIZE_6561,
-                         TEST_SIG_1, NUM_TRYTES_SIGNATURE, sizeof(TEST_SIG_1));
-  transaction_set_signature(&transaction, test_signature_or_message);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_HASH, NUM_TRITS_HASH, TEST_HASH,
+                         NUM_TRYTES_HASH, NUM_TRYTES_HASH);
+  transaction_set_hash(&transaction, TEST_FLEX_TRIT_HASH);
+  flex_trits_from_trytes(TEST_FLEX_TRIT_SIGNATURE_OR_MESSAGE,
+                         NUM_TRITS_SIGNATURE, TEST_SIG_1, NUM_TRYTES_SIGNATURE,
+                         NUM_TRYTES_SIGNATURE);
+  transaction_set_signature(&transaction, TEST_FLEX_TRIT_SIGNATURE_OR_MESSAGE);
 
-  transaction_set_snapshot_index(&transaction, test_snapshot_index);
-  transaction_set_solid(&transaction, test_solid);
-  transaction_set_arrival_timestamp(&transaction, test_arrival_timestamp);
+  transaction_set_snapshot_index(&transaction, TEST_SNAPSHOT_INDEX);
+  transaction_set_solid(&transaction, TEST_SOLID);
+  transaction_set_arrival_timestamp(&transaction, TEST_ARRIVAL_TIMESTAMP);
 
   // read transaction values
-  TEST_ASSERT_EQUAL_MEMORY(transaction_address(&transaction), test_address,
-                           sizeof(test_address));
-  TEST_ASSERT_EQUAL_INT(transaction_value(&transaction), test_value);
+  TEST_ASSERT_EQUAL_MEMORY(transaction_address(&transaction),
+                           TEST_FLEX_TRIT_ADDRESS,
+                           sizeof(TEST_FLEX_TRIT_ADDRESS));
+  TEST_ASSERT_EQUAL_INT64(transaction_value(&transaction), TEST_VALUE);
   TEST_ASSERT_EQUAL_MEMORY(transaction_obsolete_tag(&transaction),
-                           test_obsolete_tag, sizeof(test_obsolete_tag));
-  TEST_ASSERT_EQUAL_INT(transaction_timestamp(&transaction), test_timestamp);
-  TEST_ASSERT_EQUAL_INT(transaction_current_index(&transaction),
-                        test_current_index);
-  TEST_ASSERT_EQUAL_INT(transaction_last_index(&transaction), test_last_index);
-  TEST_ASSERT_EQUAL_MEMORY(transaction_bundle(&transaction), test_bundle,
-                           sizeof(test_bundle));
+                           TEST_FLEX_TRIT_OBSOLETE_TAG,
+                           sizeof(TEST_FLEX_TRIT_OBSOLETE_TAG));
+  TEST_ASSERT_EQUAL_INT64(transaction_timestamp(&transaction), TEST_TIMESTAMP);
+  TEST_ASSERT_EQUAL_INT64(transaction_current_index(&transaction),
+                          TEST_CURRENT_INDEX);
+  TEST_ASSERT_EQUAL_INT64(transaction_last_index(&transaction),
+                          TEST_LAST_INDEX);
+  TEST_ASSERT_EQUAL_MEMORY(transaction_bundle(&transaction),
+                           TEST_FLEX_TRIT_BUNDLE,
+                           sizeof(TEST_FLEX_TRIT_BUNDLE));
 
-  TEST_ASSERT_EQUAL_MEMORY(transaction_trunk(&transaction), test_trunk,
-                           sizeof(test_trunk));
-  TEST_ASSERT_EQUAL_MEMORY(transaction_branch(&transaction), test_branch,
-                           sizeof(test_branch));
-  TEST_ASSERT_EQUAL_INT(transaction_attachment_timestamp(&transaction),
-                        test_attachment_timestamp);
-  TEST_ASSERT_EQUAL_INT(transaction_attachment_timestamp_lower(&transaction),
-                        test_attachment_timestamp_lower);
-  TEST_ASSERT_EQUAL_INT(transaction_attachment_timestamp_upper(&transaction),
-                        test_attachment_timestamp_upper);
-  TEST_ASSERT_EQUAL_MEMORY(transaction_nonce(&transaction), test_nonce,
-                           sizeof(test_nonce));
-  TEST_ASSERT_EQUAL_MEMORY(transaction_tag(&transaction), test_tag,
-                           sizeof(test_tag));
+  TEST_ASSERT_EQUAL_MEMORY(transaction_trunk(&transaction),
+                           TEST_FLEX_TRIT_TRUNK, sizeof(TEST_FLEX_TRIT_TRUNK));
+  TEST_ASSERT_EQUAL_MEMORY(transaction_branch(&transaction),
+                           TEST_FLEX_TRIT_BRANCH,
+                           sizeof(TEST_FLEX_TRIT_BRANCH));
+  TEST_ASSERT_EQUAL_INT64(transaction_attachment_timestamp(&transaction),
+                          TEST_ATTACHMENT_TIMESTAMP);
+  TEST_ASSERT_EQUAL_INT64(transaction_attachment_timestamp_lower(&transaction),
+                          TEST_ATTACHMENT_TIMESTAMP_LOWER);
+  TEST_ASSERT_EQUAL_INT64(transaction_attachment_timestamp_upper(&transaction),
+                          TEST_ATTACHMENT_TIMESTAMP_UPPER);
+  TEST_ASSERT_EQUAL_MEMORY(transaction_nonce(&transaction),
+                           TEST_FLEX_TRIT_NONCE, sizeof(TEST_FLEX_TRIT_NONCE));
+  TEST_ASSERT_EQUAL_MEMORY(transaction_tag(&transaction), TEST_FLEX_TRIT_TAG,
+                           sizeof(TEST_FLEX_TRIT_TAG));
 
-  TEST_ASSERT_EQUAL_MEMORY(transaction_hash(&transaction), test_hash,
-                           sizeof(test_hash));
+  TEST_ASSERT_EQUAL_MEMORY(transaction_hash(&transaction), TEST_FLEX_TRIT_HASH,
+                           sizeof(TEST_FLEX_TRIT_HASH));
   TEST_ASSERT_EQUAL_MEMORY(transaction_signature(&transaction),
-                           test_signature_or_message,
-                           sizeof(test_signature_or_message));
+                           TEST_FLEX_TRIT_SIGNATURE_OR_MESSAGE,
+                           sizeof(TEST_FLEX_TRIT_SIGNATURE_OR_MESSAGE));
 
-  TEST_ASSERT_EQUAL_INT(transaction_snapshot_index(&transaction),
-                        test_snapshot_index);
-  TEST_ASSERT_EQUAL_INT8(transaction_solid(&transaction), test_solid);
-  TEST_ASSERT_EQUAL_INT(transaction_arrival_timestamp(&transaction),
-                        test_arrival_timestamp);
+  TEST_ASSERT_EQUAL_INT64(transaction_snapshot_index(&transaction),
+                          TEST_SNAPSHOT_INDEX);
+  TEST_ASSERT_EQUAL_INT8(transaction_solid(&transaction), TEST_SOLID);
+  TEST_ASSERT_EQUAL_INT64(transaction_arrival_timestamp(&transaction),
+                          TEST_ARRIVAL_TIMESTAMP);
 }
 
 int main(void) {
