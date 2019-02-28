dbccbc5889eab7e1a2ef02c09f6b6e1c7f2220ca
Andreas C. Osowski <andreas@osowski.de>
2019-02-28 10:50:19 +0100

    cclient/serialization/json: add get_node_info response serialisation

--- cclient/serialization/json/tests/get_node_info.c
+++ cclient/serialization/json/tests/get_node_info.c
@@ -7,7 +7,7 @@
 
 #include "cclient/serialization/json/tests/shared.h"
 
-void test_serialize_get_node_info(void) {
+static void test_request(void) {
   serializer_t serializer;
   const char* json_text = "{\"command\":\"getNodeInfo\"}";
 
@@ -21,14 +21,15 @@ void test_serialize_get_node_info(void) {
   char_buffer_free(serializer_out);
 }
 
-void test_deserialize_get_node_info(void) {
+static void test_response(void) {
   serializer_t serializer;
+  char_buffer_t* serializer_out = char_buffer_new();
+
   init_json_serializer(&serializer);
   const char* json_text =
       "{"
         "\"appName\":\"" TEST_INFO_APP_NAME "\","
         "\"appVersion\":\"" TEST_INFO_APP_VERSION "\","
-        "\"duration\":1,"
         "\"latestMilestone\":\"" TEST_81_TRYTES_1 "\","
         "\"latestMilestoneIndex\":" STR(TEST_INFO_LATEST_MILESTONE_INDEX) ","
         "\"latestSolidSubtangleMilestone\":\"" TEST_81_TRYTES_2 "\","
@@ -68,12 +69,17 @@ void test_deserialize_get_node_info(void) {
   flex_trits_from_trytes(hash, NUM_TRITS_HASH, (const tryte_t*)TEST_81_TRYTES_3, NUM_TRYTES_HASH, NUM_TRYTES_HASH);
   TEST_ASSERT_EQUAL_MEMORY(hash, node_info->coordinator_address, FLEX_TRIT_SIZE_243);
 
+  serializer.vtable.get_node_info_serialize_response(&serializer, node_info,
+                                                     serializer_out);
+  TEST_ASSERT_EQUAL_STRING(json_text, serializer_out->data);
+
   get_node_info_res_free(&node_info);
+  char_buffer_free(serializer_out);
 }
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_serialize_get_node_info);
-  RUN_TEST(test_deserialize_get_node_info);
+  RUN_TEST(test_request);
+  RUN_TEST(test_response);
   return UNITY_END();
 }
