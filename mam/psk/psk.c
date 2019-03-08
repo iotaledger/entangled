dffae6b992ad448403e8a3443d7fd9ff60ccc346
Thibault Martinez <thibault@iota.org>
2019-03-08 15:15:57 +0100

    mam/psk: clean impl

--- mam/psk/psk.c
+++ mam/psk/psk.c
@@ -14,14 +14,26 @@
 #include "utils/memset_safe.h"
 
 trits_t mam_psk_id(mam_psk_t const *const psk) {
+  if (psk == NULL) {
+    return trits_null();
+  }
+
   return trits_from_rep(MAM_PSK_ID_SIZE, psk->id);
 }
 
 trits_t mam_psk_key(mam_psk_t const *const psk) {
+  if (psk == NULL) {
+    return trits_null();
+  }
+
   return trits_from_rep(MAM_PSK_KEY_SIZE, psk->key);
 }
 
 void mam_psk_destroy(mam_psk_t *const psk) {
+  if (psk == NULL) {
+    return;
+  }
+
   memset_safe(psk->key, MAM_PSK_KEY_SIZE, 0, MAM_PSK_KEY_SIZE);
 }
 
@@ -33,22 +45,32 @@ void mam_psks_destroy(mam_psk_t_set_t *const psks) {
     return;
   }
 
-  HASH_ITER(hh, *psks, entry, tmp) { mam_psk_destroy(&entry->value); }
-  mam_psk_t_set_free(psks);
+  SET_ITER(*psks, entry, tmp) {
+    mam_psk_destroy(&entry->value);
+    mam_psk_t_set_remove_entry(psks, entry);
+  }
 }
 
 size_t mam_psks_serialized_size(mam_psk_t_set_t const psks) {
+  if (psks == NULL) {
+    return 0;
+  }
+
   return pb3_sizeof_size_t(mam_psk_t_set_size(psks)) +
-         mam_psk_t_set_size(psks) * sizeof(mam_psk_t);
+         mam_psk_t_set_size(psks) * (MAM_PSK_ID_SIZE + MAM_PSK_KEY_SIZE);
 }
 
 retcode_t mam_psks_serialize(mam_psk_t_set_t const psks, trits_t *const trits) {
   mam_psk_t_set_entry_t *entry = NULL;
   mam_psk_t_set_entry_t *tmp = NULL;
 
+  if (psks == NULL || trits == NULL) {
+    return RC_NULL_PARAM;
+  }
+
   pb3_encode_size_t(mam_psk_t_set_size(psks), trits);
 
-  HASH_ITER(hh, psks, entry, tmp) {
+  SET_ITER(psks, entry, tmp) {
     pb3_encode_ntrytes(trits_from_rep(MAM_PSK_ID_SIZE, entry->value.id), trits);
     pb3_encode_ntrytes(trits_from_rep(MAM_PSK_KEY_SIZE, entry->value.key),
                        trits);
@@ -60,14 +82,26 @@ retcode_t mam_psks_serialize(mam_psk_t_set_t const psks, trits_t *const trits) {
 retcode_t mam_psks_deserialize(trits_t *const trits,
                                mam_psk_t_set_t *const psks) {
   retcode_t ret = RC_OK;
+  size_t psks_size = 0;
   mam_psk_t psk;
 
-  size_t container_size = 0;
-  pb3_decode_size_t(&container_size, trits);
+  if (trits == NULL || psks == NULL) {
+    return RC_NULL_PARAM;
+  }
 
-  while (!trits_is_empty(*trits) && container_size-- > 0) {
-    pb3_decode_ntrytes(trits_from_rep(MAM_PSK_ID_SIZE, psk.id), trits);
-    pb3_decode_ntrytes(trits_from_rep(MAM_PSK_KEY_SIZE, psk.key), trits);
+  if ((ret = pb3_decode_size_t(&psks_size, trits)) != RC_OK) {
+    return ret;
+  }
+
+  for (size_t i = 0; i < psks_size; i++) {
+    if ((ret = pb3_decode_ntrytes(trits_from_rep(MAM_PSK_ID_SIZE, psk.id),
+                                  trits)) != RC_OK) {
+      break;
+    }
+    if ((ret = pb3_decode_ntrytes(trits_from_rep(MAM_PSK_KEY_SIZE, psk.key),
+                                  trits)) != RC_OK) {
+      break;
+    }
     if ((ret = mam_psk_t_set_add(psks, &psk)) != RC_OK) {
       break;
     }
