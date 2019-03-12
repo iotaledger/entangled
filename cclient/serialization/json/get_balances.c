c0f01aee08e8430a8a5395fa06d6a8f962e8e737
HowJMay <vulxj0j8j8@gmail.com>
2019-03-12 19:49:53 +0800

    get_balance - serialize response & deserialize request (#915)
    * implement serializaztion and deserialization funcs
    
    implement funcs of serializing response and deserializing request
    functions, but the unit test hasn't been implemented yet.
    
    implemet json_get_size_t_num() to get size_t obj from cJSON obj
    
    * Add get_balances unit test and needed helper func
    
    In order to convert uint64_t utarray to json array, I
    implemented utarray_uint64_to_json_array() to take this job.
    
    * use JSON_CHECK_ERROR and change arg order
    
    * fix miss one
    
    * `tips` not jump, add commet
    
    since `tips` is optional parameter, so we serialize it whenever we need.
    
    revise the commet of threshold in `cclient/request/get_balances.h`
    
    * tips deserialize err handling
    
    * couple fixes
    
    remove if condition
    
    use PRIu64 instead of lld
    
    move buffer out of while loop
    
    * remove unnecessaey things
    
    * Add if determine RC_CCLIENT_JSON_KEY

--- cclient/serialization/json/get_balances.c
+++ cclient/serialization/json/get_balances.c
@@ -33,11 +33,13 @@ retcode_t json_get_balances_serialize_request(
 
   cJSON_AddItemToObject(json_root, "threshold",
                         cJSON_CreateNumber(obj->threshold));
-
-  ret = hash243_queue_to_json_array(obj->tips, json_root, "tips");
-  if (ret != RC_OK) {
-    goto err;
+  if (obj->tips) {
+    ret = hash243_queue_to_json_array(obj->tips, json_root, "tips");
+    if (ret != RC_OK) {
+      goto err;
+    }
   }
+
   json_text = cJSON_PrintUnformatted(json_root);
   if (json_text) {
     len = strlen(json_text);
@@ -48,8 +50,76 @@ retcode_t json_get_balances_serialize_request(
     cJSON_free((void *)json_text);
   }
 
+err:
   cJSON_Delete(json_root);
   return ret;
+}
+
+retcode_t json_get_balances_deserialize_request(serializer_t const *const s,
+                                                char const *const obj,
+                                                get_balances_req_t *const req) {
+  retcode_t ret = RC_OK;
+  cJSON *json_obj = cJSON_Parse(obj);
+  cJSON *json_item = NULL;
+  log_info(json_logger_id, "[%s:%d] %s\n", __func__, __LINE__, obj);
+
+  JSON_CHECK_ERROR(json_obj, json_item, json_logger_id);
+
+  ret = json_get_uint8(json_obj, "threshold", &req->threshold);
+  if (ret) {
+    goto end;
+  }
+
+  ret = json_array_to_hash243_queue(json_obj, "addresses", &req->addresses);
+  if (ret) {
+    goto end;
+  }
+  ret = json_array_to_hash243_queue(json_obj, "tips", &req->tips);
+  if (ret == RC_CCLIENT_JSON_KEY) {
+    ret = RC_OK;
+  }
+
+end:
+  cJSON_Delete(json_obj);
+  return ret;
+}
+
+retcode_t json_get_balances_serialize_response(
+    serializer_t const *const s, const get_balances_res_t *const res,
+    char_buffer_t *out) {
+  retcode_t ret = RC_OK;
+  const char *json_text = NULL;
+  size_t len = 0;
+  log_info(json_logger_id, "[%s:%d]\n", __func__, __LINE__);
+  cJSON *json_root = cJSON_CreateObject();
+  if (json_root == NULL) {
+    log_critical(json_logger_id, "[%s:%d] %s\n", __func__, __LINE__,
+                 STR_CCLIENT_JSON_CREATE);
+    return RC_CCLIENT_JSON_CREATE;
+  }
+
+  ret = utarray_uint64_to_json_array(res->balances, json_root, "balances");
+  if (ret != RC_OK) {
+    goto err;
+  }
+
+  ret = hash243_queue_to_json_array(res->milestone, json_root, "references");
+  if (ret != RC_OK) {
+    goto err;
+  }
+
+  cJSON_AddItemToObject(json_root, "milestoneIndex",
+                        cJSON_CreateNumber(res->milestoneIndex));
+
+  json_text = cJSON_PrintUnformatted(json_root);
+  if (json_text) {
+    len = strlen(json_text);
+    ret = char_buffer_allocate(out, len);
+    if (ret == RC_OK) {
+      strncpy(out->data, json_text, len);
+    }
+    cJSON_free((void *)json_text);
+  }
 
 err:
   cJSON_Delete(json_root);
@@ -64,20 +134,7 @@ retcode_t json_get_balances_deserialize_response(serializer_t const *const s,
   cJSON *json_item = NULL;
   log_info(json_logger_id, "[%s:%d] %s\n", __func__, __LINE__, obj);
 
-  if (json_obj == NULL) {
-    log_error(json_logger_id, "[%s:%d] %s\n", __func__, __LINE__,
-              STR_CCLIENT_JSON_PARSE);
-    cJSON_Delete(json_obj);
-    return RC_CCLIENT_JSON_PARSE;
-  }
-
-  json_item = cJSON_GetObjectItemCaseSensitive(json_obj, "error");
-  if (cJSON_IsString(json_item) && (json_item->valuestring != NULL)) {
-    log_error(json_logger_id, "[%s:%d] %s %s\n", __func__, __LINE__,
-              STR_CCLIENT_RES_ERROR, json_item->valuestring);
-    cJSON_Delete(json_obj);
-    return RC_CCLIENT_RES_ERROR;
-  }
+  JSON_CHECK_ERROR(json_obj, json_item, json_logger_id);
 
   ret = json_array_to_uint64(json_obj, "balances", out->balances);
   if (ret) {
@@ -90,12 +147,8 @@ retcode_t json_get_balances_deserialize_response(serializer_t const *const s,
   }
 
   ret = json_get_int(json_obj, "milestoneIndex", &out->milestoneIndex);
-  if (ret != RC_OK) {
-    goto end;
-  }
 
 end:
-
   cJSON_Delete(json_obj);
   return ret;
 }
