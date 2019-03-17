dcc42eeac4d762593d496c2b6c39e0837a2889c7
Thibault Martinez <thibault.martinez.30@gmail.com>
2019-03-17 08:15:24 +0100

    common: move pow to helpers (#971)

--- common/helpers/pow.c
+++ common/helpers/pow.c
@@ -5,20 +5,36 @@
  * Refer to the LICENSE file for licensing information
  */
 
-#include "common/helpers/pow.h"
-
 #include <stdlib.h>
 #include <string.h>
 
-#include "common/curl-p/hashcash.h"
-#include "common/errors.h"
-#include "common/model/bundle.h"
-#include "common/pow/pow.h"
-#include "common/trinary/trit_tryte.h"
-#include "digest.h"
 #include "utarray.h"
+
+#include "common/curl-p/hashcash.h"
+#include "common/helpers/digest.h"
+#include "common/helpers/pow.h"
+#include "common/trinary/trit_tryte.h"
 #include "utils/export.h"
 #include "utils/time.h"
+
+#define NONCE_LENGTH 27 * 3
+
+trit_t *do_pow(Curl *const curl, trit_t const *const trits_in,
+               size_t const trits_len, uint8_t const mwm) {
+  tryte_t *nonce_trits = (tryte_t *)calloc(NONCE_LENGTH + 1, sizeof(tryte_t));
+
+  curl_absorb(curl, trits_in, trits_len - HASH_LENGTH_TRIT);
+  memcpy(curl->state, trits_in + trits_len - HASH_LENGTH_TRIT,
+         HASH_LENGTH_TRIT);
+
+  // FIXME(th0br0) deal with result value of `hashcash` call
+  hashcash(curl, BODY, HASH_LENGTH_TRIT - NONCE_LENGTH, HASH_LENGTH_TRIT, mwm);
+
+  memcpy(nonce_trits, curl->state + HASH_LENGTH_TRIT - NONCE_LENGTH,
+         NONCE_LENGTH);
+
+  return nonce_trits;
+}
 
 IOTA_EXPORT char *iota_pow_trytes(char const *const trytes_in,
                                   uint8_t const mwm) {
