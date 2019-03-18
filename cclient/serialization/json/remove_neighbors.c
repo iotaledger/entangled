06ffaf2867c6dfd654f1053c35c8e36162daff8e
YangHau <vulxj0j8j8@gmail.com>
2019-03-18 10:38:22 +0800

    implemet remove_neighbors serialization
    implement cclient/serializer/json:remove_neighbors - serialize response & deserialize request

--- cclient/serialization/json/remove_neighbors.c
+++ cclient/serialization/json/remove_neighbors.c
@@ -9,11 +9,10 @@
 #include "cclient/serialization/json/helpers.h"
 #include "cclient/serialization/json/logger.h"
 
-retcode_t json_remove_neighbors_serialize_request(const serializer_t *const s, const remove_neighbors_req_t *const req,
+retcode_t json_remove_neighbors_serialize_request(serializer_t const *const s, remove_neighbors_req_t const *const req,
                                                   char_buffer_t *out) {
   retcode_t ret = RC_OK;
   const char *json_text = NULL;
-  size_t len = 0;
   log_info(json_logger_id, "[%s:%d]\n", __func__, __LINE__);
   cJSON *json_root = cJSON_CreateObject();
   if (json_root == NULL) {
@@ -31,11 +30,7 @@ retcode_t json_remove_neighbors_serialize_request(const serializer_t *const s, c
 
   json_text = cJSON_PrintUnformatted(json_root);
   if (json_text) {
-    len = strlen(json_text);
-    ret = char_buffer_allocate(out, len);
-    if (ret == RC_OK) {
-      strncpy(out->data, json_text, len);
-    }
+    ret = char_buffer_set(out, json_text);
     cJSON_free((void *)json_text);
   }
 
@@ -43,7 +38,57 @@ retcode_t json_remove_neighbors_serialize_request(const serializer_t *const s, c
   return ret;
 }
 
-retcode_t json_remove_neighbors_deserialize_response(const serializer_t *const s, const char *const obj,
+retcode_t json_remove_neighbors_deserialize_request(serializer_t const *const s, char const *const obj,
+                                                    remove_neighbors_req_t *req) {
+  retcode_t ret = RC_OK;
+  cJSON *json_obj = cJSON_Parse(obj);
+  cJSON *json_item = NULL;
+  log_info(json_logger_id, "[%s:%d] %s\n", __func__, __LINE__, obj);
+
+  if (json_obj == NULL) {
+    log_error(json_logger_id, "[%s:%d] %s\n", __func__, __LINE__, STR_CCLIENT_JSON_PARSE);
+    ret = RC_CCLIENT_JSON_PARSE;
+    goto end;
+  }
+
+  json_item = cJSON_GetObjectItemCaseSensitive(json_obj, "error");
+  if (cJSON_IsString(json_item) && (json_item->valuestring != NULL)) {
+    log_error(json_logger_id, "[%s:%d] %s %s\n", __func__, __LINE__, STR_CCLIENT_RES_ERROR, json_item->valuestring);
+    ret = RC_CCLIENT_RES_ERROR;
+    goto end;
+  }
+
+  ret = json_string_array_to_utarray(json_obj, "uris", req->uris);
+
+end:
+  cJSON_Delete(json_obj);
+  return ret;
+}
+
+retcode_t json_remove_neighbors_serialize_response(serializer_t const *const s, remove_neighbors_res_t const *const res,
+                                                   char_buffer_t *out) {
+  retcode_t ret = RC_OK;
+  const char *json_text = NULL;
+  log_info(json_logger_id, "[%s:%d]\n", __func__, __LINE__);
+  cJSON *json_root = cJSON_CreateObject();
+  if (json_root == NULL) {
+    log_critical(json_logger_id, "[%s:%d] %s\n", __func__, __LINE__, STR_CCLIENT_JSON_CREATE);
+    return RC_CCLIENT_JSON_CREATE;
+  }
+
+  cJSON_AddNumberToObject(json_root, "removedNeighbors", res->removed_neighbors);
+
+  json_text = cJSON_PrintUnformatted(json_root);
+  if (json_text) {
+    ret = char_buffer_set(out, json_text);
+    cJSON_free((void *)json_text);
+  }
+
+  cJSON_Delete(json_root);
+  return ret;
+}
+
+retcode_t json_remove_neighbors_deserialize_response(serializer_t const *const s, char const *const obj,
                                                      remove_neighbors_res_t *res) {
   retcode_t ret = RC_OK;
   cJSON *json_obj = cJSON_Parse(obj);
--- cclient/serialization/json/tests/remove_neighbors.c
+++ cclient/serialization/json/tests/remove_neighbors.c
@@ -7,7 +7,7 @@
 
 #include "cclient/serialization/json/tests/shared.h"
 
-void test_serialize_remove_neighbors(void) {
+void test_remove_neighbors_serialize_request(void) {
   serializer_t serializer;
   const char* json_text = "{\"command\":\"removeNeighbors\",\"uris\":[\"" TEST_NEIGHBOR1 "\",\"" TEST_NEIGHBOR2 "\"]}";
 
@@ -25,7 +25,41 @@ void test_serialize_remove_neighbors(void) {
   remove_neighbors_req_free(&req);
 }
 
-void test_deserialize_remove_neighbors(void) {
+void test_remove_neighbors_deserialize_request(void) {
+  serializer_t serializer;
+  init_json_serializer(&serializer);
+  const char* json_text = "{\"command\":\"removeNeighbors\",\"uris\":[\"" TEST_NEIGHBOR1 "\",\"" TEST_NEIGHBOR2 "\"]}";
+
+  remove_neighbors_req_t* req = remove_neighbors_req_new();
+  remove_neighbors_req_add(req, TEST_NEIGHBOR1);
+  remove_neighbors_req_add(req, TEST_NEIGHBOR2);
+  serializer.vtable.remove_neighbors_deserialize_request(&serializer, json_text, req);
+
+  TEST_ASSERT_EQUAL_STRING(TEST_NEIGHBOR1, remove_neighbors_req_uris_at(req, 0));
+  TEST_ASSERT_EQUAL_STRING(TEST_NEIGHBOR2, remove_neighbors_req_uris_at(req, 1));
+
+  remove_neighbors_req_free(&req);
+}
+
+void test_remove_neighbors_serialize_response(void) {
+  serializer_t serializer;
+  const char* json_text = "{\"removedNeighbors\":" STR(REMOVE_NEIGHBORS_RES) "}";
+
+  char_buffer_t* serializer_out = char_buffer_new();
+  init_json_serializer(&serializer);
+  remove_neighbors_res_t* res = remove_neighbors_res_new();
+
+  res->removed_neighbors = REMOVE_NEIGHBORS_RES;
+
+  serializer.vtable.remove_neighbors_serialize_response(&serializer, res, serializer_out);
+
+  TEST_ASSERT_EQUAL_STRING(json_text, serializer_out->data);
+
+  char_buffer_free(serializer_out);
+  remove_neighbors_res_free(&res);
+}
+
+void test_remove_neighbors_deserialize_response(void) {
   serializer_t serializer;
   init_json_serializer(&serializer);
   const char* json_text = "{\"removedNeighbors\":" STR(REMOVE_NEIGHBORS_RES) ",\"duration\":2}";
@@ -41,7 +75,10 @@ void test_deserialize_remove_neighbors(void) {
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_serialize_remove_neighbors);
-  RUN_TEST(test_deserialize_remove_neighbors);
+  RUN_TEST(test_remove_neighbors_serialize_request);
+  RUN_TEST(test_remove_neighbors_deserialize_request);
+  RUN_TEST(test_remove_neighbors_serialize_response);
+  RUN_TEST(test_remove_neighbors_deserialize_response);
+
   return UNITY_END();
 }
