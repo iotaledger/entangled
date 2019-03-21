2aee3758c890863226bcb461735ee2c4d13b8483
Thibault Martinez <thibault@iota.org>
2019-03-21 14:50:01 +0100

    cclient: fix get_inclusion_states name inconsistencies

--- cclient/response/get_inclusion_states.c
+++ cclient/response/get_inclusion_states.c
@@ -5,10 +5,10 @@
  * Refer to the LICENSE file for licensing information
  */
 
-#include "cclient/response/get_inclusion_state.h"
+#include "cclient/response/get_inclusion_states.h"
 
-get_inclusion_state_res_t* get_inclusion_state_res_new() {
-  get_inclusion_state_res_t* res = (get_inclusion_state_res_t*)malloc(sizeof(get_inclusion_state_res_t));
+get_inclusion_states_res_t* get_inclusion_states_res_new() {
+  get_inclusion_states_res_t* res = (get_inclusion_states_res_t*)malloc(sizeof(get_inclusion_states_res_t));
   if (res) {
     res->states = NULL;
     utarray_new(res->states, &ut_int_icd);
@@ -16,7 +16,7 @@ get_inclusion_state_res_t* get_inclusion_state_res_new() {
   return res;
 }
 
-retcode_t get_inclusion_state_res_states_set(get_inclusion_state_res_t* res, int st) {
+retcode_t get_inclusion_states_res_states_set(get_inclusion_states_res_t* res, int st) {
   if (!res->states) {
     utarray_new(res->states, &ut_int_icd);
   }
@@ -27,7 +27,7 @@ retcode_t get_inclusion_state_res_states_set(get_inclusion_state_res_t* res, int
   return RC_OK;
 }
 
-bool get_inclusion_state_res_states_at(get_inclusion_state_res_t* in, int index) {
+bool get_inclusion_states_res_states_at(get_inclusion_states_res_t* in, int index) {
   int* b = (int*)utarray_eltptr(in->states, index);
   if (b != NULL) {
     return (*b > 0) ? true : false;
@@ -35,9 +35,9 @@ bool get_inclusion_state_res_states_at(get_inclusion_state_res_t* in, int index)
   return false;
 }
 
-int get_inclusion_state_res_states_count(get_inclusion_state_res_t* in) { return utarray_len(in->states); }
+int get_inclusion_states_res_states_count(get_inclusion_states_res_t* in) { return utarray_len(in->states); }
 
-void get_inclusion_state_res_free(get_inclusion_state_res_t** res) {
+void get_inclusion_states_res_free(get_inclusion_states_res_t** res) {
   if (*res) {
     utarray_free((*res)->states);
     free(*res);
