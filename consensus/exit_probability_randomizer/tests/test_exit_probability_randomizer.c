18ef58f8b5dabf9e2d400938a3a9c4327e8dc503
Thibault Martinez <thibault.martinez.30@gmail.com>
2019-03-25 11:28:06 +0100

    ciri: CLI option to skip snapshot validation (#1010)
    * snapshot: CLI option to skip signature validation
    
    * ciri: update README
    
    * consensus: fix tests
    
    * ciri/api: fix tests

--- consensus/exit_probability_randomizer/tests/test_exit_probability_randomizer.c
+++ consensus/exit_probability_randomizer/tests/test_exit_probability_randomizer.c
@@ -79,7 +79,7 @@ static void init_epv(exit_prob_transaction_validator_t *const epv) {
 
   strcpy(conf.snapshot_file, snapshot_path);
   strcpy(conf.snapshot_conf_file, snapshot_conf_path);
-  strcpy(conf.snapshot_signature_file, "");
+  conf.snapshot_signature_skip_validation = true;
   TEST_ASSERT(iota_snapshot_init(&snapshot, &conf) == RC_OK);
   TEST_ASSERT(iota_consensus_transaction_solidifier_init(&ts, &conf, NULL, NULL) == RC_OK);
   TEST_ASSERT(iota_milestone_tracker_init(&mt, &conf, &snapshot, &lv, &ts) == RC_OK);
