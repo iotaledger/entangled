a974c135a2aeaaee5f0498a83c0a15d65baf6609
Thibault Martinez <thibault.martinez.30@gmail.com>
2019-03-29 17:22:53 +0100

    api/http: get_trytes implementation (#1018)

--- ciri/api/http.c
+++ ciri/api/http.c
@@ -346,13 +346,15 @@ static inline retcode_t process_get_trytes_request(iota_api_http_t *const http, 
     goto done;
   }
 
-  // TODO Deserialize request
+  if ((ret = http->serializer.vtable.get_trytes_deserialize_request(&http->serializer, payload, req)) != RC_OK) {
+    goto done;
+  }
 
   if ((ret = iota_api_get_trytes(http->api, tangle, req, res, &error)) != RC_OK) {
     error_serialize_response(http, &error, out);
+  } else {
+    ret = http->serializer.vtable.get_trytes_serialize_response(&http->serializer, res, out);
   }
-
-  // TODO Serialize response
 
 done:
   get_trytes_req_free(&req);
