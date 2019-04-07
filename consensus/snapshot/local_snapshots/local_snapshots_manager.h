edf61bccba331abc2421b6a5089a06498b207a38
tsvisabo <tsvi.sabo@iota.org>
2019-04-07 11:32:50 +0300

    fix include guards, rename funcs and new files

--- consensus/snapshot/local_snapshots/local_snapshots_manager.h
+++ consensus/snapshot/local_snapshots/local_snapshots_manager.h
@@ -5,14 +5,15 @@
  * Refer to the LICENSE file for licensing information
  */
 
-#ifndef __CONSENSUS_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_H__
-#define __CONSENSUS_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_H__
+#ifndef __CONSENSUS_SNAPSHOT_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_MANAGER_H__
+#define __CONSENSUS_SNAPSHOT_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_MANAGER_H__
 
 #include <stdbool.h>
 #include <stdint.h>
 
 #include "consensus/conf.h"
 #include "consensus/milestone_tracker/milestone_tracker.h"
+#include "consensus/snapshot/local_snapshots/local_snapshots_service.h"
 #include "consensus/tangle/tangle.h"
 #include "utils/handles/cond.h"
 #include "utils/handles/rw_lock.h"
@@ -24,13 +25,15 @@ extern "C" {
 
 typedef struct local_snapshots_manager_s {
   bool running;
-  iota_consensus_conf_t* conf;
+  iota_consensus_conf_t *conf;
   thread_handle_t local_snapshots_thread;
   cond_handle_t cond_local_snapshots;
-  milestone_tracker_t const* mt;
+  milestone_tracker_t const *mt;
 
-  // Muteable data
-  tangle_t tangle;
+  local_snapshots_service_t
+
+      // Muteable data
+      tangle_t tangle;
   size_t last_snapshot_transactions_count;
 } local_snapshots_manager_t;
 
@@ -42,8 +45,8 @@ typedef struct local_snapshots_manager_s {
  *
  * @return a status code
  */
-retcode_t iota_local_snapshots_init(local_snapshots_manager_t* lsm, iota_consensus_conf_t const* const conf,
-                                    milestone_tracker_t const* const mt);
+retcode_t iota_local_snapshots_manager_init(local_snapshots_manager_t *lsm, iota_consensus_conf_t const *const conf,
+                                            milestone_tracker_t const *const mt);
 
 /**
  * Starts a local snapshots manager
@@ -52,7 +55,7 @@ retcode_t iota_local_snapshots_init(local_snapshots_manager_t* lsm, iota_consens
  *
  * @return a status code
  */
-retcode_t iota_local_snapshots_start(local_snapshots_manager_t* const lsm);
+retcode_t iota_local_snapshots_manager_start(local_snapshots_manager_t *const lsm);
 
 /**
  * Stops a local snapshots manager
@@ -61,7 +64,7 @@ retcode_t iota_local_snapshots_start(local_snapshots_manager_t* const lsm);
  *
  * @return a status code
  */
-retcode_t iota_local_snapshots_stop(local_snapshots_manager_t* const lsm);
+retcode_t iota_local_snapshots_manager_stop(local_snapshots_manager_t *const lsm);
 
 /**
  * Destroys a local snapshots manager
@@ -70,14 +73,14 @@ retcode_t iota_local_snapshots_stop(local_snapshots_manager_t* const lsm);
  *
  * @return a status code
  */
-retcode_t iota_local_snapshots_destroy(local_snapshots_manager_t* const lsm);
+retcode_t iota_local_snapshots_manager_destroy(local_snapshots_manager_t *const lsm);
 
-bool local_snapshots_should_take_snapshot(local_snapshots_manager_t const* const lsm);
+bool iota_local_snapshots_manager_should_take_snapshot(local_snapshots_manager_t const *const lsm);
 
-retcode_t local_snapshots_take_snapshot(local_snapshots_manager_t* const lsm);
+retcode_t iota_local_snapshots_manager_take_snapshot(local_snapshots_manager_t *const lsm);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // __CONSENSUS_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_H__
+#endif  // __CONSENSUS_SNAPSHOT_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_MANAGER_H__
---
+++ consensus/snapshot/local_snapshots/local_snapshots_service.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#ifndef __CONSENSUS_SNAPSHOT_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_SERVICE_H__
+#define __CONSENSUS_SNAPSHOT_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_SERVICE_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "consensus/conf.h"
+#include "consensus/milestone_tracker/milestone_tracker.h"
+#include "consensus/tangle/tangle.h"
+#include "utils/handles/cond.h"
+#include "utils/handles/rw_lock.h"
+#include "utils/handles/thread.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct local_snapshots_service_s {
+} local_snapshots_service_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __CONSENSUS_SNAPSHOT_LOCAL_SNAPSHOTS_LOCAL_SNAPSHOTS_SERVICE_H__
