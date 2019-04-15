59bce287314c3f7a7ccd5f0ad35a203979100951
tsvisabo <tsvi.sabo@iota.org>
2019-04-15 14:21:21 +0300

    utils: files - documentation and renaming

--- utils/files.h
+++ utils/files.h
@@ -14,9 +14,34 @@
 extern "C" {
 #endif
 
-retcode_t copy_file(const char *to, const char *from);
-retcode_t remove_file(const char *file_path);
-retcode_t write_file(char const *const file_path, char const *const content);
+/**
+ * Copies a file to a given path
+ *
+ * @param to The path of the destination file
+ * @param from The path of the source file
+ *
+ * @return error code
+ */
+retcode_t iota_utils_copy_file(char const *const to, char const *const from);
+
+/**
+ * Removes an exiting file
+ *
+ * @param file_path The path of the file
+ *
+ * @return error code
+ */
+retcode_t iota_utils_remove_file(char const *const file_path);
+
+/**
+ * Overwrites an existing file or creates a new one with provided content
+ *
+ * @param file_path The path of the file
+ * @param content The content to write
+ *
+ * @return error code
+ */
+retcode_t iota_utils_overwrite_file(char const *const file_path, char const *const content);
 
 #ifdef __cplusplus
 }
