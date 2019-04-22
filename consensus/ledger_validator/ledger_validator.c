f8f7fecf37f327ff39e7df4be9d7f97c472a498b
tsvisabo <tsvi.sabo@iota.org>
2019-04-22 18:17:36 +0300

    consensus/ledger_validator: replay several milestones

--- consensus/ledger_validator/ledger_validator.c
+++ consensus/ledger_validator/ledger_validator.c
@@ -264,7 +264,7 @@ retcode_t iota_consensus_ledger_validator_update_snapshot(ledger_validator_t con
 
       if ((ret = iota_milestone_service_replay_milestones(tangle, &lv->milestone_service,
                                                           &lv->milestone_tracker->snapshots_provider->latest_snapshot,
-                                                          milestone->index - 1)) != RC_OK) {
+                                                          milestone->index)) != RC_OK) {
         log_error(logger_id, "Applying patch failed\n");
         goto done;
       }
