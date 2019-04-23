1ab3022ba17a06bf289a8cba01f054446191d21a
Thibault Martinez <thibault@iota.org>
2019-04-23 13:39:41 +0200

    consensus: do not parse spent addresses files on testnet

--- consensus/spent_addresses/spent_addresses_service.c
+++ consensus/spent_addresses/spent_addresses_service.c
@@ -13,27 +13,53 @@
 
 static logger_id_t logger_id;
 
-retcode_t iota_spent_addresses_service_init(spent_addresses_service_t *const sas, iota_consensus_conf_t *const conf) {
+/*
+ * Private functions
+ */
+
+#if defined(IOTA_MAINNET)
+static retcode_t iota_spent_addresses_service_read_files(spent_addresses_service_t *const sas) {
   retcode_t ret = RC_OK;
   spent_addresses_provider_t sap;
+  connection_config_t db_conf = {.db_path = sas->conf->spent_addresses_db_path};
+  char *files[] = {"external/previous_epochs_spent_addresses_1/file/downloaded",
+                   "external/previous_epochs_spent_addresses_2/file/downloaded",
+                   "external/previous_epochs_spent_addresses_3/file/downloaded", NULL};
+
+  if ((ret = iota_spent_addresses_provider_init(&sap, &db_conf)) != RC_OK) {
+    log_error(logger_id, "Initializing spent addresses database connection failed\n");
+    return ret;
+  }
+
+  for (size_t i = 0; files[i]; i++) {
+    if ((ret = iota_spent_addresses_provider_read_file(&sap, files[i])) != RC_OK) {
+      break;
+    }
+  }
+
+  if ((ret = iota_spent_addresses_provider_destroy(&sap)) != RC_OK) {
+    log_error(logger_id, "Destroying spent addresses database connection failed\n");
+  }
+
+  return ret;
+}
+#endif
+
+/*
+ * Public functions
+ */
+
+retcode_t iota_spent_addresses_service_init(spent_addresses_service_t *const sas, iota_consensus_conf_t *const conf) {
+  retcode_t ret = RC_OK;
 
   sas->conf = conf;
   logger_id = logger_helper_enable(SPENT_ADDRESSES_SERVICE_LOGGER_ID, LOGGER_DEBUG, true);
 
-  {
-    connection_config_t db_conf = {.db_path = sas->conf->spent_addresses_db_path};
-
-    if ((ret = iota_spent_addresses_provider_init(&sap, &db_conf)) != RC_OK) {
-      log_critical(logger_id, "Initializing spent addresses database connection failed\n");
-      return ret;
-    }
+#if defined(IOTA_MAINNET)
+  if ((ret = iota_spent_addresses_service_read_files(sas)) != RC_OK) {
+    log_critical(logger_id, "Reading previous spent addresses files failed\n");
   }
-
-
-done:
-  if ((ret = iota_spent_addresses_provider_destroy(&sap)) != RC_OK) {
-    log_critical(logger_id, "Destroying spent addresses database connection failed\n");
-  }
+#endif
 
   return ret;
 }
