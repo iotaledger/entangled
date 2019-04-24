f909d718fd0561ea1737722d8fb3a965023672a8
tsvisabo <tsvi.sabo@iota.org>
2019-04-24 18:16:07 +0300

    ciri/api - invalid milestone status depends on initial snapshot

--- ciri/api/api.c
+++ ciri/api/api.c
@@ -24,7 +24,7 @@ static logger_id_t logger_id;
 
 static bool invalid_subtangle_status(iota_api_t const *const api, error_res_t **const error) {
   bool invalid = api->core->consensus.milestone_tracker.latest_solid_subtangle_milestone_index ==
-                 api->core->consensus.milestone_tracker.milestone_start_index;
+                 api->core->consensus.milestone_tracker.snapshots_provider->inital_snapshot.metadata.index;
 
   if (invalid) {
     *error = error_res_new(API_ERROR_INVALID_SUBTANGLE);
