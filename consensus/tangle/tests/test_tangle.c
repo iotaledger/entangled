fe3eaacaeb4cfd0e81c882b4171dceec09c590b8
Thibault Martinez <thibault@iota.org>
2019-04-29 18:17:23 +0200

    tangle: bundle_load test

--- consensus/tangle/tests/test_tangle.c
+++ consensus/tangle/tests/test_tangle.c
@@ -84,6 +84,37 @@ void test_find_tail_broken_link(void) {
   transactions_free(txs, 4);
 }
 
+void test_bundle_load(void) {
+  size_t i = 0;
+  iota_transaction_t *iter;
+  iota_transaction_t *txs[4];
+  flex_trit_t hash[FLEX_TRIT_SIZE_243];
+  tryte_t const *const txs_trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
+                                        TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
+  tryte_t const *const hashes_trytes[4] = {TX_1_OF_4_HASH, TX_2_OF_4_HASH, TX_3_OF_4_HASH, TX_4_OF_4_HASH};
+  bundle_transactions_t *bundle;
+
+  transactions_deserialize(txs_trytes, txs, 4, true);
+  TEST_ASSERT(build_tangle(&tangle, txs, 4) == RC_OK);
+
+  flex_trits_from_trytes(hash, HASH_LENGTH_TRIT, hashes_trytes[0], HASH_LENGTH_TRYTE, HASH_LENGTH_TRYTE);
+
+  bundle_transactions_new(&bundle);
+
+  TEST_ASSERT(iota_tangle_bundle_load(&tangle, hash, bundle) == RC_OK);
+
+  TEST_ASSERT_EQUAL_INT(bundle_transactions_size(bundle), 4);
+
+  BUNDLE_FOREACH(bundle, iter) {
+    flex_trits_from_trytes(hash, HASH_LENGTH_TRIT, hashes_trytes[i++], HASH_LENGTH_TRYTE, HASH_LENGTH_TRYTE);
+    TEST_ASSERT_EQUAL_MEMORY(transaction_hash(iter), hash, FLEX_TRIT_SIZE_243);
+  }
+
+  bundle_transactions_free(&bundle);
+
+  transactions_free(txs, 4);
+}
+
 int main(void) {
   UNITY_BEGIN();
   TEST_ASSERT(storage_init() == RC_OK);
@@ -93,6 +124,8 @@ int main(void) {
   RUN_TEST(test_find_tail);
   RUN_TEST(test_find_tail_unknown_tx);
   RUN_TEST(test_find_tail_broken_link);
+
+  RUN_TEST(test_bundle_load);
 
   TEST_ASSERT(storage_destroy() == RC_OK);
   return UNITY_END();
