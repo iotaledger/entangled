777c3ec68c39fc6f0d4ee17f918624be6cd12828
Thibault Martinez <thibault@iota.org>
2019-05-02 16:27:19 +0200

    wots: copyright 120 lines wrap

--- mam/wots/wots.c
+++ mam/wots/wots.c
@@ -2,8 +2,7 @@
  * Copyright (c) 2018 IOTA Stiftung
  * https://github.com/iotaledger/entangled
  *
- * MAM is based on an original implementation & specification by apmi.bsu.by
- * [ITSec Lab]
+ * MAM is based on an original implementation & specification by apmi.bsu.by [ITSec Lab]
  *
  * Refer to the LICENSE file for licensing information
  */
