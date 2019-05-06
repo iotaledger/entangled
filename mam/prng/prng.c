b5cad3d3865fc3528e171be80cc35b397cf6f7ea
Thibault Martinez <thibault@iota.org>
2019-05-06 11:50:37 +0200

    prng: replace trits_t with trit_t for dest

--- mam/prng/prng.c
+++ mam/prng/prng.c
@@ -39,19 +39,18 @@ retcode_t mam_prng_gen3(mam_prng_t const *const prng, mam_prng_destination_tryte
                         trits_t const nonce1, trits_t const nonce2, trits_t const nonce3, trits_t output) {
   mam_sponge_t sponge;
   trits_t KdN[5];
-  MAM_TRITS_DEF0(dt, 3);
-  dt = MAM_TRITS_INIT(dt, 3);
+  trit_t dt[3];
 
   if (prng == NULL) {
     return RC_NULL_PARAM;
   }
 
   KdN[0] = trits_from_rep(MAM_PRNG_KEY_SIZE, prng->secret_key);
-  KdN[1] = dt;
+  KdN[1] = trits_from_rep(3, dt);
   KdN[2] = nonce1;
   KdN[3] = nonce2;
   KdN[4] = nonce3;
-  trits_put3(dt, destination);
+  trits_put3(KdN[1], destination);
 
   mam_sponge_init(&sponge);
   mam_sponge_absorbn(&sponge, MAM_SPONGE_CTL_KEY, 5, KdN);
