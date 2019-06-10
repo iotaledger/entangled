1cd025d295677f127ef6e0a4be30a23335146fab
bingyanglin <bingyang.lin@iota.org>
2019-06-11 01:19:27 +0800

    common: fix typo and add aux func

--- consensus/bundle_validator/tests/bundle_validator.c
+++ consensus/bundle_validator/tests/bundle_validator.c
@@ -34,6 +34,12 @@ void tearDown() {
   TEST_ASSERT(iota_consensus_bundle_validator_destroy() == RC_OK);
 }
 
+void test_prepare_4txs(iota_transaction_t *txs) {
+  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
+                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
+  transactions_deserialize(trytes, txs, 4, true);
+}
+
 void test_iota_consensus_bundle_validator_validate_tail_not_found() {
   bundle_transactions_t *bundle;
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
@@ -60,10 +66,8 @@ void test_bundle_size_1_value_with_wrong_address_invalid() {
   trit_t address[HASH_LENGTH_TRIT];
   bool exist = false;
   iota_transaction_t *txs[4];
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   flex_trits_to_trits(address, HASH_LENGTH_TRIT, transaction_address(txs[1]), HASH_LENGTH_TRIT, HASH_LENGTH_TRIT);
   address[HASH_LENGTH_TRIT - 1] = -1;
   flex_trits_from_trits(transaction_address(txs[0]), HASH_LENGTH_TRIT, address, HASH_LENGTH_TRIT, HASH_LENGTH_TRIT);
@@ -86,10 +90,8 @@ void test_bundle_exceed_supply_pos_invalid() {
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
   iota_transaction_t *txs[4];
   bool exist = false;
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   transaction_set_value(txs[0], IOTA_SUPPLY + 1);
   TEST_ASSERT(build_tangle(&tangle, txs, 4) == RC_OK);
 
@@ -110,10 +112,8 @@ void test_bundle_exceed_supply_neg_invalid() {
   iota_transaction_t *txs[4];
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
   bool exist = false;
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   transaction_set_value(txs[0], -IOTA_SUPPLY - 1);
   TEST_ASSERT(build_tangle(&tangle, txs, 4) == RC_OK);
 
@@ -134,10 +134,8 @@ void test_bundle_incomplete() {
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
   iota_transaction_t *txs[4];
   bool exist = false;
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   TEST_ASSERT(build_tangle(&tangle, txs, 3) == RC_OK);
 
   TEST_ASSERT(iota_tangle_transaction_exist(&tangle, TRANSACTION_FIELD_NONE, NULL, &exist) == RC_OK);
@@ -157,10 +155,8 @@ void test_bundle_invalid_tx_with_wrong_current_idx() {
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
   iota_transaction_t *txs[4];
   bool exist = false;
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   transaction_set_current_index(txs[1], 2);
   TEST_ASSERT(build_tangle(&tangle, txs, 4) == RC_OK);
 
@@ -181,11 +177,9 @@ void test_bundle_invalid_tx_with_wrong_last_idx() {
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
   iota_transaction_t *txs[4];
   bool exist = false;
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
-  transaction_set_current_index(txs[1], 2);
+  test_prepare_4txs(txs);
+  transaction_set_last_index(txs[1], 2);
   TEST_ASSERT(build_tangle(&tangle, txs, 4) == RC_OK);
 
   TEST_ASSERT(iota_tangle_transaction_exist(&tangle, TRANSACTION_FIELD_NONE, NULL, &exist) == RC_OK);
@@ -204,12 +198,10 @@ void test_bundle_invalid_hash_with_wrong_address() {
   bundle_transactions_t *bundle;
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
   trit_t address[HASH_LENGTH_TRIT];
-  bool exist = false;
   iota_transaction_t *txs[4];
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
+  bool exist = false;
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   flex_trits_to_trits(address, HASH_LENGTH_TRIT, transaction_address(txs[1]), HASH_LENGTH_TRIT, HASH_LENGTH_TRIT);
   address[0] = -1;
   flex_trits_from_trits(transaction_address(txs[0]), HASH_LENGTH_TRIT, address, HASH_LENGTH_TRIT, HASH_LENGTH_TRIT);
@@ -230,13 +222,11 @@ void test_bundle_invalid_hash_with_wrong_address() {
 void test_bundle_invalid_hash_with_wrong_obsolete_tag() {
   bundle_transactions_t *bundle;
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
-  bool exist = false;
   iota_transaction_t *txs[4];
   trit_t obsolete_tag[NUM_TRITS_OBSOLETE_TAG];
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
+  bool exist = false;
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   flex_trits_to_trits(obsolete_tag, NUM_TRITS_OBSOLETE_TAG, transaction_obsolete_tag(txs[1]), NUM_TRITS_OBSOLETE_TAG,
                       NUM_TRITS_OBSOLETE_TAG);
   obsolete_tag[0] = -1;
@@ -262,10 +252,8 @@ void test_bundle_invalid_hash_with_wrong_transaction_timestamp() {
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
   iota_transaction_t *txs[4];
   bool exist = false;
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   transaction_set_timestamp(txs[1], 2);
   TEST_ASSERT(build_tangle(&tangle, txs, 4) == RC_OK);
 
@@ -286,11 +274,8 @@ void test_iota_consensus_bundle_validator_validate_size_4_value_wrong_sig_invali
   iota_transaction_t *txs[4];
   trit_t buffer[NUM_TRITS_PER_FLEX_TRIT];
   bundle_status_t bundle_status = BUNDLE_NOT_INITIALIZED;
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
 
-  transactions_deserialize(trytes, txs, 4, true);
-
+  test_prepare_4txs(txs);
   flex_trits_to_trits(buffer, NUM_TRITS_PER_FLEX_TRIT, transaction_signature(txs[1]), NUM_TRITS_PER_FLEX_TRIT,
                       NUM_TRITS_PER_FLEX_TRIT);
   buffer[NUM_TRITS_PER_FLEX_TRIT - 1] = !buffer[NUM_TRITS_PER_FLEX_TRIT - 1];
@@ -313,10 +298,7 @@ void test_iota_consensus_bundle_validator_validate_size_4_value_valid() {
   iota_transaction_t *txs[4];
   bool exist = false;
 
-  tryte_t const *const trytes[4] = {TX_1_OF_4_VALUE_BUNDLE_TRYTES, TX_2_OF_4_VALUE_BUNDLE_TRYTES,
-                                    TX_3_OF_4_VALUE_BUNDLE_TRYTES, TX_4_OF_4_VALUE_BUNDLE_TRYTES};
-
-  transactions_deserialize(trytes, txs, 4, true);
+  test_prepare_4txs(txs);
   TEST_ASSERT(build_tangle(&tangle, txs, 4) == RC_OK);
 
   TEST_ASSERT(iota_tangle_transaction_exist(&tangle, TRANSACTION_FIELD_NONE, NULL, &exist) == RC_OK);
