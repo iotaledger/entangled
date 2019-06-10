1ea725e78139adae60c9cc46bdbe37b1dcda8033
Thibault Martinez <thibault@iota.org>
2019-06-10 17:43:01 +0200

    mam: fix mss skn (#1277)

--- mam/mss/mss_traversal.c
+++ mam/mss/mss_traversal.c
@@ -346,11 +346,13 @@ void mam_mss_auth_path(mam_mss_t *mss, mss_mt_idx_t skn, trits_t path) {
 }
 
 bool mam_mss_next(mam_mss_t *mss) {
-  if (mss->skn == MAM_MSS_MAX_SKN(mss->height)) {
+  size_t sks = mam_mss_num_remaining_sks(mss);
+
+  if (sks == 0) {
     return false;
   }
 
-  if (mss->skn < MAM_MSS_MAX_SKN(mss->height)) {
+  if (sks != 1) {
     mss_mt_refresh(mss);
     mss_mt_build_stacks(mss);
   }
