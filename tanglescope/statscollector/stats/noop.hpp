91c84869b89f03062662e03ddc256fbaaf428bd6
bingyanglin <bingyang.lin@iota.org>
2019-06-16 23:56:13 +0800

    tanglescope: add function documentations

--- tanglescope/statscollector/stats/noop.hpp
+++ tanglescope/statscollector/stats/noop.hpp
@@ -11,9 +11,37 @@ namespace statscollector {
 
 class NoopTXStats : public TXStats {
  public:
-  virtual void trackNewTX(iri::TXMessage&, PrometheusCollector::CountersMap& /* metrics */){};
+  /**
+   * Track a new tx
+   *
+   * @param msg The message
+   * @param metric The metric counter map
+   */
+  virtual void trackNewTX(iri::TXMessage& /* msg */, PrometheusCollector::CountersMap& /* metrics */){};
+
+  /**
+   * Track a tx reattachment
+   *
+   * @param metric The metric counter map
+   */
   virtual void trackReattachedTX(PrometheusCollector::CountersMap& /* metrics */){};
+
+  /**
+   * Track a new bundle
+   *
+   * @param metric The metric counter map
+   */
   virtual void trackNewBundle(PrometheusCollector::CountersMap& /* metrics */){};
+
+  /**
+   * Track a confirmed bundle
+   *
+   * @param totalValue The total value
+   * @param size The size
+   * @param avgBundleDuration The bundle confirmation duration
+   * @param counters The counters
+   * @param histograms The histograms
+   */
   virtual void trackConfirmedBundle(int64_t /* totalValue */, uint64_t /* size */, uint64_t /* avgBundleDuration */,
                                     PrometheusCollector::CountersMap& /* counters */,
                                     PrometheusCollector::HistogramsMap& /* histograms */){};
