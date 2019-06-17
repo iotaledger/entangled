3beeafb0071d77cae0153e14a821a72fbc1b9aa7
bingyanglin <bingyang.lin@iota.org>
2019-06-17 21:01:49 +0800

    tanglescope: modify func. dir doc.

--- tanglescope/statscollector/stats/noop.hpp
+++ tanglescope/statscollector/stats/noop.hpp
@@ -14,33 +14,33 @@ class NoopTXStats : public TXStats {
   /**
    * Track a new tx
    *
-   * @param[in, out] msg    The message
-   * @param[in, out] metric The metric counter map
+   * @param[in]  msg    The message
+   * @param[out] metric The metric counter map
    */
   virtual void trackNewTX(iri::TXMessage& /* msg */, PrometheusCollector::CountersMap& /* metrics */){};
 
   /**
    * Track a tx reattachment
    *
-   * @param[in, out] metric The metric counter map
+   * @param[out] metric The metric counter map
    */
   virtual void trackReattachedTX(PrometheusCollector::CountersMap& /* metrics */){};
 
   /**
    * Track a new bundle
    *
-   * @param[in, out] metric The metric counter map
+   * @param[out] metric The metric counter map
    */
   virtual void trackNewBundle(PrometheusCollector::CountersMap& /* metrics */){};
 
   /**
    * Track a confirmed bundle
    *
-   * @param[in]      totalValue        The total value
-   * @param[in]      size              The size
-   * @param[in]      avgBundleDuration The bundle confirmation duration
-   * @param[in, out] counters          The counters
-   * @param[in, out] histograms        The histograms
+   * @param[in]  totalValue        The total value
+   * @param[in]  size              The size
+   * @param[in]  avgBundleDuration The bundle confirmation duration
+   * @param[out] counters          The counters
+   * @param[out] histograms        The histograms
    */
   virtual void trackConfirmedBundle(int64_t /* totalValue */, uint64_t /* size */, uint64_t /* avgBundleDuration */,
                                     PrometheusCollector::CountersMap& /* counters */,
