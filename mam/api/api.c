077c7bdedbf794c008b270238ebb67cea5823a01
Thibault Martinez <thibault.martinez.30@gmail.com>
2019-06-18 14:41:08 +0200

    Update mam/api/api.c
    Co-Authored-By: Sam Chen <sam.chen@iota.org>

--- mam/api/api.c
+++ mam/api/api.c
@@ -39,7 +39,7 @@ static void mam_api_bundle_wrap(bundle_transactions_t *const bundle, trit_t cons
   flex_trits_from_trits(buffer, NUM_TRITS_MESSAGE, address, NUM_TRITS_ADDRESS, NUM_TRITS_ADDRESS);
   transaction_set_address(&transaction, buffer);
   transaction_set_value(&transaction, 0);
-  transaction_set_obsolete_tag(&transaction, transaction.data.signature_or_message);
+  transaction_set_obsolete_tag(&transaction, transaction.essence.obsolete_tag);
   transaction_set_timestamp(&transaction, current_timestamp_ms() / 1000);
   flex_trits_from_trits(buffer, NUM_TRITS_MESSAGE, tag, NUM_TRITS_TAG, NUM_TRITS_TAG);
   transaction_set_tag(&transaction, buffer);
