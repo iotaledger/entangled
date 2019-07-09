be43a8575155c642da656e142dafc5c70c3422e8
Thibault Martinez <thibault@iota.org>
2019-07-09 22:56:37 +0200

    utils: define macros only if undefined

--- utils/macros.h
+++ utils/macros.h
@@ -8,10 +8,17 @@
 #ifndef __UTILS_MACROS_H__
 #define __UTILS_MACROS_H__
 
+#ifndef UNUSED
 #define UNUSED(...) (void)(__VA_ARGS__)
+#endif
 
+#ifndef MAX
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#endif
+
+#ifndef MIN
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#endif
 
 // max uint64 18446744073709551616
 #define MAX_CHARS_UINT64 20
