55a29c4814910756fd366d8fd70350791c7f7dd7
Tsvi Sabo <tsvi.sabo@iota.org>
2019-07-20 20:01:00 +0300

    Update ciri/consensus/tip_selection/cw_rating_calculator/cw_rating_dfs_impl.c
    Co-Authored-By: Thibault Martinez <thibault.martinez.30@gmail.com>

--- ciri/consensus/tip_selection/cw_rating_calculator/cw_rating_dfs_impl.c
+++ ciri/consensus/tip_selection/cw_rating_calculator/cw_rating_dfs_impl.c
@@ -32,7 +32,7 @@ static retcode_t cw_rating_dfs_do_dfs_from_db(tangle_t *const tangle, flex_trit_
   hash243_stack_t stack = NULL;
   flex_trit_t *curr_tx_hash = NULL;
   bool trunk_exist = false;
-  bool branch_exist;
+  bool branch_exist = false;
   DECLARE_PACK_SINGLE_TX(tx, tx_ptr, transaction_pack);
 
   uint64_t start_timestamp, end_timestamp;
