2e29057b86ac2a25a99e8ba55245f575c8edab90
Tsvi <tsvi.sabo@iota.org>
2019-07-21 12:29:13 +0300

    consensus/pruning: printfs

--- ciri/consensus/snapshot/local_snapshots/pruning_service.c
+++ ciri/consensus/snapshot/local_snapshots/pruning_service.c
@@ -230,9 +230,11 @@ static retcode_t prune_transactions(pruning_service_t *const ps, tangle_t const 
 
   *should_wait_for_next_snapshot = false;
 
+  printf("%s loading milestone", __FUNCTION__);
   ERR_BIND_GOTO(iota_tangle_milestone_load_by_index(tangle, ps->last_pruned_snapshot_index + 1, &milestone_pack), err,
                 cleanup);
 
+  printf("%s collecting transactions", __FUNCTION__);
   ERR_BIND_GOTO(
       collect_transactions_to_prune(ps, tangle, sap, milestone.hash, &transactions_to_prune, &has_solid_entry_points),
       err, cleanup);
