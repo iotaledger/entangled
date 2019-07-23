e4285ad5c91efff47d71b2c9d0e9ed867dbd1898
Thibault Martinez <thibault@iota.org>
2019-07-23 22:06:45 +0200

    hasher: free inner gossip packet

--- ciri/node/pipeline/hasher.c
+++ ciri/node/pipeline/hasher.c
@@ -202,6 +202,7 @@ static void *hasher_stage_routine(hasher_stage_t *const hasher) {
 
   for (size_t j = 0; j < HASHER_MAX; j++) {
     if (entries[j] != NULL) {
+      free(entries[j]->payload.gossip);
       free(entries[j]);
     }
   }
