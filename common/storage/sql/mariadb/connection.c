0a1ba176e65c1f2daa866047d9ba8bf3300a1041
Thibault Martinez <thibault@iota.org>
2019-08-15 17:40:35 -0400

    storage: add mariadb stubs

---
+++ common/storage/sql/mariadb/connection.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2019 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include "common/storage/sql/mariadb/connection.h"
---
+++ common/storage/sql/mariadb/connection.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019 IOTA Stiftung
+ * https://github.com/iotaledger/entangled
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#ifndef __COMMON_STORAGE_SQL_MARIADB_CONNECTION_H__
+#define __COMMON_STORAGE_SQL_MARIADB_CONNECTION_H__
+
+#include "common/storage/connection.h"
+#include "common/storage/sql/statements.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __COMMON_STORAGE_SQL_MARIADB_CONNECTION_H__
