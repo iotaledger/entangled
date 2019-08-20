98f1ed8cf8c61db3e1241608cca55c270dfe7dee
Thibault Martinez <thibault@iota.org>
2019-08-20 16:48:22 -0400

    mariadb: initial storage init/destroy

--- common/storage/sql/mariadb/storage.c
+++ common/storage/sql/mariadb/storage.c
@@ -6,3 +6,20 @@
  */
 
 #include "common/storage/storage.h"
+#include "utils/logger_helper.h"
+
+#define MARIADB_LOGGER_ID "mariadb"
+
+static logger_id_t logger_id;
+
+retcode_t storage_init() {
+  logger_id = logger_helper_enable(MARIADB_LOGGER_ID, LOGGER_DEBUG, true);
+
+  return RC_OK;
+}
+
+retcode_t storage_destroy() {
+  logger_helper_release(logger_id);
+
+  return RC_OK;
+}
