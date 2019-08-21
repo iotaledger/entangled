63099ab86f7d136de2c69e95daac8a3ebc9b2557
Thibault Martinez <thibault@iota.org>
2019-08-20 22:32:14 -0400

    storage: split SQL files

---
+++ common/storage/sql/mariadb/spent-addresses-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS iota_spent_address (
+  hash BLOB NOT NULL,
+  PRIMARY KEY (hash(243))
+);
+
+CREATE INDEX IF NOT EXISTS spent_address_hash_index ON iota_spent_address(hash);
