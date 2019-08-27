2c82888c7a9836a39f24767f4df095c43d5a0794
Thibault Martinez <thibault@iota.org>
2019-08-27 17:11:02 -0400

    mariadb: fix sql files

--- common/storage/sql/mariadb/tangle-schema.sql
+++ common/storage/sql/mariadb/tangle-schema.sql
@@ -27,14 +27,14 @@ CREATE INDEX IF NOT EXISTS bundle_index ON iota_transaction(bundle(243));
 CREATE INDEX IF NOT EXISTS trunk_index ON iota_transaction(trunk(243));
 CREATE INDEX IF NOT EXISTS branch_index ON iota_transaction(branch(243));
 CREATE INDEX IF NOT EXISTS tag_index ON iota_transaction(tag(81));
-CREATE INDEX IF NOT EXISTS transaction_hash_index ON iota_transaction(hash(243));
 CREATE INDEX IF NOT EXISTS arrival_time_index ON iota_transaction(arrival_timestamp);
+CREATE UNIQUE INDEX IF NOT EXISTS transaction_hash_index ON iota_transaction(hash(243));
 
 CREATE TABLE IF NOT EXISTS iota_milestone (
   id INTEGER NOT NULL,
-  hash BLOB NOT NULL UNIQUE,
+  hash BLOB NOT NULL,
   delta BLOB,
   PRIMARY KEY (id)
 );
 
-CREATE INDEX IF NOT EXISTS milestone_hash_index ON iota_milestone(hash(243));
+CREATE UNIQUE INDEX IF NOT EXISTS milestone_hash_index ON iota_milestone(hash(243));
