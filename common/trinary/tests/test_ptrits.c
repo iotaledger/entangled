a3502c1569788076135c8a7c1c9c65460ed930f8
Vlad Semenov <vlad.semenov@iota.org>
2019-08-28 15:20:21 +0300

    test_ptrits use trits_to_long

--- common/trinary/tests/test_ptrits.c
+++ common/trinary/tests/test_ptrits.c
@@ -10,14 +10,7 @@
 
 #include "common/trinary/ptrit.h"
 #include "common/trinary/ptrit_incr.h"
-
-int trits_to_int(size_t n, trit_t const *t) {
-  int s = 0;
-  for (t += n; n--;) {
-    s = s * 3 + (int)*--t;
-  }
-  return s;
-}
+#include "common/trinary/trit_long.h"
 
 #define TEST_N 6
 #define TEST_M ((3 * 3 * 3 * 3 * 3 * 3 - 1) / 2)
@@ -26,18 +19,20 @@ void test_ptrits_set_iota(void) {
   ptrit_t p[TEST_N];
   trit_t value[TEST_N];
   size_t i;
-  int s;
+  int64_t s;
 
   memset(value, -1, sizeof(value));
   ptrit_set_iota(TEST_N, p, value);
   ptrits_get_slice(TEST_N, value, p, 0);
   s = -TEST_M;
-  TEST_ASSERT_EQUAL_INT(s, trits_to_int(TEST_N, value));
+  TEST_ASSERT_EQUAL_INT(s, trits_to_long(value, TEST_N));
 
   for (i = 1; i < PTRIT_SIZE; ++i) {
     ptrits_get_slice(TEST_N, value, p, i);
-    ++s;
-    TEST_ASSERT_EQUAL_INT(s, trits_to_int(TEST_N, value));
+    if (++s > TEST_M) {
+      s = -TEST_M;
+    }
+    TEST_ASSERT_EQUAL_INT(s, trits_to_long(value, TEST_N));
   }
 }
 
