97eefdebde91cc2950855522e7ebf0d218034e1c
Tsvi <tsvi.sabo@iota.org>
2019-09-03 10:42:34 +0300

    entangled/ciri/api: http - fix endless looping

--- ciri/api/http/http.c
+++ ciri/api/http/http.c
@@ -711,6 +711,7 @@ retcode_t iota_api_http_destroy(iota_api_http_t *const api) {
   for (tangle_iter = (tangle_t *)utarray_back(api->tangle_db_connections); tangle_iter != NULL;
        tangle_iter = (tangle_t *)utarray_back(api->tangle_db_connections)) {
     iota_tangle_destroy(tangle_iter);
+    utarray_pop_back(api->tangle_db_connections);
   }
 
   utarray_free(api->tangle_db_connections);
@@ -719,6 +720,7 @@ retcode_t iota_api_http_destroy(iota_api_http_t *const api) {
        spent_addresses_iter != NULL;
        spent_addresses_iter = (spent_addresses_provider_t *)utarray_back(api->spent_addresses_db_connections)) {
     iota_spent_addresses_provider_destroy(spent_addresses_iter);
+    utarray_pop_back(api->spent_addresses_db_connections);
   }
 
   utarray_free(api->spent_addresses_db_connections);
