bd6e555a598075d90357480ea2358f5c9db3af16
Markus Kuhlmann <kuhlmann.markus@gmail.com>
2019-09-18 13:55:04 +0200

    now compliant to clang-format and buildifier

--- mam/examples/ntru-key-exchange.c
+++ mam/examples/ntru-key-exchange.c
@@ -19,40 +19,44 @@
 
 int main() {
   retcode_t ret = RC_OK;
-  //Setting up two APIs
+  // Setting up two APIs
   mam_api_t sender_api;
   tryte_t *sender_seed = (tryte_t *)SENDERSEED;
   ERR_BIND_RETURN(mam_api_init(&sender_api, sender_seed), ret);
   mam_api_t receiver_api;
   tryte_t *receiver_seed = (tryte_t *)RECEIVERSEED;
   ERR_BIND_RETURN(mam_api_init(&receiver_api, receiver_seed), ret);
-  //Generating a keypair for the receiver api
+  // Generating a keypair for the receiver api
   mam_ntru_sk_t ntru;
-  MAM_TRITS_DEF(nonce, 3*9);
-  nonce = MAM_TRITS_INIT(nonce, 3*9);
-  trits_from_str(nonce,NONCE);
+  MAM_TRITS_DEF(nonce, 3 * 9);
+  nonce = MAM_TRITS_INIT(nonce, 3 * 9);
+  trits_from_str(nonce, NONCE);
   ntru_sk_reset(&ntru);
   ntru_sk_gen(&ntru, &receiver_api.prng, nonce);
-  //Setting the generated keypair to the receiver API
+  // Setting the generated keypair to the receiver API
   ERR_BIND_RETURN(mam_api_add_ntru_sk(&receiver_api, &ntru), ret);
-  //Adding the receiver APIs public key to the sender API public key list
+  // Adding the receiver APIs public key to the sender API public key list
   ERR_BIND_RETURN(mam_api_add_ntru_pk(&sender_api, &ntru.public_key), ret);
-  //Preparing channel and endpoint with sender API with randomly chosen height
+  // Preparing channel and endpoint with sender API with randomly chosen height
   tryte_t channel_id[MAM_CHANNEL_ID_TRYTE_SIZE];
   ERR_BIND_RETURN(mam_api_channel_create(&sender_api, 2, channel_id), ret);
   tryte_t endpoint_id[MAM_ENDPOINT_ID_TRYTE_SIZE];
   ERR_BIND_RETURN(mam_api_endpoint_create(&sender_api, 2, channel_id, endpoint_id), ret);
-  //prepare bundle for sender API to write to
+  // prepare bundle for sender API to write to
   bundle_transactions_t *bundle = NULL;
   bundle_transactions_new(&bundle);
-  //prepare and write header and packet
+  // prepare and write header and packet
   trit_t message_id[MAM_MSG_ID_SIZE];
-  ERR_BIND_RETURN(mam_api_bundle_write_header_on_endpoint(&sender_api, channel_id, endpoint_id, NULL, sender_api.ntru_pks, bundle, message_id), ret);
-  ERR_BIND_RETURN(mam_api_bundle_write_packet(&sender_api, message_id, (tryte_t *)PL, strlen(PL), MAM_MSG_CHECKSUM_SIG, true, bundle), ret);
-  //Set created channel and endpoint as trusted in receiver API
+  ERR_BIND_RETURN(mam_api_bundle_write_header_on_endpoint(&sender_api, channel_id, endpoint_id, NULL,
+                                                          sender_api.ntru_pks, bundle, message_id),
+                  ret);
+  ERR_BIND_RETURN(mam_api_bundle_write_packet(&sender_api, message_id, (tryte_t *)PL, strlen(PL), MAM_MSG_CHECKSUM_SIG,
+                                              true, bundle),
+                  ret);
+  // Set created channel and endpoint as trusted in receiver API
   ERR_BIND_RETURN(mam_api_add_trusted_channel_pk(&receiver_api, channel_id), ret);
   ERR_BIND_RETURN(mam_api_add_trusted_endpoint_pk(&receiver_api, endpoint_id), ret);
-  //read the bundle with the receiver API and print the decrypted payload
+  // read the bundle with the receiver API and print the decrypted payload
   tryte_t *payload = NULL;
   size_t payload_size = 0;
   bool is_last_packet = false;
@@ -62,11 +66,11 @@ int main() {
     fprintf(stderr, "%c", payload[i]);
   }
   fprintf(stderr, "\n");
-  //free ressources
+  // free ressources
   free(payload);
   bundle_transactions_free(&bundle);
   mam_api_destroy(&sender_api);
   mam_api_destroy(&receiver_api);
-  //finished
+  // finished
   return ret;
 }
