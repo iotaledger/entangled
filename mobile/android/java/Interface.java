76391f2f3126833ca0ee17b1976e4c4a0a86ed18
Rajiv Shah <rajivshah1@icloud.com>
2019-12-02 12:53:52 -0500

    mobile/android: update bindings for mining_threshold parameter

--- mobile/android/java/Interface.java
+++ mobile/android/java/Interface.java
@@ -8,5 +8,5 @@ public class Interface {
     public static native String iota_sign_signature_gen_trytes(String seed, int index, int security, String bundleHash);
     public static native byte[] iota_sign_signature_gen_trits(byte[] seed, int index, int security, byte[] bundleHash);
     public static native String iota_digest(String trytes);
-    public static native long bundle_miner_mine(byte[] bundle_normalized_max, int security, byte[] essence, int essence_length, int count, int nprocs);
+    public static native long bundle_miner_mine(byte[] bundle_normalized_max, int security, byte[] essence, int essence_length, int count, int nprocs, int mining_threshold);
 }
