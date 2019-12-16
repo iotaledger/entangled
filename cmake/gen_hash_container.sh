3086ba9e330ca6f7d9d8c88fbf64e657cf692c7f
Michi <38973614+MichiKuhl@users.noreply.github.com>
2019-12-16 01:55:04 +0100

    Update gen_hash_container.sh
    The path of the hash_template_dir had a typo.

--- cmake/gen_hash_container.sh
+++ cmake/gen_hash_container.sh
@@ -2,7 +2,7 @@
 HASH_SIZE_LIST=("27" "81" "243" "6561" "8019")
 TYPE_LIST=("queue" "stack")
 
-HASH_TEMPLATE_DIR="./utils/containers/hash"
+HASH_TEMPLATE_DIR="../utils/containers/hash"
 for HASH_TYPE in ${TYPE_LIST[@]}; do
     for HASH_FILE in ${HASH_TEMPLATE_DIR}/hash_${HASH_TYPE}*.tpl; do
         FILE_NAME=$(basename "${HASH_FILE}" .tpl)
